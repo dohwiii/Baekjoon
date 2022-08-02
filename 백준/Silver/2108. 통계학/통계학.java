73e9095828f56e6a581b06cbae00011458314d29
dohwiii <102406112+dohwiii@users.noreply.github.com>
2022-08-02 23:03:05 +0900

    Update 통계학.java

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,83 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
+public class Main
+{
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int Test = Integer.parseInt(bf.readLine());
-		int[] arr = new int[Test];
-		int[] check=new int[8001];
-		double sum = 0.0;
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		int distance=0;
-		int mode_max=0;
-		int mode=0;
-		boolean flag=false;
-		
+	public static void main(String[] args)
+	{
 
-		
-		for(int i=0;i<Test;i++)
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int[] freq = new int[8001];
+
+		for (int i = 0; i < N; i++)
 		{
-			arr[i]=Integer.parseInt(bf.readLine());
-			sum+=arr[i];
-			check[arr[i]+4000]++;
+			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		int x=arr.length;
-		if(x>1)
-		{
-			for(int i=0;i<x;i++)
-			{
-				if(max<arr[i])
-				{
-					max=arr[i];
-				}
-				if(min>arr[i])
-				{
-					min=arr[i];
-				}
-			}
-			distance=max+(-min);
-			
-			for(int i=min+4000;i<=max+4000;i++)
-			{
-				if(check[i]>0)
-				{
-					if(mode_max<check[i])
-					{
-						mode_max=check[i];
-						mode=i-4000;
-						flag=true;
-					}
-					else if(mode_max==check[i]&&flag==true)
-					{
-						mode=i-4000;
-						flag=false;
-					}
-				}
-				
-			}
-		}
-		else
-		{
-			distance=0;
-			mode=arr[0];
-		}
-		
-		
-		
-		
-		System.out.println(Math.round(sum/Test)); //산술평균
-		System.out.println(arr[Test/2]); //중앙값
-		System.out.println(mode);
-		System.out.println(distance);
-		
 
+		System.out.println(avg(arr, N));
+		System.out.println(arr[Math.round(N / 2)]);
+
+		int max = 0;
+		for (int i = 0; i < N; i++)
+		{
+			freq[arr[i] + 4000]++;
+			max = Math.max(max, freq[arr[i] + 4000]);
+		}
+
+		int second = 0; // 두 번쨰로 작은 값 저장 변수
+		int count = 0;
+
+		for (int i = 0; i < freq.length; i++)
+		{
+			if (freq[i] == max)
+			{
+				count++;
+				second = i - 4000;
+			}
+			// 최빈값이 두 개 이상일 경우 두 번째로 작은 값을 찾아 저장한다.
+			if (count == 2)
+			{
+				second = i - 4000;
+				break;
+			}
+		}
+
+		System.out.println(second);
+		System.out.println(range(arr));
 	}
-	
+
+	public static int avg(int[] arr, int N)
+	{
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++)
+		{
+			sum = sum + arr[i];
+		}
+		if (sum < 0)
+			return (int) ((sum / (double) N) - 0.5);
+		else
+			return (int) ((sum / (double) N) + 0.5);
+	}
+
+	public static int range(int[] arr)
+	{
+		Arrays.sort(arr);
+		int max = arr[arr.length - 1];
+		int min = arr[0];
+
+		return Math.abs(max - min);
+	}
 
 }
