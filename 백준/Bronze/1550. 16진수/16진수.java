5d4c7ba9ac819c24c8db9eefbf84a26f425c8650
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-01-09 20:07:37 +0900

    [Bronze II] Title: 16진수, Time: 200 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,20 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+
+        String hex = sc.next();
+        int x = Integer.parseInt(hex, 16);
+
+        System.out.println(x);
+
+
+    }
+
+}
