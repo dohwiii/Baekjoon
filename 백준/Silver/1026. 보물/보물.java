890d28532d122f8cbc7641dedd22c548c94c19f6
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-01-11 23:44:09 +0900

    [Silver IV] Title: 보물, Time: 224 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,39 +1,36 @@
-
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args)  {
 
-	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] a=new int[N];
-		int[] b=new int[N];
-		int sum=0;
-		
-		for(int i=0;i<N;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		for(int i=0;i<N;i++)
-		{
-			b[i]=sc.nextInt();
-		}
-		Integer[] aArr=Arrays.stream(a).boxed().toArray(Integer[]::new);
-		
-		Arrays.sort(aArr, Collections.reverseOrder());
-		Arrays.sort(b);
-		
-		int[] aOriginal=Arrays.stream(aArr).mapToInt(Integer::intValue).toArray();
-		
-		for(int i=0;i<N;i++)
-		{
-			sum+=aOriginal[i]*b[i];
-		}
-		System.out.println(sum);
-		
-		
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = sc.nextInt();
+        }
 
-	}
+        List<Integer> bList = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            bList.add(b[i]);
+        }
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int max = Collections.max(bList);
+            ans = ans + (a[i] * max);
+            bList.remove(Integer.valueOf(max));
+        }
+        System.out.println(ans);
+    }
 }
