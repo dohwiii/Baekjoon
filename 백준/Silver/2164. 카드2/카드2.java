db33be6969f848e7b9a185ce0f9f144473f10814
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-02-03 20:32:14 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45548 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Queue<Integer> queue = new LinkedList<>();
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= N; i++) {
             queue.add(i);
@@ -20,9 +25,6 @@ public class Main {
             }
         }
         System.out.println(queue.peek());
-
-
-
 
     }
 }
