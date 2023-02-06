f12e201d1587e0018b55f989b48074bcf9989b5c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-02-06 22:12:41 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,31 +1,49 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
 
-public class Main
-{
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.util.*;
 
-	public static void main(String[] args)
-	{
+public class Main {
 
-		Scanner sc = new Scanner(System.in);
-		
-		String str= sc.next();
-		List<Character> list =new ArrayList<Character>();
-		
-		for(int i=0;i<str.length();i++)
-		{
-			list.add(str.charAt(i));
-		}
-		
-		Collections.sort(list, Collections.reverseOrder());
-		
-		for(char x : list)
-		{
-			System.out.print(x);
-		}
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        int N = str.length();
+        int[] arr = new int[N];
 
-	}
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = str.charAt(i) - '0';
 
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            int max = i;
+
+            for (int j = i + 1; j < N; j++)
+            {
+                if (arr[max] < arr[j])
+                {
+                    max = j; //2
+
+                }
+            }
+            swap(arr, i, max);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            System.out.print(arr[i]);
+        }
+
+    }
+
+    static void swap(int[] arr, int x, int y)
+    {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
 }
