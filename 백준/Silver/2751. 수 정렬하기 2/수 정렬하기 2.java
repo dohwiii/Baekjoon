02fdf733c4fca24897713030d1ca216b18074df5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-02-10 17:50:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 880 ms, Memory: 102792 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,34 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-		int N = Integer.parseInt(bf.readLine());
-		int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        mergeSort(arr);
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.println(sb.toString());
 
-		for (int i = 0; i < N; i++) {
+    }
+    static void mergeSort(int[] arr, int tmp[], int start, int end) //쪼개기
+    {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, tmp, start, mid);
+            mergeSort(arr, tmp, mid + 1, end);
+            merge(arr, tmp, start, mid, end);
+        }
 
-			arr[i] = Integer.parseInt(bf.readLine());
+    }
 
-		}
-		bf.close();
+    static void merge(int[] arr, int[] tmp, int start, int mid, int end) //병합하기
+    {
+        for (int i = start; i <= end; i++)
+        {
+            tmp[i] = arr[i];
+        }
+        int index = start;
+        int part1 = start;
+        int part2 = mid + 1;
 
-		Arrays.sort(arr);
+        while (part1 <= mid && part2 <= end)
+        {
 
-		for (int x : arr) {
-			bw.write(String.valueOf(x));
-			bw.write("\n");
-		}
-		bw.close();
-	}
+            if (tmp[part1] <= tmp[part2]) {
+                arr[index] = tmp[part1];
+                part1++;
+
+            } else {
+                arr[index] = tmp[part2];
+                part2++;
+            }
+            index++;
+
+        }
+        for (int i = 0; i <= mid - part1; i++)
+        {
+            arr[index + i] = tmp[part1 + i];
+        }
+
+    }
+
+    private static void mergeSort(int[] arr) {
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1);
+    }
 
 }
