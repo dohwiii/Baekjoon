45f460700ea4e44a731874fcde5499862c1eb4f6
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-02-27 19:50:06 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,71 @@
-import java.io.*;
-import java.util.*;
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
     static int[][] arr;
-    static int N, M;
+    static int N;
+    static int M;
+    static boolean[][] visited;
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); //노드 수
-        M = Integer.parseInt(st.nextToken()); //엣지 수
-
+        N = Integer.parseInt(st.nextToken()); //세로
+        M = Integer.parseInt(st.nextToken()); //가로
         arr = new int[N][M];
         visited = new boolean[N][M];
+        count = 0;
 
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
 
-            for (int j = 0; j < M; j++)
-            {
+            for (int j = 0; j < M; j++) {
                 arr[i][j] = str.charAt(j) - '0';
             }
-        }
 
+        }
         BFS(0, 0);
         System.out.println(arr[N - 1][M - 1]);
 
     }
 
-    public static void BFS(int i, int j)
+    public static void BFS(int x, int y)
     {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{i, j});
-        visited[i][j] = true;
+        int[] dx = {0, 0, -1, 1}; //아래, 위, 오른쪽, 위쪽
+        int[] dy = {-1, 1, 0, 0};
+
+        queue.add(new int[]{x, y});
+        count = 1;
 
         while (!queue.isEmpty())
         {
             int[] now = queue.poll();
 
-            for (int k = 0; k < 4; k++) //상하좌우 탐색
+            for (int i = 0; i < 4; i++)
             {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
+                int x1 = now[0] + dx[i];
+                int y1 = now[1] + dy[i];
 
-                if (x >= 0 && y >= 0 && x < N && y < M) //배열을 넘어가면 안되고
+                if ((x1 >= 0 && x1 < N) && (y1  >= 0 && y1 < M))
                 {
-                    if (arr[x][y] != 0 && !visited[x][y]) //0이거나 이미 방문한 곳이면 안됨
-                    {//이제 갈 수 있음
+                    if (arr[x1][y1] == 1 && !visited[x1][y1])
+                    {
+                        queue.add(new int[]{x1, y1});
+                        visited[x1][y1] = true;
+                        arr[x1][y1] = arr[now[0]][now[1]] + 1;
 
-                        visited[x][y] = true;
-                        arr[x][y] = arr[now[0]][now[1]] + 1; //핵심
-                        queue.add(new int[]{x, y});
 
                     }
-
                 }
 
             }
@@ -67,4 +73,5 @@ public class Main {
         }
 
     }
+
 }
