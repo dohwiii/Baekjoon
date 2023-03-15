b628b7eb6f4d202292072dcf060a69c2fc8dc21d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-03-15 20:18:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,98 +1,86 @@
-
-import javax.xml.crypto.Data;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main
-{
-    static boolean[][] visited;
+public class Main {
     static int[][] map;
-    static int N;
-    static int[] dx = {-1, 1, 0, 0};
+    static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static ArrayList<Integer> list;
+    static boolean[][] visited;
+    static long[] apart = new long[25 * 25];
+    static int N;
+    static int apartment;
+    static ArrayList<Long> list;
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException
-    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         visited = new boolean[N][N];
         map = new int[N][N];
+        apartment = 0;
         list = new ArrayList<>();
-        int apartment = 0;
 
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
-
-            for (int j = 0; j < N; j++)
-            {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = str.charAt(j) - '0';
             }
         }
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (!visited[i][j] && map[i][j] == 1)
-                {
-
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && map[i][j] > 0) {
                     apartment++;
                     BFS(i, j);
-
                 }
             }
-
         }
         System.out.println(apartment);
-        Collections.sort(list);
-        for (int i : list)
-        {
-            System.out.println(i);
+        Arrays.sort(apart);
+        for (long i : apart) {
+            if (i != 0) {
+                System.out.println(i);
+            }
         }
+
 
     }
-
     public static void BFS(int x, int y)
     {
-        if (visited[x][y])
-        {
+        if (visited[x][y]) {
+
             return;
         }
-
+        Queue<Coordinate> queue = new LinkedList<>();
+        queue.add(new Coordinate(x, y));
         visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        int count = 1;
+        apart[apartment] = 1;
 
-        while (!queue.isEmpty())
-        {
-            int[] now = queue.poll();
+        while (!queue.isEmpty()) {
+            Coordinate now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int x1 = now.x + dx[i];
+                int y1 = now.y + dy[i];
 
-            for (int i = 0; i < 4; i++)
-            {
-                //인덱스
-                int x1 = now[0] + dx[i];
-                int y1 = now[1] + dy[i];
-
-                if ((x1 >= 0 && x1 < N) && (y1 >= 0 && y1 < N))
-                {
-                    if (map[x1][y1] == 1 && !visited[x1][y1])
-                    {
+                if (x1 >= 0 && y1 >= 0 && x1 < N && y1 < N) {
+                    if (!visited[x1][y1] && map[x1][y1] != 0) {
                         visited[x1][y1] = true;
-                        queue.add(new int[]{x1, y1});
-                        count++;
-
+                        queue.add(new Coordinate(x1, y1));
+                        apart[apartment]++;
                     }
                 }
             }
-
-        }
-        if (count != 0)
-        {
-            list.add(count);
         }
     }
 }
+class Coordinate
+{
+    int x, y, count;
 
+    public Coordinate(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+}
