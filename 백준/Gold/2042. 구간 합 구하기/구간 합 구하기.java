69373556769c379757f571c59370f0b24d3f773a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-03-24 20:52:34 +0900

    [Gold I] Title: 구간 합 구하기, Time: 776 ms, Memory: 98280 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -0,0 +1,84 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+    static long[] tree;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //수의 개수
+        M = Integer.parseInt(st.nextToken()); //변경이 일어나는 횟수
+        K = Integer.parseInt(st.nextToken()); //구간의 합을 구하는 횟수
+        int k = 1;
+        while (Math.pow(2, k) < N) {
+            k++;
+        }
+        tree = new long[(int) Math.pow(2, k) * 2];
+        int startIndex = (int) Math.pow(2, k);
+
+        for (int i = startIndex; i < N + startIndex; i++) {
+
+            tree[i] = Long.parseLong(br.readLine());
+        }
+        for (int i = startIndex - 1; i > 0; i--) {
+            tree[i] = tree[2 * i] + tree[2 * i + 1];
+        }
+        for (int i = 0; i < M + K; i++) {
+            st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+
+            if (a == 1)
+            {
+                int index = (int) (b + (Math.pow(2, k) - 1));
+                changeVal(index, c);
+            }
+            if (a == 2)
+            {
+                int start = (int) (b + (Math.pow(2, k) - 1));
+                long end = (int) (c + (Math.pow(2, k) - 1));
+                System.out.println(sumVal(start, (int)end));
+            }
+        }
+
+
+
+    }
+
+    public static void changeVal(int index, long val) {
+        long diff = val - tree[index]; //6-tree[10](3) = 3
+
+        while (index > 0)
+        {
+            tree[index] = tree[index] + diff;
+            index = index / 2;
+        }
+    }
+
+    public static long sumVal(int s, int e)
+    {
+        long sum = 0;
+        int start = s;
+        int end = e;
+        while (start <= end) {
+
+            if (start % 2 == 1) {
+                sum = sum + tree[start];
+            }
+            if (end % 2 == 0) {
+                sum = sum + tree[end];
+            }
+            start = (start + 1) / 2;
+            end = (end - 1) / 2;
+
+        }
+        return sum;
+    }
+}
