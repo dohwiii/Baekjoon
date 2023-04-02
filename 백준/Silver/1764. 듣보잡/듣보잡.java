69e9b6ba19201ad1817b6397543c62772e87d653
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-04-02 18:34:19 +0900

    [Silver IV] Title: 듣보잡, Time: 404 ms, Memory: 28932 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int N, M;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++)
+        {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        for (int i = 0; i < M; i++)
+        {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        int count = 0;
+        List<String> list = new ArrayList<>();
+        for (String key : map.keySet())
+        {
+            if (map.get(key) > 1)
+            {
+                count++;
+                list.add(key);
+            }
+        }
+        System.out.println(count);
+        Collections.sort(list);
+        for (String str : list)
+        {
+            System.out.println(str);
+        }
+
+        br.close();
+    }
+}
