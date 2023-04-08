ce34d3c84a79798407e1c31acd69b8b63d2e7be8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-04-08 15:04:39 +0900

    [Silver V] Title: 다리 놓기, Time: 164 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -12,14 +12,14 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            int[][] dp = new int[31][31];
+            int[][] dp = new int[M + 1][M + 1];
 
-            for (int i = 0; i <= 30; i++) {
+            for (int i = 0; i <= M; i++) {
                 dp[i][0] = 1;
                 dp[i][1] = i;
                 dp[i][i] = 1;
             }
-            for (int i = 0; i <= 30; i++) {
+            for (int i = 0; i <= M; i++) {
                 for (int j = 1; j < i; j++) {
                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
                 }
