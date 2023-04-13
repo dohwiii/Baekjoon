4e2ea2a16dde1c57c4ab580255d594af6e167e31
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-04-13 15:14:57 +0900

    [Silver III] Title: 이친수, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][] D = new long[N + 1][2];
+        D[1][0] = 0;
+        D[1][1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][0] + D[i - 1][1];
+            D[i][1] = D[i - 1][0];
+        }
+        System.out.println(D[N][0] + D[N][1]);
+    }
+}
