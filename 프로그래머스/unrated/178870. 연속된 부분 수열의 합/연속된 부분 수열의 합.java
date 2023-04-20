4b335d9e9fdea701fac5b7da36353236b3ce0faf
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-04-20 21:07:31 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.05 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int s = 0;
+        int e = 0;
+        int sum = sequence[0];
+        int[] answer = new int[2];
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        while(s <= e)
+        {
+            if (sequence[s] == k) {
+                list.add(new int[]{s, s});
+                break;
+            } else if (sequence[e] == k) {
+                list.add(new int[]{e, e});
+                break;
+            }
+            
+            if(sum == k)
+            {
+                list.add(new int[]{s, e});
+                e++;
+                if (e >= sequence.length) {
+                    break;
+                }
+                sum += sequence[e];
+            }
+            else if(sum < k)
+            {
+                e++;
+                if (e >= sequence.length) {
+                    break;
+                }
+                sum = sum + sequence[e];
+            }
+            else if(sum > k)
+            {
+                sum = sum - sequence[s];                
+                s++;
+                
+            }
+            
+        }
+        int min = Integer.MAX_VALUE;
+        for (int[] arr : list) {
+            int diff = arr[1] - arr[0];
+            min = Math.min(min, diff);
+        }
+        for (int[] arr : list) 
+        {
+            if (min == (arr[1] - arr[0])) {
+                answer[1] = arr[1];
+                answer[0] = arr[0];
+                break;
+            }
+        }
+        return answer;
+        
+    }
+}
