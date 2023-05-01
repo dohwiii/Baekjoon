8f29c7c9dad6a9fb9d4a9ebad8d013d4e2769ee0
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-05-01 21:30:38 +0900

    [level 3] Title: 여행경로, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    ArrayList<String> allRoute;
+    
+    public String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length]; //행 길이
+        allRoute = new ArrayList<>();
+        String[] answer = {};
+        
+        dfs("ICN", "ICN", tickets, 0);
+        Collections.sort(allRoute);
+        answer= allRoute.get(0).split(" ");
+        
+        return answer; 
+    }
+    public void dfs(String start, String route, String[][] tickets, int cnt)
+    {
+        if(cnt == tickets.length)
+        {
+            allRoute.add(route);
+            return;
+        }
+        for(int i=0; i<tickets.length; i++)
+        {
+            if(!visited[i] && tickets[i][0].equals(start))
+            {
+                visited[i] = true;
+                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
