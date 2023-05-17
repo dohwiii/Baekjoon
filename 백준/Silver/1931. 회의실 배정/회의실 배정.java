278acbf9fd657c20a5fa9cad6d6739e6f1ff22ae
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-05-17 12:42:22 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 43752 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,49 +1,42 @@
 import java.io.*;
-import java.sql.Time;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+//        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][2];
-
+        int[][] meetings = new int[N][2];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-
-
+            meetings[i][0] = Integer.parseInt(st.nextToken());
+            meetings[i][1] = Integer.parseInt(st.nextToken());
 
         }
-        Arrays.sort(arr, new Comparator<int[]>() {
+        Arrays.sort(meetings, new Comparator<int[]>() {
             @Override
-            public int compare(int[] s, int[] e) {
-                if (s[1] == e[1]) {
-                    return s[0] - e[0];
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
                 }
-                else
-                    return s[1] - e[1];
-
+                return o1[0] - o2[0];
             }
         });
+        int start = 0;
+        int end = 0;
         int count = 0;
-        int end = -1;
 
-        for (int i = 0; i < N; i++)
-        {
-            if (arr[i][0] >= end)
-            {
-                end = arr[i][1];
+        for (int[] now : meetings) {
+            if (end <= now[0]) { //새로운 회의
+                start = now[0];
+                end = now[1];
                 count++;
             }
-
+            else if (end > now[1]) {
+                start = now[0];
+                end = now[1];
+            }
         }
         System.out.println(count);
-
-
     }
-
 }
