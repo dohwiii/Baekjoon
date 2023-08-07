a2bdb4ff6be8425299ae61ee126b2beeb072423a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-07 15:46:14 +0900

    [Silver I] Title: 그림, Time: 528 ms, Memory: 44780 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int N, M;
+    static boolean[][] visited;
+    static int[][] map;
+    static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int area = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    bfs(i, j);
+                    area++;
+                }
+            }
+        }
+        System.out.println(area);
+        System.out.println(result);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(x, y));
+        visited[x][y] = true;
+        int cnt = 0;
+
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+            cnt++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        queue.add(new Pos(nx, ny));
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        result = Math.max(result, cnt);
+    }
+}
+
+class Pos {
+    int x, y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
