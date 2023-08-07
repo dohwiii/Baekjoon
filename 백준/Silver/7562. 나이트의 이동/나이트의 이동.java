756c41c63c2520fdc7c6638c04076efa8d38a75b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-07 21:25:57 +0900

    [Silver I] Title: 나이트의 이동, Time: 300 ms, Memory: 65852 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,59 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-import java.io.*;
-import java.util.*;
+public class Main
+{
+    static int[] dx = {-1, -1, 1, 1, -2, -2, 2, 2};
+    static int[] dy = {-2, 2, -2, 2, 1, -1, 1, -1};
+    static boolean[][] visited;
+    static int nowX, nowY;
+    static int destX, destY;
+    static int result;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int[] dx = {-2, -1, -2, -1, 1, 1, 2, 2,};
-        int[] dy = {-1, -2, 1, 2, -2, 2, -1, 1};
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine()); //크기
+        int T = Integer.parseInt(br.readLine()); //테스트 케이스 개수
+
+        for (int i = 0; i < T; i++)
+        {
+            int L = Integer.parseInt(br.readLine()); //체스판 크기 LxL
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            nowX = Integer.parseInt(st.nextToken());
+            nowY = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            int destX = Integer.parseInt(st.nextToken());
-            int destY = Integer.parseInt(st.nextToken());
-            Pos dest = new Pos(destX, destY, 0);
-            boolean[][] visited = new boolean[N][N];
-            int[][] map = new int[N][N];
-            Queue<Pos> queue = new LinkedList<>();
-            queue.add(new Pos(x, y, 0));
-            int result = 0;
+            destX = Integer.parseInt(st.nextToken());
+            destY = Integer.parseInt(st.nextToken());
+            visited = new boolean[L][L];
 
-            while (!queue.isEmpty()) {
-                Pos now = queue.poll();
-                if (now.x == dest.x && now.y == dest.y) {
-                    result = now.cnt;
-                    break;
-                }
-                for (int j = 0; j < 8; j++) {
-                    int nx = now.x + dx[j];
-                    int ny = now.y + dy[j];
-
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                        if (!visited[nx][ny]) {
-                            queue.add(new Pos(nx, ny, now.cnt + 1));
-                            visited[nx][ny] = true;
-                        }
-                    }
-                }
-            }
+            bfs(nowX, nowY, L);
             System.out.println(result);
-
         }
+
 
     }
 
+    public static void bfs(int x, int y, int L)
+    {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(x, y, 0));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty())
+        {
+            Pos now = queue.poll();
+            if (now.x == destX && now.y == destY)
+            {
+                result = now.cnt;
+                return;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx >= 0 && nx < L && ny >= 0 && ny < L)
+                {
+                    if (!visited[nx][ny])
+                    {
+                        queue.add(new Pos(nx, ny, now.cnt + 1));
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+
+    }
 }
 
-class Pos {
+class Pos
+{
     int x, y, cnt;
 
-    public Pos(int x, int y, int cnt) {
+    public Pos(int x, int y, int cnt)
+    {
         this.x = x;
         this.y = y;
         this.cnt = cnt;
