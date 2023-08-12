c1e478bc5ff3ec19e9f6b6b1ccfe58f1200366b2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-12 21:13:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 253 ms, Memory: 22,112 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,67 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, dishNum;
     static int[][] map;
-    static int[] dishA, dishB;
-    static boolean[] visited;
+    static int N;
+    static boolean[] isSelected;
     static int min;
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
-            StringBuilder sb = new StringBuilder();
             N = Integer.parseInt(br.readLine());
+            isSelected = new boolean[N];
             map = new int[N][N];
             min = Integer.MAX_VALUE;
-            dishNum = N / 2; //두개의 요리에 들어갈 재료의 개수
+
             for (int j = 0; j < N; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < N; k++) {
                     map[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
-            visited = new boolean[N];
-            pick(0, 0);
-            sb.append("#" + (i + 1) + " ");
-            sb.append(min);
-            System.out.println(sb);
-
+            makeIngredient(0, 0);
+            sb.append("#" + (i + 1)).append(" ").append(min).append("\n");
         }
+        System.out.println(sb.toString());
+
     }
 
-    public static void pick(int start, int cnt) {
-        //2/N개 뽑혔다면 끝
-        if (cnt == dishNum) {
-            cook();
-            return;
-        }
-        for (int i = start; i < N; i++) {
-            visited[i] = true;
-            pick(i + 1, cnt + 1);
-            visited[i] = false;
-        }
-    }
-
-    public static void cook() {
+    public static void findMin() {
         int A = 0;
         int B = 0;
+
         for (int i = 0; i < N - 1; i++) {
             for (int j = i + 1; j < N; j++) {
-                if (visited[i] && visited[j]) {
+                if (isSelected[i] && isSelected[j]) { //A 음식이라면
                     A += map[i][j] + map[j][i];
                 }
-                if (!visited[i] && !visited[j]) {
+                else if (!isSelected[i] && !isSelected[j]) { //B 음식
                     B += map[i][j] + map[j][i];
                 }
             }
         }
         min = Math.min(min, Math.abs(A - B));
+    }
+
+    public static void makeIngredient(int start, int cnt) {
+        if (cnt == N / 2) {
+            findMin();
+            return;
+
+        }
+        for (int i = start; i < N; i++) { //0,1,2,3
+            if (isSelected[i]) {
+                continue;
+            }
+            isSelected[i] = true;
+            makeIngredient(i + 1, cnt + 1);
+            isSelected[i] = false;
+        }
     }
 }
