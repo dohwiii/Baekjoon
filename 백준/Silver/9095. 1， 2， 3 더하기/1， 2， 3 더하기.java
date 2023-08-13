661ac8de32403711938feeb3e00881a6b5a70034
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-13 11:46:37 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 144 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,49 +3,35 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static boolean[] isSelected;
     static int[] number;
-    static int[] arr;
     static int ans;
-    static List<List<Integer>> list;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             N = Integer.parseInt(br.readLine());
-            isSelected = new boolean[4];
             number = new int[]{1, 2, 3};
-            arr = new int[N];
             ans = 0;
-            list = new ArrayList<>();
 
-            combination(0, 0,  new ArrayList<>());
+            combination(0, 0, new ArrayList<>());
             System.out.println(ans);
         }
-
-
-
-
     }
 
     public static void combination(int depth, int sum, List<Integer> numList) {
         if (sum == N) {
-//            System.out.println(numList);
             ans++;
             return;
         }
         if (depth == N) {
-//            if (sum == N) {
-//                ans++;
-//            }
             return;
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) { //number 배열 인덱스 (1, 2, 3 값 집어넣기용)
             numList.add(number[i]);
             combination(depth + 1, sum + number[i], numList);
-            numList.remove(numList.size() - 1);
+            numList.remove(numList.size() - 1); //마지막 넣은 원소 제거
         }
 
     }
-
 }
