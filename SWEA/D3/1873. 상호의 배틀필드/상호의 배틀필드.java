2cf13e77c4755e2be0c0b93fa201db0775094904
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-17 21:12:26 +0900

    [D3] Title: 상호의 배틀필드, Time: 110 ms, Memory: 18,360 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -45,8 +45,7 @@ public class Solution {
                 }
                 sb.append("\n");
             }
-
-        } 
+        }
         System.out.println(sb.toString());
 
 
@@ -54,99 +53,103 @@ public class Solution {
 
     public static void game(String command) {
         for (int i = 0; i < command.length(); i++) {
-            if (command.charAt(i) == 'S') { //포탄
-                int nowDir = tank.dir;
-                int tempR = dx[nowDir];
-                int tempC = dy[nowDir];
-                while (tank.x + tempR >= 0 && tank.x + tempR < H && tank.y + tempC >= 0 && tank.y + tempC < W) {
-                    if (map[tank.x + tempR][tank.y + tempC] == '*') { //벽돌 벽
-                        map[tank.x + tempR][tank.y + tempC] = '.'; //벽 뿌시기 -> 평지 만들기
-                        break;
-                    } else if (map[tank.x + tempR][tank.y + tempC] == '#') {
-                        break;
-                    } else { //평지면 전진 가능
-                        if (tempR > 0) {
-                            tempR++;
-                        } else if (tempR < 0) {
-                            tempR--;
-                        }
-                        if (tempC > 0) {
-                            tempC++;
-                        } else if (tempC < 0) {
-                            tempC--;
-                        }
-                    }
-                }
-            } else if (command.charAt(i) == 'U') {
-                int nx = tank.x + dx[1];
-                int ny = tank.y + dy[1];
-                int nDir = 1;
-                map[tank.x][tank.y] = '^'; //모양 복제
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
-                    if (map[nx][ny] == '.') { //평지
-                        map[nx][ny] = '^'; //모양 복제
-                        map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
-                        tank = new Pos(nx, ny, nDir);
-                    } else {
-                        tank = new Pos(tank.x, tank.y, nDir);
-                    }
-                }else {
-                    tank = new Pos(tank.x, tank.y, nDir);
-                }
-            } else if (command.charAt(i) == 'D') {
-                int nx = tank.x + dx[0];
-                int ny = tank.y + dy[0];
-                int nDir = 0;
-                map[tank.x][tank.y] = 'v'; //모양 복제
+            commandExecute(command, i);
+        }
+    }
 
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
-                    if (map[nx][ny] == '.') { //평지
-                        map[nx][ny] = 'v'; //모양 복제
-                        map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
-                        tank = new Pos(nx, ny, nDir);
-                    } else {
-                        tank = new Pos(tank.x, tank.y, nDir);
+    private static void commandExecute(String command, int i) {
+        if (command.charAt(i) == 'S') { //포탄
+            int nowDir = tank.dir;
+            int tempR = dx[nowDir];
+            int tempC = dy[nowDir];
+            while (tank.x + tempR >= 0 && tank.x + tempR < H && tank.y + tempC >= 0 && tank.y + tempC < W) {
+                if (map[tank.x + tempR][tank.y + tempC] == '*') { //벽돌 벽
+                    map[tank.x + tempR][tank.y + tempC] = '.'; //벽 뿌시기 -> 평지 만들기
+                    break;
+                } else if (map[tank.x + tempR][tank.y + tempC] == '#') { //강철 벽 만나면 아무일 일어나지 않음
+                    break;
+                } else { //평지 or 물 전진 가능
+                    if (tempR > 0) {
+                        tempR++;
+                    } else if (tempR < 0) {
+                        tempR--;
                     }
+                    if (tempC > 0) {
+                        tempC++;
+                    } else if (tempC < 0) {
+                        tempC--;
+                    }
+                }
+            }
+        } else if (command.charAt(i) == 'U') {
+            int nx = tank.x + dx[1];
+            int ny = tank.y + dy[1];
+            int nDir = 1;
+            map[tank.x][tank.y] = '^'; //방향 전환
+            
+            if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+                if (map[nx][ny] == '.') { //평지
+                    map[nx][ny] = '^'; //모양 복제
+                    map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
+                    tank = new Pos(nx, ny, nDir);
                 } else {
                     tank = new Pos(tank.x, tank.y, nDir);
                 }
+            }else {
+                tank = new Pos(tank.x, tank.y, nDir);
+            }
+        } else if (command.charAt(i) == 'D') {
+            int nx = tank.x + dx[0];
+            int ny = tank.y + dy[0];
+            int nDir = 0;
+            map[tank.x][tank.y] = 'v'; //방향 전환
 
-            } else if (command.charAt(i) == 'L') {
-                int nx = tank.x + dx[3];
-                int ny = tank.y + dy[3];
-                int nDir = 3;
-                map[tank.x][tank.y] = '<'; //모양 복제
-
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
-                    if (map[nx][ny] == '.') { //평지
-                        map[nx][ny] = '<'; //모양 복제
-                        map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
-                        tank = new Pos(nx, ny, nDir);
-                    } else {
-                        tank = new Pos(tank.x, tank.y, nDir);
-                    }
+            if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+                if (map[nx][ny] == '.') { //평지
+                    map[nx][ny] = 'v'; //모양 복제
+                    map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
+                    tank = new Pos(nx, ny, nDir);
                 } else {
                     tank = new Pos(tank.x, tank.y, nDir);
                 }
+            } else {
+                tank = new Pos(tank.x, tank.y, nDir);
+            }
 
-            } else if (command.charAt(i) == 'R') {
-                int nx = tank.x + dx[2];
-                int ny = tank.y + dy[2];
-                int nDir = 2;
-                map[tank.x][tank.y] = '>'; //모양 복제
-
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
-                    if (map[nx][ny] == '.') { //평지
-                        map[nx][ny] = '>'; //모양 복제
-                        map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
-                        tank = new Pos(nx, ny, nDir);
-                    } else {
-                        tank = new Pos(tank.x, tank.y, nDir);
-                    }
+        } else if (command.charAt(i) == 'L') {
+            int nx = tank.x + dx[3];
+            int ny = tank.y + dy[3];
+            int nDir = 3;
+            map[tank.x][tank.y] = '<'; //방향 전환
+            
+            if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+                if (map[nx][ny] == '.') { //평지
+                    map[nx][ny] = '<'; //모양 복제
+                    map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
+                    tank = new Pos(nx, ny, nDir);
                 } else {
                     tank = new Pos(tank.x, tank.y, nDir);
                 }
+            } else {
+                tank = new Pos(tank.x, tank.y, nDir);
+            }
 
+        } else if (command.charAt(i) == 'R') {
+            int nx = tank.x + dx[2];
+            int ny = tank.y + dy[2];
+            int nDir = 2;
+            map[tank.x][tank.y] = '>'; //방향 전환
+            
+            if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+                if (map[nx][ny] == '.') { //평지
+                    map[nx][ny] = '>'; //모양 복제
+                    map[tank.x][tank.y] = '.'; //탱크 이동했으니까 빈칸
+                    tank = new Pos(nx, ny, nDir);
+                } else {
+                    tank = new Pos(tank.x, tank.y, nDir);
+                }
+            } else {
+                tank = new Pos(tank.x, tank.y, nDir);
             }
         }
     }
@@ -160,5 +163,4 @@ class Pos {
         this.y = y;
         this.dir = dir;
     }
-
 }
