8ef7a46ec57be32accf54d8ec237276b1f49a1c2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-20 19:00:25 +0900

    [Silver III] Title: 주유소, Time: 344 ms, Memory: 30400 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,58 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //도시 개수
+        int minMoney = 0; //최소금액
+        int[] distances = new int[N - 1];
+        int[] oilMoney = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            distances[i] = Integer.parseInt(st.nextToken()); //도시 길이
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            oilMoney[i] = Integer.parseInt(st.nextToken()); //리터당 가격
+        }
+        int money = 0;
+        int nowOilMoney = oilMoney[0];
+        for (int i = 0; i < N - 1; i++) {
+            int dis = distances[i]; //현재 도로 거리
+
+            //현재 오일가격 * 거리 < 다음 오일 가격 * 거리
+            if (nowOilMoney * dis > oilMoney[i] * dis) {
+                nowOilMoney = oilMoney[i];
+                money += oilMoney[i] * dis;
+            } else {
+                money += nowOilMoney * dis;
+            }
+        }
+        System.out.println(money);
+
+    }
+}
+
+class Time implements Comparable<Time> {
+    int start, end;
+
+    public Time(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(Time o) {
+        if (this.end == o.end) {
+            return this.start - o.start;
+        }
+        return this.end - o.end;
+    }
+}
