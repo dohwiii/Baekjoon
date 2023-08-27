0fb27340e8890ca13d8e305eaa7169ad1e0799a8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-27 15:07:42 +0900

    [D3] Title: 퍼펙트 셔플, Time: 117 ms, Memory: 26,360 KB -BaekjoonHub

---
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution
+{
+    static int N;
+    static String[] cards;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++)
+        {
+            N = Integer.parseInt(br.readLine()); //카드 수
+            cards = new String[N]; //카드
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++)
+            {
+                cards[i] = st.nextToken();
+            }
+            int halfN = cards.length / 2;
+            Queue<String> queue = new ArrayDeque<>();
+
+            //포인터
+            int p1 = 0;
+            int p2 = cards.length - halfN;
+            sb.append("#" + (t + 1) + " ");
+
+            while (p1 < cards.length - halfN)
+            {
+                sb.append(cards[p1] + " ");
+                p1++;
+
+                if (p2 <= N - 1)
+                {
+                    sb.append(cards[p2] + " ");
+                    p2++;
+                }
+            }
+
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+}
