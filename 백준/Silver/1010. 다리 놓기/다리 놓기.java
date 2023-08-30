e1e51871c39a309d6c37cfbd2878363c3e29b06b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-30 15:39:13 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 13492 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -4,22 +4,25 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+    static int N;
+    static int[][] dp;
 
-        while (T-- > 0) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for (int t = 0; t < N; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int[][] dp = new int[M + 1][M + 1];
+            int M = Integer.parseInt(st.nextToken()); //M개 중에서 N개를 뽑아야 함
+            dp = new int[M + 1][M + 1];
 
-            for (int i = 0; i <= M; i++) {
+            for (int i = 1; i <= M; i++) {
                 dp[i][0] = 1;
-                dp[i][1] = i;
                 dp[i][i] = 1;
+                dp[i][1] = i;
             }
-            for (int i = 0; i <= M; i++) {
+            for (int i = 2; i <= M; i++) {
                 for (int j = 1; j < i; j++) {
                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
                 }
