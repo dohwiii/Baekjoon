d07d855493ac132e89e7f05985c28b74ced59a25
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-08-30 21:25:56 +0900

    [Silver II] Title: 외판원 순회 2, Time: 112 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,73 +1,55 @@
-
 import org.w3c.dom.Node;
 
-import javax.xml.crypto.Data;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class Main
-{
-    static int[][] money;
+public class Main {
     static int N;
+    static int[][] money;
+    static int min;
     static boolean[] visited;
-    static int answer = Integer.MAX_VALUE;
 
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException
-    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        money = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
+        money = new int[N][N];
+        visited = new boolean[N];
 
-        for (int i = 1; i <= N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++)
-            {
+            for (int j = 0; j < N; j++) {
                 money[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 1; i <= N; i++)
-        {
+        min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
             visited[i] = true;
-            DFS(i, i, 0, 0);
-
+            travel(i, i, 0, 0);
         }
-        System.out.println(answer);
+        System.out.println(min);
 
     }
 
-    public static void DFS(int start, int now, int sum, int cnt)
-    {
-        if (cnt == N - 1)
-        {
-            if (money[now][start] > 0)
-            {
-                sum = sum + money[now][start];
+    public static void travel(int start, int now, int sum, int depth) {
 
-                if (sum < answer)
-                {
-                    answer = sum;
-                }
+        if (depth == N - 1) {
+            if (money[now][start] > 0) {
+                sum += money[now][start];
+                min = Math.min(min, sum);
             }
             return;
         }
 
-        for (int i = 1; i <= N; i++)
-        {
-            if (!visited[i] && money[now][i] > 0)
-            {
+        for (int i = 0; i < N; i++) {
+            if (!visited[i] && money[now][i] > 0) {
                 visited[i] = true;
-                DFS(start, i, sum + money[now][i], cnt + 1);
+                travel(start, i, sum + money[now][i], depth + 1);
                 visited[i] = false;
-
             }
         }
-
-
     }
 }
-
-
