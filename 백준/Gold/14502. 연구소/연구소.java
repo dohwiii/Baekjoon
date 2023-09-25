59bd496a8947b770e238cf41e3a8607b8034beaf
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-09-25 13:48:09 +0900

    [Gold IV] Title: 연구소, Time: 804 ms, Memory: 294408 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,99 +1,108 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int N, M;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1}; //동 서 북 남
-    static int[] dy = {1, -1, 0, 0,};
-    static ArrayList<Pos> virusList;
     static int max = Integer.MIN_VALUE;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static List<Pos> virus = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
         visited = new boolean[N][M];
-        virusList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) {
-                    virusList.add(new Pos(i, j)); //바이러스 위치
+
+                if (map[i][j] == 2) { //바이러스
+                    virus.add(new Pos(i, j));
                 }
             }
+
         }
-        setWall(0);
+        makeWall(0);
         System.out.println(max);
     }
 
-    public static void setWall(int wallCnt) {
+    public static void makeWall(int wallCnt) {
+        //벽이 3개 세워졌다면
         if (wallCnt == 3) {
-            virusBFS();
+            bfs();
             return;
         }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = 1;
-                    setWall(wallCnt + 1);
-                    map[i][j] = 0;
+                if (map[i][j] == 0) { //빈칸이라면
+                    map[i][j] = 1; //벽 세우기
+                    makeWall(wallCnt + 1);
+                    map[i][j] = 0; //벽 허물기
 
                 }
             }
         }
     }
 
-    public static void virusBFS() {
-        Queue<Pos> queue = new LinkedList<>();
-        int[][] copy = new int[N][M];
+    public static void bfs() {
+        Queue<Pos> queue = new ArrayDeque<>();
+        visited = new boolean[N][M];
+        int room = 0;
 
-        for (int i = 0; i < N; i++) {
-            copy[i] = map[i].clone();
-        }
-        for (int i = 0; i < virusList.size(); i++) {
-            Pos v = virusList.get(i);
-            queue.add(new Pos(v.x, v.y));
+        for (Pos wall : virus) {
+            queue.add(new Pos(wall.x, wall.y));
 
             while (!queue.isEmpty()) {
                 Pos now = queue.poll();
+                room++;
 
-                for (int j = 0; j < 4; j++) {
-                    int nx = now.x + dx[j];
-                    int ny = now.y + dy[j];
+                for (int i = 0; i < 4; i++) {
+                    int nx = now.x + dx[i];
+                    int ny = now.y + dy[i];
 
                     if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                        if (!visited[nx][ny] && copy[nx][ny] == 0) {
+                        if (map[nx][ny] == 0 && !visited[nx][ny]) {
                             visited[nx][ny] = true;
-                            copy[nx][ny] = 2;
                             queue.add(new Pos(nx, ny));
-                            visited[nx][ny]=false;
                         }
                     }
                 }
             }
         }
-        int count = 0;
+
+        int safe = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (copy[i][j] == 0) {
-                    count++;
+                if (!visited[i][j] && map[i][j] == 0) {
+                    safe++;
                 }
             }
         }
-        max = Math.max(max, count);
+        max = Math.max(max, safe);
+
+
     }
+
+
 }
 
 class Pos {
     int x, y;
+
     public Pos(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
 }
