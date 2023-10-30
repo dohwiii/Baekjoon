54bbf22c50262189c696658e0e942e655e3edfc8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-10-30 16:33:44 +0900

    [Silver I] Title: 그림, Time: 384 ms, Memory: 47516 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,20 +1,21 @@
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int N, M;
-    static boolean[][] visited;
-    static int[][] map;
-    static int result = 0;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
@@ -26,29 +27,29 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int area = 0;
+        int picCnt = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (!visited[i][j] && map[i][j] == 1) {
                     bfs(i, j);
-                    area++;
+                    picCnt++;
                 }
             }
         }
-        System.out.println(area);
-        System.out.println(result);
+        System.out.println(picCnt);
+        System.out.println(max);
+
     }
 
     public static void bfs(int x, int y) {
-        Queue<Pos> queue = new LinkedList<>();
+        Queue<Pos> queue = new ArrayDeque<>();
         queue.add(new Pos(x, y));
         visited[x][y] = true;
-        int cnt = 0;
+        int area = 0;
 
         while (!queue.isEmpty()) {
             Pos now = queue.poll();
-            cnt++;
-
+            area++; //면적
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
@@ -61,7 +62,8 @@ public class Main {
                 }
             }
         }
-        result = Math.max(result, cnt);
+        max = Math.max(max, area);
+
     }
 }
 
