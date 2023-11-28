9767e77afd0919c4cb37b740c793d9e0c1ac82d4
dohwiii <102406112+dohwiii@users.noreply.github.com>
2023-11-28 20:46:20 +0900

    [Gold IV] Title: 빙산, Time: 612 ms, Memory: 250216 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,150 +6,136 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[][] iceberg;
-    static boolean[][] visited;
+    static int[][] map;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int[][] water;
+    static boolean[][] visited;
+    static int[][] seawater;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        iceberg = new int[N][M];
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        seawater = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                iceberg[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         int year = 0;
+        while (true) {
 
-        while (true)
-        {
-            int count = 0;
-            boolean melt = true;
-            visited = new boolean[N][M];
-            water = new int[N][M];
+            int[][] temp = new int[N][M]; //임시배열
 
-            //인접한 바닷물의 개수 water 배열 채우기
+            for (int i = 0; i < N; i++) {
+                temp[i] = map[i].clone();
+            }
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (!visited[i][j] && iceberg[i][j] != 0) {
-                        countAndSubtract(i, j);
-                        count++;
+                    if (temp[i][j] != 0) { //빙산
+                        int x = i;
+                        int y = j;
+                        int ice = 0;
+
+                        //바닷물 개수
+                        for (int k = 0; k < 4; k++) {
+                            int nx = x + dx[k];
+                            int ny = y + dy[k];
+
+                            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                                //아직 방문하지 않았고, 빙산이라면
+                                if (!visited[nx][ny] && temp[nx][ny] == 0) {
+                                    ice++;
+                                }
+                            }
+                        }
+//                        seawater[i][j] = ice;
+
+                        if (map[i][j] - ice < 0) {
+                            map[i][j] = 0;
+                        } else {
+                            map[i][j] -= ice;
+                        }
+
                     }
-                }
-            }
-            if (count >= 2) {
-                System.out.println(year);
-                break;
-            }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (iceberg[i][j] - water[i][j] < 0) {
-                        iceberg[i][j] = 0;
-                    }
-                    else
-                        iceberg[i][j] = iceberg[i][j] - water[i][j];
                 }
             }
             year++;
 
+            visited = new boolean[N][M];
+            int icebergCnt = 0;
+            //이어진 빙산 개수 카운트
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (iceberg[i][j] != 0) {
-                        melt = false;
+                    //아직 방문하지 않았고, 빙산일 때
+                    if (map[i][j] != 0 && !visited[i][j]) {
+                        iceberg(i, j);
+                        icebergCnt++;
                     }
                 }
             }
-            if (melt && count < 2) {
+            //빙산이 두 덩어리 이상 분리되는 최초의 시간
+            if (icebergCnt > 1) {
+                System.out.println(year);
+                break;
+            } else if (icebergCnt == 0) {
                 System.out.println(0);
                 break;
             }
 
+
         }
+
     }
-    public static void countAndSubtract(int x, int y) {
-        Queue<Pos> queue = new LinkedList<>();
-        visited[x][y] = true;
+
+    public static boolean isNotSeparated() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //주위 바닷물 seawater 배열에 넣고 뺀 후 빙산의 개수
+    public static void iceberg(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
         queue.add(new Pos(x, y));
+        visited[x][y] = true;
 
         while (!queue.isEmpty()) {
             Pos now = queue.poll();
-            int waterCount = 0;
 
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
                 if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (!visited[nx][ny]) {
-                        if (iceberg[nx][ny] == 0) {
-                            waterCount++;
-                        } else {
-                            queue.add(new Pos(nx, ny));
-                            visited[nx][ny] = true;
-                        }
+                    //아직 방문하지 않았고, 빙산이라면
+                    if (!visited[nx][ny] && map[nx][ny] != 0) {
+                        queue.add(new Pos(nx, ny));
+                        visited[nx][ny] = true;
                     }
                 }
             }
-            water[now.x][now.y] = waterCount;
         }
     }
 
-
-//    public static void BFS(int x, int y) {
-//        Queue<Pos> queue = new LinkedList<>();
-//        visited[x][y] = true;
-//        queue.add(new Pos(x, y));
-//
-//        while (!queue.isEmpty()) {
-//            Pos now = queue.poll();
-//            int count = 0;
-//
-//            for (int i = 0; i < 4; i++)
-//            {
-//                int nx = now.x + dx[i];
-//                int ny = now.y + dy[i];
-//
-//                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-//                    if (!visited[nx][ny] && iceberg[nx][ny] == 0) {
-//                        count++;
-//                    }
-//                }
-//            }
-//            water[now.x][now.y] = count;
-//        }
-//    }
-//    public static void chunk(int x, int y) {
-//        Queue<Pos> queue = new LinkedList<>();
-//        visited[x][y] = true;
-//        queue.add(new Pos(x, y));
-//
-//        while (!queue.isEmpty()) {
-//            Pos now = queue.poll();
-//
-//            for (int i = 0; i < 4; i++)
-//            {
-//                int nx = now.x + dx[i];
-//                int ny = now.y + dy[i];
-//
-//                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-//                    if (!visited[nx][ny] && iceberg[nx][ny] != 0) {
-//                        queue.add(new Pos(nx, ny));
-//                        visited[nx][ny] = true;
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
 
 class Pos {
     int x, y;
+
     public Pos(int x, int y) {
         this.x = x;
         this.y = y;
