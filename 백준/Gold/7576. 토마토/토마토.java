85c6a1c3bdbeb414f5b693861feba79060b90933
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-06-13 19:43:06 +0900

    [Gold V] Title: 토마토, Time: 600 ms, Memory: 101488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,98 +1,88 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
 
-class Main
-{
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
     static int N, M;
     static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static boolean[][] visited;
-    static Queue<Coordinate> tomatoQueue = new LinkedList<>();
+    static Queue<Pos> tomatoQueue = new ArrayDeque<>();
 
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        visited = new boolean[N][M];
         map = new int[N][M];
+        visited = new boolean[N][M];
 
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++)
-            {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1)
-                {
-                    tomatoQueue.add(new Coordinate(i, j));
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    tomatoQueue.offer(new Pos(i, j, 0));
+
                 }
             }
         }
-        bfs();
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-                if (map[i][j] == 0)
-                {
-                    System.out.println(-1);
-                    return;
-                }
-                
-            }
-        }
-        int max = Arrays.stream(map).flatMapToInt(s->Arrays.stream(s)).max().getAsInt();
-        if (max == 1)
-        {
-            System.out.println(0);
-        }
-        else
-        {
-            System.out.println(max - 1);
-        }
-        return;
+        System.out.println(tomato());
 
     }
+    // 1 - 익은 토마토
+    // 0 - 익지 않은 토마토
+    // -1 - 토마토 없음
+    public static int tomato() {
+        int max = 0;
 
-    public static void bfs()
-    {
+        while (!tomatoQueue.isEmpty()) {
+            Pos now = tomatoQueue.poll();
+            max = Math.max(max, now.day);
 
-        while (!tomatoQueue.isEmpty())
-        {
-            Coordinate now = tomatoQueue.poll();
-
-            for(int i=0;i<4;i++)
-            {
+            for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M)
-                {
-                    if (map[nx][ny] == 0)
-                    {
-                        tomatoQueue.add(new Coordinate(nx, ny));
-                        map[nx][ny] = map[now.x][now.y] + 1;
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (!visited[nx][ny] && map[nx][ny] == 0) {
+                        tomatoQueue.offer(new Pos(nx, ny, now.day + 1));
+                        visited[nx][ny] = true;
+                        map[nx][ny] = now.day + 1;
                     }
                 }
             }
         }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    max = -1;
+                    break;
+                }
+            }
+            if (max == -1) {
+                break;
+            }
+        }
+        return max;
     }
 }
 
-class Coordinate
-{
-    int x;
-    int y;
+class Pos {
+    int x, y, day;
 
-    public Coordinate(int x, int y)
-    {
+    public Pos(int x, int y, int day) {
         this.x = x;
         this.y = y;
+        this.day = day;
     }
 }
