3b48bbedfcaee613226f01c5b212bd8005c21127
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-06-27 19:40:26 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 364 ms, Memory: 88952 KB -BaekjoonHub

--- 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.java
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.java
@@ -1,7 +1,5 @@
 import java.io.*;
 import java.math.BigDecimal;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,26 +10,26 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());   //테스트까지 남은 시간
         int L = Integer.parseInt(st.nextToken());   //알코올 지속시간
-        double[] alcohol = new double[N + 1];
-        Queue<Integer> queue = new ArrayDeque<>();
-        long sum = 0;
-        int cnt = 0;
+        int[] alcohol = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             int input = Integer.parseInt(st.nextToken());
-            queue.offer(input);
+            alcohol[i] = alcohol[i - 1] + input;
+        }
+        int cnt = 0;
 
-            sum += input;
-            if (queue.size() > L) {
-                sum -= queue.poll();
+        // 알코올 농도 체크
+        for (int i = 1; i <= N; i++) {
+            int density = alcohol[i];
+            if (i > L) {
+                density = alcohol[i] - alcohol[i - L];
+
             }
-
-            if (sum >= 129 && sum <= 138) {
+            if (density >= 129 && density <= 138) {
                 cnt++;
             }
         }
-
         System.out.println(cnt);
 
     }
