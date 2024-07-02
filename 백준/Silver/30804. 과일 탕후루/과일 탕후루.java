da84e80ba8f63c296f382eff8c0c8df8cf13c377
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-02 13:50:11 +0900

    [Silver II] Title: 과일 탕후루, Time: 128 ms, Memory: 18228 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,60 +1,48 @@
-
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int[] num = new int[10]; ;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String line = br.readLine();
+        int[] fruits = new int[n];
 
-        int N = Integer.parseInt(br.readLine());
-        int[] fruits = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            fruits[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            fruits[i] = line.charAt(i * 2) - '0';
         }
 
         int left = 0;
         int right = 0;
-        int result = 0;
+        int[] cnt = new int[10];
+        int kindOfFruits = 0;
+        int maxLen = 0;
 
-        while (left < N) {
-            while (right < N) {
-                num[fruits[right]]++;
-                right++;
+        while (right < n) {
+            cnt[fruits[right]]++;
 
-                //종류가 2개 이상이라면
-                if (!checkCnt()) {
-                    right--;
-                    num[fruits[right]]--;
+            if (cnt[fruits[right]] == 1) {
+                kindOfFruits++;
 
-                    break;
+                while (kindOfFruits == 3) {
+                    cnt[fruits[left]]--;
+
+                    if (cnt[fruits[left]] == 0) {
+                        kindOfFruits--;
+                    }
+
+                    left++;
                 }
             }
-            result = Math.max(result, right - left);
-            num[fruits[left]]--;
-            left++;
+
+            right++;
+            int curLen = right - left;
+            maxLen = curLen > maxLen ? curLen : maxLen;
         }
-        System.out.println(result);
 
-
+        System.out.println(maxLen);
     }
-
-    public static boolean checkCnt() {
-        int cnt = 0;
-
-        for (int i = 0; i < 10; i++) {
-            if (num[i] > 0) {
-                cnt++;
-            }
-            if (cnt > 2) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 
 }
