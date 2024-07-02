e96c83586ffd4e887e3f2eafe159eaa75bb37522
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-02 12:48:58 +0900

    [Gold V] Title: 문자열 게임 2, Time: 144 ms, Memory: 19648 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -7,56 +7,57 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
         int T = Integer.parseInt(br.readLine());    //게임 진행 횟수
 
-        for (int i = 0; i < T; i++) {
-            Map<Character, Integer> cntHm = new HashMap<>();    //몇번 나왔는지 카운트
-
-            String str = br.readLine();
-            int len = str.length();
+        for (int t = 0; t < T; t++) {
+            char[] word = br.readLine().toCharArray();
             int K = Integer.parseInt(br.readLine());
+            
+            int[] cntArr = new int[26]; //알파벳 빈도 체크배열
+            int[] prevK = new int[26];
+            int[] next = new int[word.length];
+            int[] prev = new int[26];
+            int max = -1;
+            int min = Integer.MAX_VALUE;
+
             if (K == 1) {
-                System.out.println("1 1");
+                bw.write("1 1\n");
                 continue;
             }
 
-            for (int j = 0; j < str.length(); j++) {
-                if (cntHm.containsKey(str.charAt(j))) {
-                    cntHm.put(str.charAt(j), cntHm.get(str.charAt(j)) + 1);
+            for (int i = 0; i < word.length; i++) {
+                int index = word[i] - 'a';
+                cntArr[index]++;
+
+                if (cntArr[index] == 1) {   //처음 나오는 알파벳
+                    prevK[index] = i;
+                } else if (cntArr[index] >= 2) {
+                    next[prev[index]] = i;  //지금 알파벳의 이전 위치의 인덱스 값 현재로 갱신
                 }
-                else {
-                    cntHm.put(str.charAt(j), 1);
+                prev[index] = i;    //현재 위치로 갱신
+
+                //K개를 포함한다면
+                if (cntArr[index] == K) {
+                    max = Math.max(max, i - prevK[index] + 1);
+                    min = Math.min(min, i - prevK[index] + 1);
                 }
-            }
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for (int j = 0; j < len; j++) {
-                if (cntHm.get(str.charAt(j)) < K) {
-                    continue;
-                }
-                int cnt = 1;
-                for (int k = j + 1; k < len; k++) {
-                    if (str.charAt(j) == str.charAt(k)) {
-                        cnt++;
-                    }
-                    if (cnt == K) {
-                        max = Math.max(max, k - j + 1);
-                        min = Math.min(min, k - j + 1);
-                        break;
-                    }
+                else if (cntArr[index] > K) {
+                    prevK[index] = next[prevK[index]];
+                    max = Math.max(max, i - prevK[index] + 1);
+                    min = Math.min(min, i - prevK[index] + 1);
                 }
             }
-            if (max == Integer.MIN_VALUE || min == Integer.MAX_VALUE) {
-                System.out.println(-1);
+            if (max == -1 || min == Integer.MAX_VALUE) {
+                bw.write("-1");
+                bw.write("\n");
                 continue;
             }
-            System.out.println(min + " " + max);
+            bw.write(min + " " + max);
+            bw.write("\n");
 
         }
 
-
-
+        bw.flush();
 
     }
 }
