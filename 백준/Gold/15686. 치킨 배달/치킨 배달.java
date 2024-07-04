434629faa3e41f546aefaf0b74a01d948c091c5e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-04 16:06:01 +0900

    [Gold V] Title: 치킨 배달, Time: 164 ms, Memory: 13852 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -4,68 +4,67 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[][] map;
-    static ArrayList<Pos> chickenList;
-    static ArrayList<Pos> homeList;
-    static ArrayList<Pos> choice = new ArrayList<>();
-    static boolean[] visitedChicken;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int result = Integer.MAX_VALUE;
+
+    static List<Pos> chickenList = new ArrayList<>();
+    static List<Pos> homeList = new ArrayList<>();
+    static int minDistance = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());   //도시 개수 NxN
+        M = Integer.parseInt(st.nextToken());   //치킨집 최대 개수
         map = new int[N][N];
-        chickenList = new ArrayList<>();
-        homeList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) {
+                if (map[i][j] == 2) {
+                    chickenList.add(new Pos(i, j)); //치킨집 인덱스 저장
+                } else if (map[i][j] == 1) {
                     homeList.add(new Pos(i, j));
                 }
-                if (map[i][j] == 2) { //치킨집 정보저장
-                    chickenList.add(new Pos(i, j));
-                }
             }
         }
-        visitedChicken = new boolean[chickenList.size()];
-        DFS(0, 0);
-        System.out.println(result);
+        boolean[] visited = new boolean[chickenList.size()];
 
+        combination(visited, 0, chickenList.size(), M);
+
+        System.out.println(minDistance);
     }
 
-    public static void DFS(int start, int depth) {
-        if (depth == M)
-        {
-            int sum = 0;
-            for (Pos home : homeList) {
-                int min = Integer.MAX_VALUE;
-                for (Pos store : choice) {
-                    int distance = Math.abs(home.x - store.x) + Math.abs(home.y - store.y);
-                    min = Math.min(min, distance);
-                }
-                sum += min;
-            }
-            result = Math.min(result, sum);
+    public static void combination(boolean[] visited, int start, int n, int r) {
+        if (r == 0) {
+            minDistance = Math.min(minDistance, calculate(visited));
             return;
         }
-
         for (int i = start; i < chickenList.size(); i++) {
-            if (!visitedChicken[i]) {
-                visitedChicken[i] = true;
-                choice.add(chickenList.get(i));
-                DFS(i + 1, depth + 1);
-                choice.remove(chickenList.get(i));
-                visitedChicken[i] = false;
-            }
+            visited[i] = true;
+            combination(visited, i + 1, n, r - 1);
+            visited[i] = false;
         }
     }
 
+    public static int calculate(boolean[] visited) {
+        int distance = 0;
+
+        for (Pos home : homeList) {
+            int minTemp = Integer.MAX_VALUE;
+
+            for (int i = 0; i < chickenList.size(); i++) {
+                if (visited[i]) {
+                    Pos chic = chickenList.get(i);   //치킨집 주소
+                    minTemp = Math.min(minTemp, Math.abs(home.x - chic.x) + Math.abs(home.y - chic.y));
+                }
+            }
+            distance += minTemp;
+        }
+
+        return distance;
+
+    }
 }
 
 class Pos {
