4acfe204eb13c0e74360ec0ee3ba9c252dd74097
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-04 14:13:44 +0900

    [Silver I] Title: 안전 영역, Time: 284 ms, Memory: 42024 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,89 +1,95 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
+    static int[][] map;
+    static int minHeight, N, maxHeight;
+    static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static boolean[][] visited;
-    static int[][] map;
-    static int N;
-    static boolean[] mapNum;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         visited = new boolean[N][N];
-        mapNum = new boolean[101];
-        int maxHeight = 0;
+        minHeight = 101;
+        maxHeight = 0;
+        int maxArea = 1;
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++)
-            {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] > maxHeight) {
-                    maxHeight = map[i][j];
-                }
+                minHeight = minHeight > map[i][j] ? map[i][j] : minHeight;
+                maxHeight = maxHeight < map[i][j] ? map[i][j] : maxHeight;
             }
         }
-        int count = 0;
-        int max = 0;
-        for (int height = 0; height < maxHeight+ 1; height++)
-        {
-            count = 0;
+
+
+        while (minHeight < maxHeight) {
             visited = new boolean[N][N];
+            int[][] cloneMap = new int[N][N];
+            int area = 0;
 
-            for (int j = 0; j < N; j++)
-            {
-                for (int k = 0; k < N; k++)
-                {
-                    if (!visited[j][k] && map[j][k] > height)
-                    {
-                        count++;
-                        DFS(j, k, height);
+            for (int i = 0; i < N; i++) {   //깊은 복사
+                cloneMap[i] = map[i].clone();
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    cloneMap[i][j] -= minHeight;
+                    if (cloneMap[i][j] < 0) {
+                        cloneMap[i][j] = 0;
                     }
-
                 }
             }
-            max = Math.max(max, count);
-
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (cloneMap[i][j] != 0 && !visited[i][j]) {
+                        bfs(i, j, cloneMap);
+                        area++;
+                    }
+                }
+            }
+            maxArea = Math.max(maxArea, area);
+            minHeight++;
         }
-        System.out.println(max);
 
+        System.out.println(maxArea);
     }
 
-
-    public static void DFS(int x, int y, int height)
-    {
-        if (visited[x][y]) {
-            return;
-        }
+    public static void bfs(int x, int y, int[][] cloneMap) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.add(new Pos(x, y));
         visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++)
-        {
-            int x1 = x + dx[i];
-            int y1 = y + dy[i];
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
 
-            if (x1 >= 0 && y1 >= 0 && x1 < N && y1 < N)
-            {
-                if (map[x1][y1] > height && !visited[x1][y1])
-                {
-                    DFS(x1, y1, height);
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (!visited[nx][ny] && cloneMap[nx][ny] != 0) {
+                        queue.offer(new Pos(nx, ny));
+                        visited[nx][ny] = true;
+                    }
                 }
-
             }
-
         }
-
     }
 
+}
+
+class Pos {
+    int x, y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
