159f41761351c0b1fae0e7175015a54d654fa70b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-06 14:59:08 +0900

    [Gold IV] Title: 불, Time: 444 ms, Memory: 54632 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -4,12 +4,9 @@ import java.util.*;
 public class Main {
     static int N, M;
     static char[][] map;
-    static boolean[][] fireVisited;
-    static boolean[][] personVisited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static Queue<Pos> fire;
-    static Queue<Pos> person;
+    static Queue<Pos> queue;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,10 +18,8 @@ public class Main {
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             map = new char[N][M];
-            fireVisited = new boolean[N][M];
-            personVisited = new boolean[N][M];
-            fire = new ArrayDeque<>();
-            person = new ArrayDeque<>();
+            queue = new ArrayDeque<>();
+            int x = 0, y = 0;
 
             for (int i = 0; i < N; i++) {
                 String str = br.readLine();
@@ -32,14 +27,14 @@ public class Main {
                     map[i][j] = str.charAt(j);
 
                     if (map[i][j] == '*') { //불
-                        fire.offer(new Pos(i, j));
+                        queue.offer(new Pos(i, j));
                     } else if (map[i][j] == '@') {  //상근
-                        person.offer(new Pos(i, j, 0));
-                        personVisited[i][j] = true;
-                        map[i][j] = '.';    //빈공간으로 바꿔주기
+                        x = i;
+                        y = j;
                     }
                 }
             }
+            queue.offer(new Pos(x, y, 0));
             bw.write(bfs());
             bw.write("\n");
         }
@@ -47,49 +42,29 @@ public class Main {
     }
 
     public static String bfs() {
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+            char nowChar = map[now.x][now.y];
 
-        while (person.size() > 0 || fire.size() > 0) {
-            int fireSize = fire.size();
-            int personSize = person.size();
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-            while (fireSize-- > 0) {
-                Pos now = fire.poll();
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
-
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] != '.') {
-                        continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                    if (nowChar == '@') {
+                        return String.valueOf(now.cnt + 1);
                     }
-                    fire.offer(new Pos(nx, ny));
-                    map[nx][ny] = '*';  //확산
+                    continue;
+                }
+
+                if (map[nx][ny] == '.') {
+                    queue.offer(new Pos(nx, ny, now.cnt + 1));
+                    map[nx][ny] = nowChar;
                 }
             }
-
-            while (personSize-- > 0) {
-                Pos now = person.poll();
-
-                if (now.x == 0 || now.x == N - 1 || now.y == 0 || now.y == M - 1) {
-                    return String.valueOf(now.cnt + 1);
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
-
-                    if (nx < 0 || nx >= N || ny < 0 || ny >= M || personVisited[nx][ny]) {
-                        continue;
-                    }
-                    if (map[nx][ny] == '.') {
-                        person.offer(new Pos(nx, ny, now.cnt + 1));
-                        personVisited[nx][ny] = true;
-                    }
-                }
-            }
-
         }
         return "IMPOSSIBLE";
+
     }
 }
 
