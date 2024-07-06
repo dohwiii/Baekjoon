07fe1496420e19b0ddbaba3b091e81b8bdf2dd60
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-06 10:37:33 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,97 +1,117 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
-class Main
-{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
     static char[][] map;
-    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int N;
+    static boolean[][] visited;
 
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         map = new char[N][N];
         visited = new boolean[N][N];
 
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            for (int j = 0; j < N; j++)
-            {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = str.charAt(j);
             }
         }
         int normal = 0;
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (!visited[i][j])
-                {
+        int special = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
                     bfs(i, j);
                     normal++;
                 }
             }
         }
-        int special = 0;
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (map[i][j] == 'G')
-                {
-                    map[i][j] = 'R';
-                }
-            }
-        }
         visited = new boolean[N][N];
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if (!visited[i][j])
-                {
-                    bfs(i, j);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    bfs2(i, j);
                     special++;
                 }
             }
         }
-        System.out.println(normal + " " + special);
-
+        System.out.print(normal + " " + special);
 
     }
 
-    public static void bfs(int x, int y)
-    {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
+    public static void bfs(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.add(new Pos(x, y));
         visited[x][y] = true;
 
-        while (!queue.isEmpty())
-        {
-            int[] now = queue.poll();
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
 
-            for (int i = 0; i < 4; i++)
-            {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N)
-                {
-                    if (!visited[nx][ny] && map[nx][ny] == map[x][y])
-                    {
-                        queue.add(new int[]{nx, ny});
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    //방문하지 않았고, 같은 색상이면 고
+                    if (!visited[nx][ny] && map[nx][ny] == map[x][y]) {
+                        queue.offer(new Pos(nx, ny));
                         visited[nx][ny] = true;
                     }
                 }
             }
         }
+    }
+    public static void bfs2(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.add(new Pos(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (!visited[nx][ny]) {
+                        if (map[x][y] == 'R') {
+                            if (map[nx][ny] == 'R' || map[nx][ny] == 'G') {
+                                queue.offer(new Pos(nx, ny));
+                                visited[nx][ny] = true;
+                            }
+                        } else if (map[x][y] == 'G') {
+                            if (map[nx][ny] == 'G' || map[nx][ny] == 'R') {
+                                queue.offer(new Pos(nx, ny));
+                                visited[nx][ny] = true;
+                            }
+                        }
+                        else {
+                            if (map[nx][ny] == map[x][y]) {
+                                queue.offer(new Pos(nx, ny));
+                                visited[nx][ny] = true;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Pos {
+    int x, y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
