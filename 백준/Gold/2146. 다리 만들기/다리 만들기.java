6855d6124302237b6c7517d24ff831cf58235f2f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-11 11:35:59 +0900

    [Gold III] Title: 다리 만들기, Time: 1408 ms, Memory: 292624 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,3 +1,4 @@
+import javax.swing.text.Position;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,22 +6,19 @@ import java.util.*;
 
 public class Main {
     static int N;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static ArrayList<ArrayList<int[]>> sumList;
-    static ArrayList<int[]> mList;
-    static Queue<int[]> queue;
-    static int min = Integer.MAX_VALUE;
+    static int bridge = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        visited = new boolean[N][N];
         map = new int[N][N];
-        sumList = new ArrayList<>();
+        visited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,88 +26,93 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int count = 1;
+        int index = 2;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && map[i][j] != 0) {
-                    Numbering(i, j, count++);
-                    sumList.add(mList);
+                if (map[i][j] == 1) {   //육지
+                    findIsland(i, j, index++);
                 }
             }
         }
-        queue = new LinkedList<>();
-        visited = new boolean[N][N];
-
-        for (int i = 0; i < sumList.size(); i++)
-        {
-            ArrayList<int[]> now = sumList.get(i);
-            for (int j = 0; j < now.size(); j++)
-            {
-                int r = now.get(j)[0];
-                int c = now.get(j)[1];
-
-                makeBridge(r, c, map[r][c]);
-            }
-        }
-        System.out.println(min);
-
-    }
-    public static void makeBridge(int a, int b, int v)
-    {
-        visited = new boolean[N][N];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{a, b, 0});
-        visited[a][b] = true;
-
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nx = now[0] + dx[d];
-                int ny = now[1] + dy[d];
-
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                    if (map[nx][ny] != 0 && map[nx][ny] != v) {
-                        min = Math.min(min, now[2]);
-                        return;
-                    }
-                    if (!visited[nx][ny] && map[nx][ny] == 0) {
-                        visited[nx][ny] = true;
-                        q.add(new int[]{nx, ny, now[2] + 1});
-                    }
-
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] != 0) {
+                    visited = new boolean[N][N];
+                    makeBridge(i, j);
                 }
             }
-
         }
+        System.out.println(bridge);
 
     }
-    public static void Numbering(int a, int b, int v){
-        queue = new LinkedList<>();
-        mList = new ArrayList<>();
-        queue.add(new int[]{a, b, v});
-        visited[a][b] = true;
-        map[a][b] = v;
-        mList.add(new int[]{a, b});
+
+    public static void findIsland(int x, int y, int index) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(new Pos(x, y));
+        map[x][y] = index;
 
         while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int r2 = pos[0];
-            int c2 = pos[1];
+            Pos now = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int tempR = dx[i];
-                int tempC = dy[i];
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-                if (r2 + tempR >= 0 && r2 + tempR < N && c2 + tempC >= 0 && c2 + tempC < N) {
-                    if (!visited[r2 + tempR][c2 + tempC] && map[r2 + tempR][c2 + tempC] != 0) {
-                        map[r2 + tempR][c2 + tempC] = pos[2];
-                        visited[r2 + tempR][c2 + tempC] = true;
-                        queue.add(new int[]{r2 + tempR, c2 + tempC, pos[2]});
-                        mList.add(new int[]{r2 + tempR, c2 + tempC});
-                    }
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == 0) {
+                    continue;
+                }
+                if (map[nx][ny] == 1) { //바다
+                    queue.offer(new Pos(nx, ny));
+                    map[nx][ny] = index;
                 }
             }
         }
+
+    }
+
+    public static void makeBridge(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(new Pos(x, y, 0));
+        visited[x][y] = true;
+        int nowNum = map[x][y];
+
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == nowNum || visited[nx][ny]) {
+                    continue;
+                }
+
+                if (map[nx][ny] == 0) { //바다
+                    queue.offer(new Pos(nx, ny, now.cnt + 1));
+                    visited[nx][ny] = true;
+                } else {
+                    if (map[nx][ny] != nowNum) { //새로운 섬
+                        bridge = Math.min(bridge, now.cnt);
+                    }
+                }
+
+            }
+        }
+    }
+
+}
+
+class Pos {
+    int x, y, cnt;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Pos(int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
     }
 }
