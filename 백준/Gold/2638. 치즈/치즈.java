ae1a632f780826d81ddbf7b20233c7bc502756b8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-12 15:05:01 +0900

    [Gold III] Title: 치즈, Time: 92 ms, Memory: 13160 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -13,7 +13,7 @@ public class Main {
     static int[] dy = {0, 1, 0, -1};
     static int[][] map;
     static boolean[][] visited;
-    static int[][] dp;
+    static Queue<Pos> air = new ArrayDeque<>();
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +22,7 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N][M];
         map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
@@ -30,109 +31,48 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
+        air.offer(new Pos(0, 0));
         int hour = 0;
 
         while (true) {
-            int[][] temp = new int[N][M];
-            visited = new boolean[N][M];
-            dp = new int[N][M];
+            int size = air.size();
 
-            for (int i = 0; i < N; i++) {
-                temp[i] = map[i].clone();
+            while (size-- > 0) {
+                Pos now = air.poll();
+                dfs(now.x, now.y);
             }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (i == 0 || i == N - 1 || j == 0 || j == M - 1) { //맨 가장자리
-                        if (temp[i][j] == 0) {
-                            air(i, j, temp);
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (!visited[i][j] && temp[i][j] == 1) {
-                        bfs(i, j, temp);
-                    }
-                }
-            }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (dp[i][j] >= 2) {
-                        map[i][j] = 0;  //녹음
-                    }
-                }
+            if (air.isEmpty()) {
+                break;
             }
             hour++;
 
-            boolean isMelt = true;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] != 0) {
-                        isMelt = false;
-                    }
-                }
-            }
-            if (isMelt) {
-                break;
-            }
-
         }
+
         bw.write(hour + " ");
         bw.flush();
 
     }
 
-    public static void air(int x, int y, int[][] temp) {
-        Queue<Pos> queue = new ArrayDeque<>();
-        queue.offer(new Pos(x, y));
-        temp[x][y] = 3;
-
-        while (!queue.isEmpty()) {
-            Pos now = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || temp[nx][ny] == 3) {
-                    continue;
-                }
-                if (temp[nx][ny] == 0) { //공기
-                    queue.offer(new Pos(nx, ny));
-                    temp[nx][ny] = 3;
-                }
-            }
-        }
-    }
-
-    public static void bfs(int x, int y, int[][] temp) {
-        Queue<Pos> queue = new ArrayDeque<>();
-        queue.offer(new Pos(x, y));
+    public static void dfs(int x, int y) {
         visited[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            Pos now = queue.poll();
-            int cnt = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) {
-                    continue;
-                }
-                if (temp[nx][ny] == 3) { //공기 중 접촉
-                    cnt++;
-                }
-                else {  //치즈
-                    queue.offer(new Pos(nx, ny));
-                    visited[nx][ny] = true;
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) {
+                continue;
+            }
+            if (map[nx][ny] != 0) { //치즈
+                if (++map[nx][ny] == 3) { //2변이상 닿았을 경우
+                    air.offer(new Pos(nx, ny));
                 }
             }
-            dp[now.x][now.y] = cnt;
+            else {  //공기
+                dfs(nx, ny);
+            }
+
         }
     }
 
