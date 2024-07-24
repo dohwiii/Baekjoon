bd73c560a978bd7fa53a14afc1aa1a257ad310ba
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-24 19:25:11 +0900

    [Gold IV] Title: 알파벳, Time: 928 ms, Memory: 12616 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,59 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] board;
+    static int R, C;
+    static int[][] dp;
+    static char[][] map;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int R, C;
-    static boolean[] visited;
-    static int max = Integer.MIN_VALUE;
+    static int maxLen = 0;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        board = new int[R][C];
-        visited = new boolean[26];
+        map = new char[R][C];
+        dp = new int[R][C];
+        boolean[] visited = new boolean[26];
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
-                board[i][j] = str.charAt(j) - 'A';
+                map[i][j] = str.charAt(j);
             }
         }
-        DFS(0, 0, 0);
-        System.out.println(max);
+        for (int i = 0; i < R; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
+        dfs(0, 0, 1, visited);
+        bw.write(maxLen + "\n");
+        bw.flush();
     }
 
-    public static void DFS(int x, int y, int count)
-    {
-        if (visited[board[x][y]])
-        {
-            if (max < count) {
-                max = count;
-            }
-            return;
-        }
-        visited[board[x][y]] = true;
+    public static void dfs(int x, int y, int count, boolean[] visited) {
+        visited[map[x][y] - 'A'] = true;
+        maxLen = Math.max(maxLen, count);
 
         for (int i = 0; i < 4; i++) {
-            int x1 = x + dx[i];
-            int y1 = y + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if (x1 >= 0 && y1 >= 0 && x1 < R && y1 < C)
-            {
-                DFS(x1, y1, count + 1);
-
+            if (nx >= 0 && nx < R && ny >= 0 && ny < C && !visited[map[nx][ny] - 'A']) {
+                dfs(nx, ny, count + 1, visited);
             }
         }
-        visited[board[x][y]] = false;
-    }
 
+        visited[map[x][y] - 'A'] = false; // 재귀 호출이 끝나면 원래 상태로 복구
+    }
 }
