ee3e364f2f8a9905a25fcfb5570fa0d7e6c8c49a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-07-27 17:27:43 +0900

    [Gold V] Title: 숫자고르기, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -3,70 +3,43 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[] secondRow;
+    static int[] arr;
     static boolean[] visited;
-    static List<Integer> result;
+    static List<Integer> ans = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-        secondRow = new int[N + 1];
+        arr = new int[N + 1];
         visited = new boolean[N + 1];
-        result = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
-            secondRow[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                List<Integer> cycle = new ArrayList<>();
-                findCycle(i, cycle);
-                if (!cycle.isEmpty()) {
-                    result.addAll(cycle);
-                }
-            }
+            visited[i] = true;
+            dfs(i, i);
+            visited[i] = false;
         }
-
-        Collections.sort(result);
-        bw.write(result.size() + "\n");
-        for (int num : result) {
+        bw.write(ans.size() + "\n");
+        for (int num : ans) {
             bw.write(num + "\n");
         }
         bw.flush();
         bw.close();
     }
 
-    private static void findCycle(int start, List<Integer> cycle) {
-        boolean[] inStack = new boolean[N + 1];
-        Stack<Integer> stack = new Stack<>();
-        stack.push(start);
-
-        while (!stack.isEmpty()) {
-            int node = stack.peek();
-            if (visited[node]) {
-                stack.pop();
-                continue;
-            }
-            visited[node] = true;
-            inStack[node] = true;
-
-            int next = secondRow[node];
-            if (!visited[next]) {
-                stack.push(next);
-            } else if (inStack[next]) {
-                // Cycle detected
-                while (!stack.isEmpty()) {
-                    int cycleNode = stack.pop();
-                    cycle.add(cycleNode);
-                    if (cycleNode == next) {
-                        break;
-                    }
-                }
-                return;
-            }
+    private static void dfs(int start, int target) {
+        if (!visited[arr[start]]) {
+            visited[arr[start]] = true;
+            dfs(arr[start], target);
+            visited[arr[start]] = false;
+        }
+        if (arr[start] == target) {
+            ans.add(target);
         }
     }
 }
