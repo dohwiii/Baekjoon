e3a909bec7eff773d4b3122ebcd7e2cc09a558f4
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-08-03 18:25:55 +0900

    [Gold IV] Title: 트리, Time: 308 ms, Memory: 53464 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] parent;
-    static int[] rank;
+    static boolean[] isTree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,36 +15,33 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-            if (N == 0 && M == 0) break;
-
-            parent = new int[N + 1];
-            rank = new int[N + 1];
-            for (int i = 1; i <= N; i++) {
-                parent[i] = i; // 초기화
-                rank[i] = 0;
+            if (N == 0) {
+                break;
             }
-
-            boolean[] isTree = new boolean[N + 1];
+            isTree = new boolean[N + 1];
+            parent = new int[N + 1];
+            for (int i = 1; i <= N; i++) {
+                parent[i] = i;
+            }
             Arrays.fill(isTree, true);
 
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(st.nextToken());
-                int v = Integer.parseInt(st.nextToken());
-                if (find(u) == find(v)) {
-                    isTree[find(u)] = false; // 사이클이 있으면 트리가 아님
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+
+                if (find(s) == find(e)) {
+                    isTree[find(s)] = false;
                 } else {
-                    union(u, v); // 두 집합을 합침
+                    union(s, e);
                 }
             }
-
             int treeCount = 0;
             for (int i = 1; i <= N; i++) {
                 if (parent[i] == i && isTree[i]) {
                     treeCount++;
                 }
             }
-
             sb.append("Case ").append(t).append(": ");
             if (treeCount == 0) {
                 sb.append("No trees.\n");
@@ -54,32 +51,35 @@ public class Main {
                 sb.append("A forest of ").append(treeCount).append(" trees.\n");
             }
             t++;
+
         }
 
-        System.out.print(sb.toString());
+        System.out.println(sb);
     }
 
-    static int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // 경로 압축
+    public static int find(int a) {
+        if (a != parent[a]) {
+            return parent[a] = find(parent[a]);
         }
-        return parent[x];
+        return parent[a];
     }
 
-    static void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
+    public static void union(int a, int b) {
+        int parentA = find(a);
+        int parentB = find(b);
 
-        if (rootX != rootY) {
-            // 랭크 기반 유니온
-            if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-            } else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
+        if (parentA != parentB) {
+            parent[parentB] = parentA;
+            isTree[parentA] = isTree[parentA] && isTree[parentB];
         }
+    }
+}
+
+class Node {
+    int n1, n2;
+
+    public Node(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
     }
 }
