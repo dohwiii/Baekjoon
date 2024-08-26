06e18ca842eb608873143eca47e058eaced36a2c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-08-27 08:40:22 +0900

    [Silver I] Title: 현수막, Time: 180 ms, Memory: 23856 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 0, 1, -1, 1, -1, -1, 1};
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[M][N];
+        visited = new boolean[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int num = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    num++;
+                    dfs(i, j);
+                }
+            }
+        }
+        System.out.println(num);
+
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N || map[nx][ny] == 0 || visited[nx][ny]) {
+                continue;
+            }
+            dfs(nx, ny);
+        }
+    }
+
+}
+
+class Pos {
+    int x, y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
