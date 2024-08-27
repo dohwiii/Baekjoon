ebb7b9825b09fdd72d992ae9502be8f866fa51ae
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-08-27 15:16:14 +0900

    [Gold IV] Title: 미친 로봇, Time: 128 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -45,31 +45,9 @@ public class Main {
             if (probs[i] == 0.0) {
                 continue;
             }
-            Pos nextPos = new Pos(nx, ny);
             dfs(nx, ny, count + 1, prob * probs[i]);
         }
         visited[x][y] = false;
 
-    }
-}
-
-class Pos {
-    int x, y;
-
-    public Pos(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pos pos = (Pos) o;
-        return x == pos.x && y == pos.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
     }
 }
