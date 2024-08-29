7d72b69daa12f6d57a9aeff1df316ce40b2d125d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-08-29 22:38:29 +0900

    [Silver I] Title: 나무 탈출, Time: 1360 ms, Memory: 224224 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -5,7 +5,6 @@ public class Main {
     static int N;
     static List<Integer>[] list;
     static boolean[] visited;
-    static List<Integer> leaf;
     static int sum;
 
     public static void main(String[] args) throws IOException {
@@ -13,7 +12,6 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         visited = new boolean[N + 1];
-        leaf = new ArrayList<>();
         list = new List[N + 1];
         for (int i = 0; i <= N; i++) {
             list[i] = new ArrayList<>();
@@ -27,11 +25,7 @@ public class Main {
             list[b].add(a);
         }
         findParent(1, 0);  //부모노드 찾기 -> 리프노드 찾기
-
-//        for (int node : leaf) {
-//            visited = new boolean[N + 1];
-//            game(node, 0);
-//        }
+        
         if (sum % 2 != 0) {
             System.out.println("Yes");
         }
@@ -56,25 +50,8 @@ public class Main {
             }
         }
         if (isLeaf) {
-            leaf.add(node);
             sum += depth;
         }
     }
 
-//    public static void game(int node, int route) {
-//        if (node == 1) {    //루트노드 도착
-//            sum += route;
-//            return;
-//        }
-//        if (visited[node]) {
-//            return;
-//        }
-//        visited[node] = true;
-//
-//        for (int next : list[node]) {
-//            if (!visited[next]) {
-//                game(next, route + 1);
-//            }
-//        }
-//    }
 }
