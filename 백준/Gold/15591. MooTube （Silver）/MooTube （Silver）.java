424b03013c6a2eada6d6f564e03b29c3b715aa7b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-09-04 17:36:23 +0900

    [Gold V] Title: MooTube (Silver), Time: 300 ms, Memory: 22732 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class Main {
     static int N, Q;
-    static Node[] nodes;
-    static boolean[][] visited;
-    static long[][] usado;
+    static Edge[] edges;
+    static Query[] queries;
+    static int[] parent, size;
+    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,66 +16,93 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1][N + 1];
-        nodes = new Node[N + 1];
-        usado = new long[N + 1][N + 1];
 
+        answer = new int[Q];
+        edges = new Edge[N - 1];
+        queries = new Query[Q];
+        parent = new int[N + 1];
+        size = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int p = Integer.parseInt(st.nextToken());   //동영상 p
             int q = Integer.parseInt(st.nextToken());   //동영상 q
             int r = Integer.parseInt(st.nextToken());   //유사도
-
-            nodes[p] = new Node(q, r, nodes[p]);
-            nodes[q] = new Node(p, r, nodes[q]);
+            edges[i] = new Edge(p, q, r);
         }
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(usado[i], 1_000_000_001);
-        }
-
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());   //유사도
             int v = Integer.parseInt(st.nextToken());   //동영상 번호
-            int cnt = 0;
-            if (!visited[v][v]) {
-                dfs(v, v, 1_000_000_001);
-            }
-            for (int j = 1; j <= N; j++) {
-                if (j == v) {
-                    continue;
-                }
-                if (usado[v][j] >= k) {
-                    cnt++;
-                }
-            }
-            bw.write(cnt + "\n");
+            queries[i] = new Query(k, v, i);
         }
+        Arrays.sort(edges, (a, b) -> b.usado - a.usado);
+        Arrays.sort(queries, (a, b) -> b.k - a.k);
+
+        int edgeIndex = 0;
+
+        for (Query query : queries) {
+            while (edgeIndex < N - 1 && edges[edgeIndex].usado >= query.k) {
+                union(edges[edgeIndex].p, edges[edgeIndex].q);
+                edgeIndex++;
+            }
+            answer[query.index] = size[find(query.v)] - 1;
+        }
+
+        for (int i = 0; i < Q; i++) {
+            bw.write(answer[i] + "\n");
+        }
+
         bw.flush();
         bw.close();
         br.close();
 
     }
 
-    public static void dfs(int video, int node, long minValue) {
-        visited[video][node] = true;
-        for (Node next = nodes[node]; next != null; next = next.next) {
-            if (!visited[video][next.node]) {
-                long currentMin = Math.min(minValue, next.usado);
-                usado[video][next.node] = currentMin;
-                dfs(video, next.node, currentMin);
+    public static int find(int a) {
+        if (parent[a] != a) {
+            return parent[a] = find(parent[a]);
+        }
+        return a;
+    }
+
+    public static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            if (size[rootA] >= size[rootB]) {
+                parent[rootB] = rootA;
+                size[rootA] += size[rootB];
+            }
+            else {
+                parent[rootA] = rootB;
+                size[rootB] += size[rootA];
             }
         }
     }
 
 }
-class Node {
-    int node, usado;
-    Node next;
+class Edge  {
+    int p, q, usado;
 
-    public Node(int node, int usado, Node next) {
-        this.node = node;
+    public Edge(int p, int q, int usado) {
+        this.p = p;
+        this.q = q;
         this.usado = usado;
-        this.next = next;
+    }
+}
+// 쿼리를 나타내는 클래스
+class Query {
+    int k, v, index;
+
+    public Query(int k, int v, int index) {
+        this.k = k;
+        this.v = v;
+        this.index = index;
     }
 }
