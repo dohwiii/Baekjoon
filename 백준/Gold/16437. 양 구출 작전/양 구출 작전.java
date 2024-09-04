512266a60b937a81dbbd528638fbe6c790428db3
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-09-04 13:36:20 +0900

    [Gold III] Title: 양 구출 작전, Time: 456 ms, Memory: 69548 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -4,66 +4,56 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static List<Integer>[] list;
     static boolean[] visited;
-    static long sum = 0;
-    static Node[] values;
+    static Node[] islands;
+    static long[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());    //섬의 개수
         visited = new boolean[N + 1];
-        values = new Node[N + 1];
-        list = new List[N + 1];
-        for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
+        arr = new long[N + 1];
+        islands = new Node[N + 1];
+
         for (int i = 2; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char t = st.nextToken().charAt(0);   //양 or 늑대
-            int a = Integer.parseInt(st.nextToken());   //수
-            int p = Integer.parseInt(st.nextToken());   //연결되어 있는 섬
-
-            list[i].add(p);
-            list[p].add(i);
-            values[i] = new Node(t, a);
+            char t = st.nextToken().charAt(0);  //늑대 'W', 양 'S'
+            if (t == 'W') {
+                arr[i] = -Integer.parseInt(st.nextToken());
+            }
+            else {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int p = Integer.parseInt(st.nextToken());   //연결된 섬
+            islands[p] = new Node(i, islands[p]);
         }
-        values[1] = new Node('S', 0);
-        dfs(1);
+        System.out.println(dfs(1));
 
-        System.out.println(sum);
-        br.close();
+
+
+
+
     }
 
     public static long dfs(int node) {
-        visited[node] = true;
-        long totalSheep = 0;
-
-        for (int next : list[node]) {
-            if (!visited[next]) {
-                long sheep = dfs(next);
-                Node now = values[next];
-                if (now.type == 'S') {  //양이라면
-                    totalSheep += sheep + now.count;
-                }
-                else {  //늑대라면
-                    totalSheep += Math.max((sheep - now.count), 0);
-                }
-            }
+        for (Node next = islands[node]; next != null; next = next.next) {
+            arr[node] += dfs(next.node);
         }
-        sum = totalSheep;
-        return totalSheep;
+        if (arr[node] < 0) {
+            return 0;
+        }
+        return arr[node];
     }
 
 
 }
 class Node {
-    char type;
-    int count;
+    int node;
+    Node next;
 
-    public Node(char type, int count) {
-        this.type = type;
-        this.count = count;
+    public Node(int node, Node next) {
+        this.node = node;
+        this.next = next;
     }
 }
