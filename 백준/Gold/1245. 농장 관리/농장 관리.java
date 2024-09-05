f05cc2fcfadcf07bd9d744a8c935c86f385f6fff
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-09-06 00:01:15 +0900

    [Gold V] Title: 농장 관리, Time: 84 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -1,12 +1,13 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[] dx = {0, 0, -1, 1, 1, 1, -1, -1};
-    static int[] dy = {1, -1, 0, 0, 1, -1, -1, 1};
-    static boolean[][] visited;
     static int[][] map;
+    static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 0, 1, -1, 1, -1, -1, 1};
+    static boolean[][] visited;
     static boolean isPeak;
 
     public static void main(String[] args) throws IOException {
@@ -27,7 +28,7 @@ public class Main {
         int mountain = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] != 0 && !visited[i][j]) {
+                if (!visited[i][j] && map[i][j] != 0) {
                     isPeak = true;
                     dfs(i, j, map[i][j]);
                     if (isPeak) {
@@ -42,6 +43,7 @@ public class Main {
     }
 
     public static void dfs(int x, int y, int height) {
+
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
@@ -49,16 +51,16 @@ public class Main {
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
                 continue;
             }
-            if (map[nx][ny] > height) {
+            if (height < map[nx][ny]) {
                 isPeak = false;
+                continue;
             }
-            if (map[nx][ny] == height && !visited[nx][ny]) {
+            if (height == map[nx][ny] && !visited[nx][ny]) {
                 visited[nx][ny] = true;
                 dfs(nx, ny, height);
             }
-
         }
-    }
 
+    }
 
 }
