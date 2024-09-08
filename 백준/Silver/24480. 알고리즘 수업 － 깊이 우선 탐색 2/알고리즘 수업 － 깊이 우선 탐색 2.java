c498219f6f563c76d81cc152cc54dc8280c3ca39
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-09-08 23:28:52 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 756 ms, Memory: 94908 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
     static int N, M, R;
-    static List<Integer>[] list;
+    static PriorityQueue<Integer>[] pq;
     static boolean[] visited;
     static int[] order;
     static int idx = 1;
@@ -18,18 +18,19 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         visited = new boolean[N + 1];
         order = new int[N + 1];
-        list = new List[N + 1];
+        pq = new PriorityQueue[N + 1];
         for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
+            pq[i] = new PriorityQueue<>(Comparator.reverseOrder());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list[u].add(v);
-            list[v].add(u);
+            pq[u].add(v);
+            pq[v].add(u);
         }
+
         StringBuilder sb = new StringBuilder();
         dfs(R);
         for (int i = 1; i <= N; i++) {
@@ -42,14 +43,10 @@ public class Main {
     public static void dfs(int node) {
         visited[node] = true;
         order[node] = idx++;
-        list[node].sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
 
-        for (int next : list[node]) {
+        while (!pq[node].isEmpty()) {
+            int next = pq[node].poll();
+
             if (!visited[next]) {
                 dfs(next);
             }
