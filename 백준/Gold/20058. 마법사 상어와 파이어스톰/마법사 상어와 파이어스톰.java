8404451978e88803f99cc2aad9da4fe1198e5ad7
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-09-09 17:03:46 +0900

    [Gold III] Title: 마법사 상어와 파이어스톰, Time: 520 ms, Memory: 179008 KB -BaekjoonHub

--- 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.java
+++ 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.java
@@ -4,16 +4,15 @@ import java.util.*;
 
 public class Main {
     static int N, Q;
-    static long size;
-    static long[][] map;
-    static long[][] tempMap;
+    static int size;
+    static int[][] map;
+    static int[][] tempMap;
     static boolean[][] visited;
     static int[] lSize;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static long area = 0;
     static long sum = 0;
-    static List<Pos> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,9 +20,9 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
-        size = (long) Math.pow(2, N);
-        map = new long[(int) size][(int) size];
-        tempMap = new long[(int) size][(int) size];
+        size = (int) Math.pow(2, N);
+        map = new int[size][size];
+        tempMap = new int[size][size];
         lSize = new int[Q];
 
         for (int i = 0; i < size; i++) {
@@ -35,95 +34,117 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < Q; i++) {
             lSize[i] = Integer.parseInt(st.nextToken());
-            visited = new boolean[(int) size][(int) size];
-            list = new ArrayList<>();
+            visited = new boolean[size][size];
             int lsize = (int) Math.pow(2, lSize[i]);    //2
 
-            for (int j = 0; j < size; j += lsize) { //0,2,4,6
-                for (int k = 0; k < size; k += lsize) {
-                    rotate(j, k, lsize);
-                }
-            }
+            //시계방향으로 회전
+            rotate(lsize);
 
+            //인접방향 얼음 개수 체크하기
             for (int j = 0; j < size; j++) {
                 for (int k = 0; k < size; k++) {
-                    if (!visited[j][k] && tempMap[j][k] > 0) {
-                        dfs(j, k);
+                    if (!visited[j][k]) {
+                        melt(j, k);
                     }
                 }
             }
-            for (Pos p : list) {
-                tempMap[p.x][p.y]--;
-            }
-            for (int j = 0; j < size; j++) {
-                map[j] = tempMap[j].clone();
-            }
+            //map은 녹은거 계산 끝난 상황
         }
         long maxArea = 0;
-        long ansSum = 0;
-        visited = new boolean[(int) size][(int) size];
+        long totalIce = countIce();
+        visited = new boolean[size][size];
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (!visited[i][j] && tempMap[i][j] > 0) {
+                if (!visited[i][j] && map[i][j] != 0) {
                     area = 0;
-                    findAnswer(i, j, 1);
+                    findAnswer(i, j);
                     maxArea = Math.max(maxArea, area);
                 }
             }
         }
-        System.out.println(sum);
+        System.out.println(totalIce);
         System.out.println(maxArea);
 
     }
-    public static void dfs(int x, int y) {
+
+    public static void findAnswer(int x, int y) {
         visited[x][y] = true;
-        int near = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= size || ny < 0 || ny >= size || tempMap[nx][ny] <= 0) {
-                continue;
-            }
-            near++;
-            if (!visited[nx][ny]) {
-                dfs(nx, ny);
-            }
-        }
-        if (near < 3) {
-            list.add(new Pos(x, y));
-        }
-    }
-
-    public static void findAnswer(int x, int y, int depth) {
-        visited[x][y] = true;
-        sum += tempMap[x][y];
         area++;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || nx >= size || ny < 0 || ny >= size || tempMap[nx][ny] <= 0) {
+            if (nx < 0 || nx >= size || ny < 0 || ny >= size || map[nx][ny] == 0) {
                 continue;
             }
             if (!visited[nx][ny]) {
-                findAnswer(nx, ny, depth + 1);
+                findAnswer(nx, ny);
             }
         }
 
     }
 
-    public static void rotate(int x, int y, int mid) {
-        for (int i = 0; i < mid; i++) {
-            for (int j = 0; j < mid; j++) {
-                tempMap[x + i][y + mid - j - 1] = map[x + j][y + i];
+    public static int countIce() {
+        int ans = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map[i][j] != 0) {
+                    ans += map[i][j];
+                }
             }
         }
+        return ans;
+    }
+
+    public static void melt(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(new Pos(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+            int ice = 0;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx < 0 || nx >= size || ny < 0 || ny >= size || tempMap[nx][ny] == 0) {
+                    continue;
+                }
+                ice++;
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.offer(new Pos(nx, ny));
+                }
+            }
+            if (ice <= 2) {
+                map[now.x][now.y] = Math.max(0, tempMap[now.x][now.y] - 1);
+            } else {
+                map[now.x][now.y] = tempMap[now.x][now.y];
+            }
+        }
+
+    }
+
+    public static void rotate(int lsize) {
+        for (int r = 0; r < size; r += lsize) { //0,2,4,6
+            for (int c = 0; c < size; c += lsize) {
+                for (int i = 0; i < lsize; i++) {
+                    for (int j = 0; j < lsize; j++) {
+                        tempMap[r + i][c + lsize - j - 1] = map[r + j][c + i];
+                    }
+                }
+            }
+        }
+
+
     }
 
 }
+
 class Pos {
     int x, y;
 
