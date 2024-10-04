053a7cbfe7d369bc434653abb1cfd1f3871ca66b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-05 08:52:03 +0900

    [Gold IV] Title: 운동, Time: 532 ms, Memory: 66144 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class Main {
     static int V, E;
+    static List<Node>[] list;
     static long[][] dist;
-    static final long INF = 1_600_000_000L;
+    static final int INF = 1600000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,26 +14,21 @@ public class Main {
 
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-
         dist = new long[V + 1][V + 1];
-
+        list = new List[V + 1];
+        for (int i = 0; i <= V; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= V; i++) {
+            Arrays.fill(dist[i], INF);
+        }
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
+            list[a].add(new Node(b, c));
             dist[a][b] = c;
-        }
-        for (int i = 1; i <= V; i++) {
-            for (int j = 1; j <= V; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (dist[i][j] == 0) {
-                    dist[i][j] = INF;
-                }
-            }
         }
         for (int k = 1; k <= V; k++) {
             for (int i = 1; i <= V; i++) {
@@ -40,32 +36,37 @@ public class Main {
                     continue;
                 }
                 for (int j = 1; j <= V; j++) {
-                    if (i == j || j == k) {
-                        continue;
-                    }
-                    dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
+                    if (i == j) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        long min = INF;
+        long min = 1600000000;
         for (int i = 1; i <= V; i++) {
             for (int j = 1; j <= V; j++) {
                 if (i == j) {
                     continue;
                 }
-                if (dist[i][j] == INF) {
-                    continue;
-                }
                 min = Math.min(min, dist[i][j] + dist[j][i]);
             }
         }
-        if (min != INF) {
+        if (min != 1600000000) {
             System.out.println(min);
         } else {
             System.out.println(-1);
-
         }
+
 
     }
 
+
+}
+
+class Node {
+    int node, value;
+
+    public Node(int node, int value) {
+        this.node = node;
+        this.value = value;
+    }
 }
