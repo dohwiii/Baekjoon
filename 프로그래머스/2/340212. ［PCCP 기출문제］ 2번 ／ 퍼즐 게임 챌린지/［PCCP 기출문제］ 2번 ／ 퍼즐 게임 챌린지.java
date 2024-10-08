f471f4bde5d11a44eba1c8eace0f52bc39440640
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-08 15:59:43 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 57.60 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int i=0; i<diffs.length; i++) {
+            sum += diffs[i];
+        }
+        int N = diffs.length;
+        int l = 1, r = Arrays.stream(diffs).max().getAsInt();;
+        
+        while(l <= r) {
+            int level = (int) (l + r) / 2;
+            long totalTime = solve(limit, level, diffs, times);    //숙련도와 걸린 시간
+
+            if(totalTime > limit) {    //시간이 초과
+                l = level + 1;
+            }
+            else if(totalTime <= limit) {  //시간 괜찮
+                answer = Math.min(answer, level);
+                r = level - 1;
+            }
+            
+        }
+        
+        return answer;
+    }
+    public long solve(long limit, int level, int[] diffs, int[] times) {
+        long time = 0;
+        
+        for(int i=0; i<diffs.length; i++) {
+            if(level >= diffs[i]) { //숙련도 충분해서 퍼즐 바로 풀 수 있음
+                time += times[i];
+            }
+            else {
+                int remain = diffs[i] - level;  //틀린 횟수
+                int x = times[i - 1] + times[i];
+                time += x * remain + times[i];
+                    
+            }
+        }
+        
+        return time;
+    }
+}
