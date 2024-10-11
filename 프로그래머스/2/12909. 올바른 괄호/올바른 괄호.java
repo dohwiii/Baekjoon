7cdccf3e2cd13641d3253bbfc48a3f6bfc36d74f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-11 19:20:50 +0900

    [level 2] Title: 올바른 괄호, Time: 22.00 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        int N = s.length();
+        Queue<Character> queue = new ArrayDeque<>();
+        
+        if(s.charAt(0) != '(') {
+            return false;
+        }
+        for(int i=0; i<N; i++) {
+            char c = s.charAt(i);
+            queue.offer(c);
+        }
+        int countA = 0;
+        int countB = 0;
+        
+        while(!queue.isEmpty()) {
+            char now = queue.poll();
+            if(now == '(') {
+                countA++;
+            }
+            else if(now == ')') {
+                countB++;
+            }
+            if(countA < countB) {
+                return false;
+            }
+        
+        }
+        if(countA == countB) {
+            return true;
+        }
+        return false;
+    }
+}
