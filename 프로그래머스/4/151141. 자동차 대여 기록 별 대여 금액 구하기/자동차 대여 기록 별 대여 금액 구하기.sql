affc8c0fcf5f41a40f50514345c02f304b3eab09
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-16 13:30:38 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,27 +1,3 @@
--- 코드를 입력하세요
-# SELECT HISTORY_ID, 
-#     ROUND(CASE
-#         WHEN datediff(end_date, start_date) + 1 >= 90 and DURATION_TYPE like '90%'
-#              THEN (c.daily_fee * ((100 - DISCOUNT_RATE) * 0.01)) * (datediff(end_date, start_date) + 1)
-#         WHEN datediff(end_date, start_date) + 1 >= 30 and datediff(end_date, start_date) + 1 < 90 and DURATION_TYPE like '30%'
-#              THEN (c.daily_fee * ((100 - DISCOUNT_RATE) * 0.01)) * (datediff(end_date, start_date) + 1)
-#         WHEN datediff(end_date, start_date) + 1 >= 7 and datediff(end_date, start_date) + 1 < 30 and DURATION_TYPE like '7%'
-#              THEN (c.daily_fee * ((100 - DISCOUNT_RATE) * 0.01)) * (datediff(end_date, start_date) + 1)
-#         WHEN datediff(end_date, start_date) + 1 < 7
-#              THEN c.daily_fee * (datediff(end_date, start_date) + 1)
-#     END, 0) AS 'FEE'
-# from CAR_RENTAL_COMPANY_RENTAL_HISTORY h 
-# join CAR_RENTAL_COMPANY_CAR c using (car_id)
-# left outer join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on c.car_type = p.car_type
-# where c.car_type = '트럭'
-# order by FEE desc, HISTORY_ID desc;
-
-# select *
-# from CAR_RENTAL_COMPANY_RENTAL_HISTORY h 
-# join CAR_RENTAL_COMPANY_CAR c using (car_id)
-# left outer join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on c.car_type = p.car_type
-# where c.car_type = '트럭';
-# HISTORY_ID, IF(period >= 7, ROUND((car.daily_fee * (100 - rate)), 0) * period, car.daily_fee * period) as 'FEE'
 select HISTORY_ID, IF(period >= 7, ROUND((car.daily_fee * (rate * 0.01)), 0) * period, car.daily_fee * period) as 'FEE'
 from (
     select HISTORY_ID, h.car_id, datediff(end_date, start_date) + 1 as 'period', c.car_type, c.daily_fee, duration_type, (100 - DISCOUNT_RATE) as rate
