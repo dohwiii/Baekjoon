20a793ffdd0236a0264dec4df1a73f3ba0092361
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-16 16:08:43 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,15 +1,15 @@
-select HISTORY_ID, IF(period >= 7, ROUND((car.daily_fee * (rate * 0.01)), 0) * period, car.daily_fee * period) as 'FEE'
+select HISTORY_ID, ROUND((1 - IF(ISNULL(p.DISCOUNT_RATE), 0, p.DISCOUNT_RATE)/100) * n.daily_fee * period, 0) as 'FEE'
 from (
-    select HISTORY_ID, h.car_id, datediff(end_date, start_date) + 1 as 'period', c.car_type, c.daily_fee, duration_type, (100 - DISCOUNT_RATE) as rate
+    select HISTORY_ID, h.car_id, datediff(end_date, start_date) + 1 as 'period', c.car_type, c.daily_fee
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY h 
         join CAR_RENTAL_COMPANY_CAR c using (car_id)
-        left outer join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on c.car_type = p.car_type
-    where (case
-            when datediff(end_date, start_date) + 1 >= 90 then p.duration_type like '90%'
-            when datediff(end_date, start_date) + 1 >= 30 then p.duration_type like '30%'
-            when datediff(end_date, start_date) + 1 >= 7 then p.duration_type like '7%'
-            else p.duration_type like '7%'
+    where c.car_type = '트럭'
+    ) as n
+    left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p on n.car_type = p.car_type
+    and p.DURATION_TYPE = (case
+            when period >= 90 then '90일 이상'
+            when period >= 30 then '30일 이상'
+            when period >= 7 then '7일 이상'
            end
-          ) and c.car_type = '트럭'
-) as car
+          )
 order by FEE desc, HISTORY_ID desc;
