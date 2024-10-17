7bdb678116df79e349bc68bc0b5d0ad9a1e088cb
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-17 16:14:48 +0900

    [level 1] Title: 소수 만들기, Time: 24.11 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    static int answer;
+    
+    public int solution(int[] nums) {
+        visited = new boolean[nums.length];
+        combi(0, 0, nums);
+        return answer;
+    }
+    public static void combi(int start, int depth, int[] nums) {
+        if(depth == 3) {    //3개 다 뽑음
+            int sum = 0;
+            for(int i=0; i<nums.length; i++) {
+                if(visited[i]) {
+                    sum += nums[i];
+                }
+            }
+            boolean isTrue = true;
+            if(sum > 1) {
+                for(int i = 2; i < sum; i++) {
+                    if(sum % i == 0) {
+                        isTrue = false;
+                        break;
+                    }
+                }
+            }         
+            if(isTrue) {
+                answer++;
+            }
+            return;
+        }
+        for(int i = start; i < nums.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                
+                combi(i + 1, depth + 1, nums);
+                
+                visited[i] = false;
+            }
+        }
+    }
+}
