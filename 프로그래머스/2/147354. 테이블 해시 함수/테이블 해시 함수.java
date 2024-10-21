7c1e0e142286ed6435273cef1818d99c7f952e6b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-21 16:31:12 +0900

    [level 2] Title: 테이블 해시 함수, Time: 15.52 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        
+        Arrays.sort(data, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if(a[col-1] == b[col-1]) {
+                    return b[0] - a[0]; //기본키 기준 내림차순
+                }
+                return a[col-1] - b[col-1];
+            }
+        });
+
+        int result = 0;
+        for(int i = row_begin - 1; i <= row_end -1 ; i++) {
+            int sum = 0;
+            
+            for(int j = 0; j < data[0].length; j++) {
+                sum += (data[i][j] % (i + 1));
+            }
+            result = result ^ sum;
+        }
+        return result;
+    }
+}
