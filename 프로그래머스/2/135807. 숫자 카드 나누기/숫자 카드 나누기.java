77395c92bf83688a88fd3bfa7e610bbd324d6b5d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-23 10:23:44 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 36.06 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,37 +1,36 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
+        // arrayA와 arrayB의 최대공약수를 각각 구함
         int gcdA = getGCD(arrayA);
         int gcdB = getGCD(arrayB);
-        
-        int candidateA = (gcdA > 1) ? checkCondition(gcdA, arrayB) : 0;
-        int candidateB = (gcdB > 1) ? checkCondition(gcdB, arrayA) : 0;
-        
+
+        // gcdA와 gcdB가 각각 상대 배열에서 나누어지지 않으면 해당 gcd를 반환
+        int candidateA = (gcdA > 1 && !canBeDivided(gcdA, arrayB)) ? gcdA : 0;
+        int candidateB = (gcdB > 1 && !canBeDivided(gcdB, arrayA)) ? gcdB : 0;
+
+        // 두 조건 중 더 큰 값 반환
         return Math.max(candidateA, candidateB);
     }
-    public int checkCondition(int gcdValue, int[] otherArray) {
-        for(int num : otherArray) {
-            if(num % gcdValue == 0) {
-                return 0;   //조건 만족 안함(나누어짐)
-            }
-        }
-        return gcdValue;
+
+    // 배열의 최대공약수를 구하는 함수
+    private int getGCD(int[] array) {
+        return Arrays.stream(array).reduce(array[0], (a, b) -> gcd(a, b));
     }
-    public int getGCD(int[] array) {
-        int gcdValue = array[0];    //첫 배열값
-        
-        for(int i=1; i<array.length; i++) {
-            gcdValue = gcd(gcdValue, array[i]);
-            if(gcdValue == 1) {
-                return 1;
-            }
-        }
-        return gcdValue;
+
+    // 상대 배열에서 최대공약수로 나누어지는지 확인하는 함수
+    private boolean canBeDivided(int gcdValue, int[] otherArray) {
+        return Arrays.stream(otherArray).anyMatch(num -> num % gcdValue == 0);
     }
-    public int gcd(int a, int b) {
-        if(b == 0) {
-            return a;
+
+    // 유클리드 알고리즘을 이용한 최대공약수 계산
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-        return gcd(b, a % b);
-            
+        return a;
     }
 }
