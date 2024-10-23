234cec819d455cd80ef4c4a7b5b62b0b701fde15
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-23 10:21:00 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 38.19 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,69 +1,34 @@
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        int answer = 0;
-        int a = 0;
-        int b = 0;
-        boolean isPossibleA = true;
-        boolean isPossibleB = true;
+        int gcdA = getGCD(arrayA);
+        int gcdB = getGCD(arrayB);
         
-        if(arrayA.length > 1) {
-             a = gcd(arrayA[0], arrayA[1]);
-             b = gcd(arrayB[0], arrayB[1]);
-            if(a == 1) {
-                isPossibleA = false;
-            }
-            if(b == 1) {
-                isPossibleB = false;
-            }
-            if(isPossibleA) {   //a가 1이 아닌 경우
-                for(int i=2; i<arrayA.length; i++) {
-                    a = gcd(a, arrayA[i]);
-                }
-                for(int i=0; i<arrayB.length; i++) {
-                    if(arrayB[i] % a == 0) {    //A의 최대공약수가 B에서도 나누어지면 안됨
-                        isPossibleA = false;
-                        break;
-                    }
-                }
-            }
-            if(isPossibleB) {
-                for(int i=2; i<arrayB.length; i++) {
-                    b = gcd(b, arrayB[i]);
-                }
-                for(int i=0; i<arrayA.length; i++) {
-                    if(arrayA[i] % b == 0) {    //B의 최대공약수가 A에서 나누어지면 안됨
-                        isPossibleB = false;
-                        break;
-                    }
-                }
-            }
-        }
-        else {
-            a = arrayA[0];
-            b = arrayB[0];
-            if(a % b == 0) {
-                isPossibleB = false;
-            }
-            if(b % a == 0) {
-                isPossibleA = false;
-            } 
-        }
-        // System.out.println(isPossibleA + " " +isPossibleB);
+        int candidateA = (gcdA > 1) ? checkCondition(gcdA, arrayB) : 0;
+        int candidateB = (gcdB > 1) ? checkCondition(gcdB, arrayA) : 0;
         
-        if(isPossibleA && isPossibleB) {    //A와 B 배열 모두 답인 값이 있는 경우 -> 더 큰 값
-            answer = Math.max(a, b);
-        }
-        if(isPossibleB && !isPossibleA) {
-            answer = b;
-        }
-        if(isPossibleA && !isPossibleB) {
-            answer = a;
-        }
-        
-        return answer;
+        return Math.max(candidateA, candidateB);
     }
-    public static int gcd(int a, int b) {
-        if(b==0) {
+    public int checkCondition(int gcdValue, int[] otherArray) {
+        for(int num : otherArray) {
+            if(num % gcdValue == 0) {
+                return 0;   //조건 만족 안함(나누어짐)
+            }
+        }
+        return gcdValue;
+    }
+    public int getGCD(int[] array) {
+        int gcdValue = array[0];    //첫 배열값
+        
+        for(int i=1; i<array.length; i++) {
+            gcdValue = gcd(gcdValue, array[i]);
+            if(gcdValue == 1) {
+                return 1;
+            }
+        }
+        return gcdValue;
+    }
+    public int gcd(int a, int b) {
+        if(b == 0) {
             return a;
         }
         return gcd(b, a % b);
