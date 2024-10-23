cf24f0dd889393088f1442782b2e8a209728e1d2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-23 14:13:42 +0900

    [level 2] Title: 택배상자, Time: 165.90 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,34 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Queue<Integer> original = new ArrayDeque<>();
         Stack<Integer> stack = new Stack<>();
-        
-        for(int i=1; i<=order.length; i++) {    //기존 컨테이너 벨트
-            original.offer(i);
-        }
+
+        int cnt = 0;
         for(int i=0; i<order.length; i++) {
             int now = order[i];
+            stack.push(i+1);
             
-            while(!original.isEmpty()) {
-                if(now > original.peek()) {
-                    stack.push(original.poll());    //보조 컨테이너 등록
+            while(!stack.isEmpty()) {
+                if(stack.peek() == order[cnt]) {
+                    stack.pop();
+                    answer++;
+                    cnt++;
                 }
                 else {
                     break;
                 }
             }
-            if(!original.isEmpty() && now == original.peek()) {   //현재 차례 상자라면
-                original.poll();
-                answer++;
-            }
-            else if(now == stack.peek()) {   //보조 컨테이너에 맨 위에 있는 상자 차례라면
-                stack.pop();
-                answer++;
-            }
-            else {
-                break;
-            }
+          
         }
 
         return answer;
