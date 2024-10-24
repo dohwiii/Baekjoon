49180bd2015a0641064ad436dc9243c9b385368e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-24 15:47:58 +0900

    [level 2] Title: 양궁대회, Time: 0.50 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,52 +1,47 @@
-import java.util.*;
-
 class Solution {
-    static int[] result = {-1};
     static int maxDiff = 0;
+    static int[] result = {-1};
     
-    public int[] solution(int n, int[] info) {
-        int[] answer = {};
+    public int[] solution(int n, int[] info) {  //화살 개수, 어피치 과녁 화살 개수
         int[] ryanArrows = new int[11];
-        int arrow = n;
-        
         backtrack(n, info, ryanArrows, 0, 0, 0);
+        
         return result;
     }
-    public void backtrack(int n, int[] info, int[] ryanArrows, int idx, int ryanScore, int apeachScore) {
-        if(idx == 11) { //모든 과녁 처리
-            ryanArrows[10] += n;
+    public void backtrack(int arrow, int[] info, int[] ryanArrows, int idx, int ryanScore, int apeachScore) {
+        if(idx == 11) { //모든 과녁 마침
+            ryanArrows[10] += arrow;    //남은 화살 수
             
             if(ryanScore > apeachScore) {
                 int diff = ryanScore - apeachScore;
-                if(diff > maxDiff || (diff == maxDiff && betterResult(ryanArrows))) {
+                if(diff > maxDiff || (diff == maxDiff && betterScore(ryanArrows))) {
                     maxDiff = diff;
                     result = ryanArrows.clone();
                 }
             }
-            ryanArrows[10] -= n;
+            ryanArrows[10] -= arrow;    //남은 화살 수
             return;
         }
-        int apeach = info[idx]; //현재 과녁 점수에 어피치가 쏜 화살의 수
+        int nowTarget = 10 - idx; //지금 가리키고 있는 과녁 점수
+        int apeach = info[idx]; //어피치가 쏜 화살 개수
         
         //라이언이 화살을 쏠 경우
-        if(n >= apeach + 1) {
+        if(arrow >= (apeach + 1)) {
             ryanArrows[idx] = apeach + 1;
-            backtrack(n - (apeach + 1), info, ryanArrows, idx + 1, ryanScore + (10 - idx), apeachScore);
-            ryanArrows[idx] = 0;    //백트래킹
+            backtrack(arrow - (apeach + 1), info, ryanArrows, idx + 1, ryanScore + nowTarget, apeachScore);
+            ryanArrows[idx] = 0;
         }
         
-        //라이언이 화살을 쏘지 않을경우
+        //라이언이 화살 포기
         ryanArrows[idx] = 0;
         if(apeach > 0) {
-            backtrack(n, info, ryanArrows, idx + 1, ryanScore, apeachScore + (10 - idx));
+            backtrack(arrow, info, ryanArrows, idx + 1, ryanScore, apeachScore + nowTarget);
         }
-        else {
-            backtrack(n, info, ryanArrows, idx + 1, ryanScore, apeachScore);    //점수 누구도 못가져감
+        else {  //어피치는 해당 과녁에 0발을 쏨
+            backtrack(arrow, info, ryanArrows, idx + 1, ryanScore, apeachScore);
         }
-           
-        
     }
-    public boolean betterResult(int[] ryanArrows) {
+    public boolean betterScore(int[] ryanArrows) {
         for(int i=10; i>=0; i--) {
             if(ryanArrows[i] > result[i]) {
                 return true;
