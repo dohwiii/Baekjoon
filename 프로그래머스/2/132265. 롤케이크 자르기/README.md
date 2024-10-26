7e8652693276d1a7fc9b3007ac5dfbdd9c91f8fe
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-26 19:12:17 +0900

    [level 2] Title: 롤케이크 자르기, Time: 87.07 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java#) 
 
 ### 성능 요약
 
-메모리: 180 MB, 시간: 365.13 ms
+메모리: 146 MB, 시간: 87.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 26일 19:09:19
+2024년 10월 26일 19:12:16
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,27 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Map<Integer, Integer> mapA = new HashMap<>();
-        Map<Integer, Integer> mapB = new HashMap<>();
+        int n = topping.length;
         
-        for(int t : topping) {
-            mapB.put(t, mapB.getOrDefault(t, 0) + 1);
+        // 오른쪽에서의 고유 토핑 개수를 저장할 배열
+        int[] rightUniqueCount = new int[n];
+        Set<Integer> rightSet = new HashSet<>();
+        
+        // 오른쪽 고유 토핑 개수 계산 (뒤에서부터 누적)
+        for (int i = n - 1; i >= 0; i--) {
+            rightSet.add(topping[i]);
+            rightUniqueCount[i] = rightSet.size();
         }
-        for(int i=0; i<topping.length; i++) {
-            int t = topping[i];
-            mapA.put(t, mapA.getOrDefault(t, 0) + 1);
+        
+        // 왼쪽에서 고유 토핑 수를 관리하는 Set
+        Set<Integer> leftSet = new HashSet<>();
+        
+        // 왼쪽에서부터 순회하면서 공평하게 나눌 수 있는 지점 확인
+        for (int i = 0; i < n - 1; i++) {
+            leftSet.add(topping[i]);
             
-            if(mapB.get(t) == 1) {
-                mapB.remove(t);
-            }
-            else if(mapB.get(t) > 1) {
-                mapB.put(t, mapB.get(t) - 1);
-            }
-            if(mapA.size() == mapB.size()) {
+            // 왼쪽과 오른쪽의 고유 토핑 개수가 같으면 카운트 증가
+            if (leftSet.size() == rightUniqueCount[i + 1]) {
                 answer++;
             }
         }
-        
         
         return answer;
     }
