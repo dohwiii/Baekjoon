581f6d647e9da8304c767400a43d76799d684273
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-27 11:26:47 +0900

    [level 2] Title: 할인 행사, Time: 47.05 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,26 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
+        boolean isPossible = true;
+        Map<String, Integer> map = new HashMap<>(); //사야할 제품과 수량
+        Map<String, Integer> buy = new HashMap<>(); //할인 제품 산 것
+        for(int i=0; i<want.length; i++) {
+            map.put(want[i], number[i]);
+        } 
+        for(int i=0; i<10; i++) {
+            buy.put(discount[i], buy.getOrDefault(discount[i], 0) + 1);
+        }
+        if(checkMatches(map, buy)) {
+            answer++;
+        }
+        int quantity = 0;
+        for(int i=1; i<discount.length - 9; i++) {
+            isPossible = true;
 
-        for (int i=0; i<want.length; i++) {
-            while (number[i]-- > 0) {
-                list.add(want[i]);
+            String prev = discount[i - 1];
+            quantity = buy.get(prev);
+            if(quantity == 1) {
+                buy.remove(prev);
+            }
+            else if(quantity > 1) {
+                buy.put(prev, quantity - 1);
+            }
+
+            String next = discount[i + 9];
+            quantity = buy.getOrDefault(next, 0);
+            buy.put(next, quantity + 1);
+
+            //수량 맞는지 확인
+            if(checkMatches(map, buy)) {
+                answer++;
             }
         }
-
-        for (int i=0; i<discount.length-list.size()+1;i ++) {
-            ArrayList<String> l = (ArrayList<String>) list.clone();
-            for (int j=i; j<i+list.size(); j++) {
-                if (l.contains(discount[j])) {
-                    l.remove(discount[j]);
-                } else {
-                    break;
-                }
-            }
-            answer += l.size()==0 ? 1 : 0;
-        }
-
+        
         return answer;
+    }
+    public boolean checkMatches(Map<String, Integer> wantMap, Map<String, Integer> buyMap) {
+        for(String key : wantMap.keySet()) {
+            if(!buyMap.getOrDefault(key, 0).equals(wantMap.get(key))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
