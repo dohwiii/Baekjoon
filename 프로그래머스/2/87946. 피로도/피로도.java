34b30d20ddca52a49d0e859e6d06719dbe3232e9
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-28 10:15:14 +0900

    [level 2] Title: 피로도, Time: 10.17 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,24 @@
+class Solution {
+    static int maxDungeon = Integer.MIN_VALUE;
+    static boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        for(int i=0; i<dungeons.length; i++) {
+            int[] dun = dungeons[i];
+            solve(k, dungeons, 0);
+        }
+        return maxDungeon;
+    }
+    public void solve(int fatigue, int[][] dungeons, int dungeon) {
+        maxDungeon = Math.max(maxDungeon, dungeon);
+        
+        for(int i=0; i<dungeons.length; i++) {
+            if(!visited[i] && dungeons[i][0] <= fatigue) {
+                visited[i] = true;
+                solve(fatigue - dungeons[i][1], dungeons, dungeon + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
