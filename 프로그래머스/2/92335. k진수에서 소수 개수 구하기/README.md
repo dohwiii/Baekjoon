f39b05b001dfe70be4718177d4027cacb90fd8cb
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-10-28 09:12:02 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.15 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 69.4 MB, 시간: 25.15 ms
+메모리: 76.6 MB, 시간: 11.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 28일 09:04:38
+2024년 10월 28일 09:12:00
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,28 +1,17 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String transform = Integer.toString(n, k); //n을 k진수로 변환(String)
-        String sentence = "";
+        String[] transform = Integer.toString(n, k).split("0"); //n을 k진수로 변환(String)
         
-        for(int i=0; i<transform.length(); i++) {
-            char c = transform.charAt(i);
-            if(c == '0') {
-                if(sentence.length() > 0) {
-                    if(isPrime(sentence)) {
-                        answer++;
-                    }
-                }
-                sentence = "";
+        for(String sen : transform) {
+            if(sen.isEmpty()) {
+                continue;
             }
-            else {
-                sentence += c;
-            }
-        }
-        if(sentence.length() > 0) {
-            if(isPrime(sentence)) {
+            if(isPrime(sen)) {
                 answer++;
             }
         }
+
         return answer;
     }
     public boolean isPrime(String n) {   //소수 판별
