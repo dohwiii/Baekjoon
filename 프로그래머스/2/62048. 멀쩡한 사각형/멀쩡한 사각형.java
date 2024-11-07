0160f1833b087898a6f1d6f29bdb67add3d64350
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-07 17:29:42 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 889.95 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 class Solution {
     public long solution(int w, int h) {
-        long answer = 0;
-        double slope = -1 * h / (double) w;     
+        long answer = 0;  
         double prev = h;   //y절편
         
         for(long i=1; i<=w; i++) {
@@ -11,12 +10,6 @@ class Solution {
             double diff = prev - Math.floor(temp);
             answer += diff;
             prev = Math.ceil(temp);
-            
-            // System.out.println(i);
-            // System.out.println(temp);
-            // System.out.println(diff);
-            // System.out.println(prev);
-            // System.out.println();
         }
         
         return (long) w*h - answer;
