bfaba768c1d8ec99c32aef0e60556e37d79e851f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-08 22:16:46 +0900

    [level 2] Title: 방문 길이, Time: 1.55 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,47 +1,37 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Set;
 
 class Solution {
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static Map<String, Integer> map = new HashMap<>();
-    
     public int solution(String dirs) {
-        int answer = 0;
-        int x = 0;
-        int y = 0;
-        for(int i=0; i<dirs.length(); i++) {
-            int index = direction(dirs.charAt(i));
-            int nx = x + dx[index];
-            int ny = y + dy[index];
+        Set<String> visited = new HashSet<>();
+        int x = 0, y = 0;
+        
+        for (char dir : dirs.toCharArray()) {
+            int nx = x, ny = y;
             
-            if(nx < -5 || nx > 5 || ny < -5 || ny > 5) {
-                continue;
+            switch (dir) {
+                case 'U': ny++; break;
+                case 'D': ny--; break;
+                case 'R': nx++; break;
+                case 'L': nx--; break;
             }
-            String route1 = x + " "+y + " "+ nx +" "+ny;
-            String route2 = nx + " "+ ny + " "+ x +" "+y;
-            if(!map.containsKey(route1) && !map.containsKey(route2)) {   //처음 가는 길
-                answer++;
-                map.put(route1, 1);
-                map.put(route2, 1);
+            
+            if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
+            
+            // 배열을 문자열로 변환하여 경로 저장
+            String path1 = Arrays.toString(new int[]{x, y, nx, ny});
+            String path2 = Arrays.toString(new int[]{nx, ny, x, y});
+            
+            if (!visited.contains(path1) && !visited.contains(path2)) {
+                visited.add(path1);
+                visited.add(path2);
             }
+            
             x = nx;
             y = ny;
         }
-        return answer;
-    }
-
-    public int direction(char c) {
-        int di = 0;
-        switch(c) {
-            case 'U': di = 2;
-                    break;
-            case 'D': di = 3; 
-                    break;
-            case 'R': di = 0;
-                    break;
-            case 'L': di = 1;
-                    break;
-        }
-        return di;
+        
+        return visited.size() / 2;
     }
 }
