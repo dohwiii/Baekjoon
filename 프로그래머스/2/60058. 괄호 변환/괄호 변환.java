f7e77ad30a5e642284d10e115d27be6662e94783
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-08 12:21:54 +0900

    [level 2] Title: 괄호 변환, Time: 1.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class Solution {
+    static StringBuilder sb = new StringBuilder();
+    public String solution(String p) {
+        String answer = "";
+        String str = p;
+        if(isRightSentence(p)) {
+            return p;
+        }
+        splitStr(p);
+        
+        return sb.toString();
+    }
+    
+    public void splitStr(String str) {
+        if(str.isEmpty()) {
+            return;
+        }
+        int left = 0;
+        int right = 0;
+        int index = 0;
+        
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
+            if(left == right) {
+                index = i;
+                break;
+            }
+        }
+        index = index + 1;
+        String u = str.substring(0, index);
+        String v = str.substring(index, str.length());
+        // System.out.println(u +" "+v);
+        
+        if(isRightSentence(u)) {
+            sb.append(u);
+            splitStr(v);
+        }
+        else {
+            sb.append("(");
+            splitStr(v);
+            sb.append(")");
+            String temp = u.substring(1, u.length() - 1);
+            for(int i=0; i<temp.length(); i++) {
+                if(temp.charAt(i) == '(') {
+                    sb.append(")");
+                }
+                else {
+                    sb.append("(");
+                }
+            }
+        }
+    }
+    //올바른 괄호 문자열인지
+    public boolean isRightSentence(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == '(') {
+                stack.push(str.charAt(i));
+            }
+            else if(str.charAt(i) == ')') {
+                if(!stack.isEmpty()) {
+                    stack.pop();    // '(' 빼기
+                }
+                else {
+                    return false;
+                }
+            }        
+        }
+        return stack.isEmpty();
+    }
+}
