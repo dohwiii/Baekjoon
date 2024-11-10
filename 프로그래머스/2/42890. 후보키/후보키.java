2f60243a534868816dfa8660f1717c7dd63b6440
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-10 18:37:55 +0900

    [level 2] Title: 후보키, Time: 9.48 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,35 +2,50 @@ import java.util.*;
 
 class Solution {
     static int N, M;
-    static int answer;
-    static List<Integer> candidateKeys;
+    static List<Set<Integer>> candidateKeys; // 후보키 목록을 Set<Integer>로 저장
+    static int answer = 0;
 
     public int solution(String[][] relation) {
-        N = relation.length;    // 행
-        M = relation[0].length; // 열(속성 개수)
+        N = relation.length;    // 행의 수 (튜플 개수)
+        M = relation[0].length; // 열의 수 (속성 개수)
         candidateKeys = new ArrayList<>();
 
-        for (int i = 1; i < (1 << M); i++) {
-            if (isUnique(i, relation) && isMinimal(i)) {
-                candidateKeys.add(i);
-            }
+        // 1개 속성 조합부터 M개 속성 조합까지 생성
+        for (int r = 1; r <= M; r++) {
+            findCombinations(new HashSet<>(), 0, r, relation);
         }
 
-        return candidateKeys.size();
+        return candidateKeys.size(); // 후보키의 개수
+    }
+
+    // 조합을 생성하고 유일성, 최소성 검사를 수행하는 메소드
+    private void findCombinations(Set<Integer> combination, int start, int r, String[][] relation) {
+        // r개의 조합이 완성되면 유일성 및 최소성 검사
+        if (combination.size() == r) {
+            if (isUnique(combination, relation) && isMinimal(combination)) {
+                candidateKeys.add(new HashSet<>(combination)); // 후보키로 추가
+            }
+            return;
+        }
+
+        // 조합 생성 (백트래킹)
+        for (int i = start; i < M; i++) {
+            combination.add(i);
+            findCombinations(combination, i + 1, r, relation);
+            combination.remove(i); // 백트래킹
+        }
     }
 
     // 유일성 검사
-    private boolean isUnique(int subset, String[][] relation) {
-        Set<String> seen = new HashSet<>();
-        
-        for (String[] row : relation) {
+    private boolean isUnique(Set<Integer> combination, String[][] relation) {
+        Set<String> uniqueCheck = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
             StringBuilder key = new StringBuilder();
-            for (int j = 0; j < M; j++) {
-                if ((subset & (1 << j)) != 0) {  // j번째 속성이 subset에 포함되면 추가
-                    key.append(row[j]).append(",");
-                }
+            for (int col : combination) {
+                key.append(relation[i][col]).append(",");
             }
-            if (!seen.add(key.toString())) {  // 이미 존재하는 키이면 유일성 실패
+            if (!uniqueCheck.add(key.toString())) { // 유일성 검사 실패
                 return false;
             }
         }
@@ -38,10 +53,10 @@ class Solution {
     }
 
     // 최소성 검사
-    private boolean isMinimal(int subset) {
-        for (int key : candidateKeys) {
-            if ((key & subset) == key) { // subset이 key를 포함하고 있으면 최소성 실패
-                return false;
+    private boolean isMinimal(Set<Integer> combination) {
+        for (Set<Integer> key : candidateKeys) {
+            if (combination.containsAll(key)) {
+                return false; // 기존 후보키의 부분집합이면 최소성 실패
             }
         }
         return true;
