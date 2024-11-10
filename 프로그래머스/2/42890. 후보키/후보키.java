76f75848753ccb317e96be385963adf71811b035
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-10 18:26:31 +0900

    [level 2] Title: 후보키, Time: 4.51 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,84 +2,48 @@ import java.util.*;
 
 class Solution {
     static int N, M;
-    static boolean[] candidate;
-    static Set<String> confirmCandidate;
     static int answer;
-    
+    static List<Integer> candidateKeys;
+
     public int solution(String[][] relation) {
-        N = relation.length;    //행
-        M = relation[0].length; //열(속성 개수)
-        candidate = new boolean[M];
-        confirmCandidate = new HashSet<>();
+        N = relation.length;    // 행
+        M = relation[0].length; // 열(속성 개수)
+        candidateKeys = new ArrayList<>();
+
+        for (int i = 1; i < (1 << M); i++) {
+            if (isUnique(i, relation) && isMinimal(i)) {
+                candidateKeys.add(i);
+            }
+        }
+
+        return candidateKeys.size();
+    }
+
+    // 유일성 검사
+    private boolean isUnique(int subset, String[][] relation) {
+        Set<String> seen = new HashSet<>();
         
-        for(int i=1; i<=M; i++) {
-            candidate = new boolean[M];
-            solve(0, i, relation, 0);
-        }
-        return answer;
-    }
-    public void solve(int depth, int candidateCnt, String[][] relation, int index) {  //순열
-        if(depth == candidateCnt) { //개수만큼 다 뽑았을 때
-            List<Integer> list = new ArrayList<>();
-            for(int i=0; i<M; i++) {
-                if(candidate[i]) {
-                    list.add(i);
+        for (String[] row : relation) {
+            StringBuilder key = new StringBuilder();
+            for (int j = 0; j < M; j++) {
+                if ((subset & (1 << j)) != 0) {  // j번째 속성이 subset에 포함되면 추가
+                    key.append(row[j]).append(",");
                 }
             }
-            if(checkCandidateKey(list, relation)) {
-                answer++;
-            }
-            return;
-        }
-        for(int i=index; i<M; i++) {
-            if(!candidate[i]) {
-                candidate[i] = true;
-                solve(depth + 1, candidateCnt, relation, i+1);
-                candidate[i] = false;
-            }
-        }
-    }
-    public boolean checkCandidateKey(List<Integer> colList, String[][] relation) {
-        List<String> rowList = new ArrayList<>();
-        //후보키 중에 같은 값이 있는지 걸러냄(유일성)
-        for(int i=0; i<N; i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int col : colList) {
-                sb.append(relation[i][col]);
-            }
-            String str = sb.toString();
-            if(rowList.contains(str)) { //이미 값이 존재한다면
-                return false;
-            }
-            rowList.add(str);
-        }
-        //같은 값을 안가지는 컬럼집합 중에서 최소키가 만족하는지(최소성)
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
-        for(int i : colList) {
-            list.add(i);
-            sb.append(i);
-        }
-        String str = sb.toString();
-        // System.out.println(str);
-        for(String s : confirmCandidate) {
-            if(s.contains(str) || str.contains(s)) {
-                // System.out.println(str + " " + s);
+            if (!seen.add(key.toString())) {  // 이미 존재하는 키이면 유일성 실패
                 return false;
             }
         }
-        for(String s : confirmCandidate) {  //02
-            int index = 0;
-            for(char str_c : str.toCharArray()) {   //012
-                if(s.charAt(index) == str_c) {
-                    index++;
-                }
-                if(index == s.length()) {
-                    return false;
-                }
+        return true;
+    }
+
+    // 최소성 검사
+    private boolean isMinimal(int subset) {
+        for (int key : candidateKeys) {
+            if ((key & subset) == key) { // subset이 key를 포함하고 있으면 최소성 실패
+                return false;
             }
         }
-        confirmCandidate.add(str);
         return true;
     }
 }
