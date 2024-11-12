9fd5e874fb548e52867b362747798f608600f87e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-12 13:00:55 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -8,12 +8,10 @@ class Solution {
         
         for(int i=3; i <= (int) Math.sqrt(total); i++) {
             if(total % i == 0) {
-                int a = total/i;
-                int b = i;
-                int edge = 2 * a + 2 * (b - 2);
-                if(edge == brown) {
-                    answer = new int[]{Math.max(a, b), Math.min(a, b)};
-                    break;
+                int width = total/i;
+                int height = i;
+                if(2 * width + 2 * (height - 2) == brown) {
+                    return answer = new int[]{width, height};
                 }
             } 
         }
