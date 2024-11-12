9538f5b494991f10ab09457a77453caecddfcb68
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-12 12:57:42 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow; // 전체 카펫 넓이
-
-        for (int height = 3; height <= (int) Math.sqrt(total); height++) {
-            if (total % height == 0) {
-                int width = total / height;
-
-                if ((width - 2) * (height - 2) == yellow) {
-                    return new int[]{width, height}; // 가로 길이가 더 크거나 같으므로 바로 반환
-                }
-            }
-        }
+        int[] answer = new int[2];
+        int total = brown + yellow;
+        List<int[]> list = new ArrayList<>();
         
-        return new int[0]; // 문제가 없는 이상 여기로 올 수 없음
+        for(int i=3; i <= (int) Math.sqrt(total); i++) {
+            if(total % i == 0) {
+                int a = total/i;
+                int b = i;
+                int edge = 2 * a + 2 * (b - 2);
+                if(edge == brown) {
+                    answer = new int[]{Math.max(a, b), Math.min(a, b)};
+                    break;
+                }
+            } 
+        }
+
+        return answer;
     }
-}
+} 
