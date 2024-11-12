6e87bd433c3730f5cc1ab5f082bdab13ced7fa0e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-12 11:40:58 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,23 +3,22 @@ class Solution {
         int answer = 0;
         int length = name.length();
         
-        //알파벳 변경 횟수 계산
-        for(int i=0; i<length; i++) {
+        //알파벳 변경 횟수
+        for(int i=0; i<name.length(); i++) {
             char c = name.charAt(i);
             answer += Math.min(c - 'A', 'Z' - c + 1);
         }
-        
-        int minMove = length - 1;
+        int minMove = length - 1;   //정방향으로 쭉 가기
         for(int i=0; i<length; i++) {
-            int next = i + 1;
+            int nextIndex = i + 1;
             
-            while(next < length && name.charAt(next) == 'A') {
-                next++;
+            while(nextIndex < length && name.charAt(nextIndex) == 'A') {
+                nextIndex++;
             }
-            minMove = Math.min(minMove, i+length-next+Math.min(i, length - next));
+            minMove = Math.min(minMove, i + length - nextIndex + Math.min(i, (length - nextIndex)));
         }
-        answer += minMove;
         
+        answer += minMove;
         return answer;
     }
 }
