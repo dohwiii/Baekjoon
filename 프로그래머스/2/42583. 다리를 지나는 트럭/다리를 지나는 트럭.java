92b662e964acf3487378c6323cf7debadcd69bb2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-13 11:43:33 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 22.70 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,38 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new ArrayDeque<>(); // 다리 위 트럭 상태 (큐)
-        int totalWeightOnBridge = 0; // 다리 위 트럭의 총 무게
-        int time = 0; // 경과 시간
-        int index = 0; // 대기 중인 트럭 배열의 인덱스
-
-        // 다리 길이만큼 큐를 0으로 채워서 초기화 (트럭이 없는 상태)
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
+        int answer = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for(int i=0; i<bridge_length; i++) {
+            queue.offer(0);
         }
-
-        while (!bridge.isEmpty()) {
-            time++; // 시간 경과
-
-            // 다리 맨 앞 트럭이 나가는 시간
-            int exitingTruck = bridge.poll();
-            totalWeightOnBridge -= exitingTruck; // 다리를 빠져나간 트럭의 무게 제거
-
-            // 대기 트럭이 남아있고, 다리에 올라갈 수 있는지 체크
-            if (index < truck_weights.length) {
-                if (totalWeightOnBridge + truck_weights[index] <= weight) {
-                    // 새로운 트럭을 다리에 올림
-                    bridge.offer(truck_weights[index]);
-                    totalWeightOnBridge += truck_weights[index];
+        int currentWeight = 0;
+        int index = 0;
+        while(!queue.isEmpty()) {
+            currentWeight -= queue.poll(); //앞에 있는거 빼기
+            
+            if(index < truck_weights.length) {
+                if(currentWeight + truck_weights[index] <= weight) {
+                    currentWeight += truck_weights[index];  //다리에 있는 차량의 누적 무게
+                    queue.offer(truck_weights[index]);
                     index++;
-                } else {
-                    // 트럭이 다리 위에 올라갈 수 없으면 빈 공간 추가
-                    bridge.offer(0);
+                }
+                else {  //하중을 이길 수 없음 -> 같이 못건넘
+                    queue.offer(0);
                 }
             }
+            
+            answer++;
         }
-
-        return time;
+        return answer;
     }
 }
