15beccd4f629c04e45258f2e4c75e8b5af31eb14
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-15 13:28:01 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 44.81 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -3,24 +3,22 @@ import java.util.regex.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        Pattern p = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
-
+        String[] answer = {};
+        Pattern pattern = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
         Arrays.sort(files, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2) {
-                Matcher m1 = p.matcher(s1.toLowerCase());
-                Matcher m2 = p.matcher(s2.toLowerCase());
+            public int compare(String a, String b) {
+                Matcher m1 = pattern.matcher(a.toLowerCase());
+                Matcher m2 = pattern.matcher(b.toLowerCase());
                 m1.find();
                 m2.find();
-
-                if(!m1.group(1).equals(m2.group(1))) {
-                    return m1.group(1).compareTo(m2.group(1));
-                } else {
+                
+                if(m1.group(1).equals(m2.group(1))) {
                     return Integer.parseInt(m1.group(2)) - Integer.parseInt(m2.group(2));
                 }
+                return m1.group(1).compareTo(m2.group(1));
             }
         });
-
         return files;
     }
 }
