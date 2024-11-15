8915ee052c9bd3404b4fcc18d26a63be182ae109
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-15 14:57:57 +0900

    [level 2] Title: [3차] 압축, Time: 4.46 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -3,42 +3,39 @@ import java.util.*;
 class Solution {
     public int[] solution(String msg) {
         int[] answer = {};
-        HashMap<String, Integer> dictionary = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-
+        HashMap<String, Integer> map = new HashMap<>();
         char c = 'A';
         for(int i=1; i<=26; i++) {
             String s  = String.valueOf(c);
-            dictionary.put(s, i);
+            map.put(s, i);
             c++;
         }
+        List<Integer> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        int dictIndex = 27;
+        int alpIndex = 27;
         int index = 0;
         
         while(index < msg.length()) {
-            String current = String.valueOf(msg.charAt(index));
-            sb = new StringBuilder(current);
-            int nextIndex = index + 1;
-            
-            while(nextIndex <= msg.length() && dictionary.containsKey(sb.toString())) {
-                if(nextIndex < msg.length()) {
-                    sb.append(msg.charAt(nextIndex));
-                }
-                nextIndex++;
-            }
-            if(!dictionary.containsKey(sb.toString())) {    //마지막 글자가 사전에 없을 경우
-                result.add(dictionary.get(sb.toString().substring(0, sb.length() - 1)));   //색인번호 추출
-                dictionary.put(sb.toString(), dictIndex++); //사전에 입력
-                index = nextIndex - 2;
-            }
-            else {  //사전에 있을 경우
-                result.add(dictionary.get(sb.toString()));
-                index = nextIndex - 1;
+            char now = msg.charAt(index);
+            sb.append(now);
+            // System.out.println(sb.toString());
+            if(!map.containsKey(sb.toString())) {   //사전에 일치하는 문자 없음
+                map.put(sb.toString(), alpIndex++); //새로운 문자열 사전에 입력
+                sb.deleteCharAt(sb.length() - 1);   //일치하지 않은 알파벳 삭제
+                int dicIndex = map.get(sb.toString());  //사전 인덱스
+                list.add(dicIndex); //사전 색인번호 출력
+                sb.setLength(0);
+                sb.append(now);
             }
             index++;
         }
+        if(sb.length() > 0) {
+            list.add(map.get(sb.toString()));
+        }
+        // for(int i : list) {
+        //     System.out.println(i);
+        // }
 
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
