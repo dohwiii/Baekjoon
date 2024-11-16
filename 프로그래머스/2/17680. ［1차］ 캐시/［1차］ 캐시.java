2f394df2c25109f04c0d7485173382abe33fb0d5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-16 21:41:00 +0900

    [level 2] Title: [1차] 캐시, Time: 40.03 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,35 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        Queue<String> queue = new ArrayDeque<>();
+        LinkedHashSet<String> cache = new LinkedHashSet<>();
         
-        for(String city : cities) {
-            boolean isFind = false;
-            if(!queue.isEmpty()) {
-                for(String s : queue) {
-                    if(city.equalsIgnoreCase(s)) {
-                        answer += 1;
-                        isFind = true;
-                        queue.remove(s);
-                        queue.offer(city);
-                        break;
+        for (String city : cities) {
+            city = city.toLowerCase(); // 대소문자 구분 제거
+            if (cache.contains(city)) { // 캐시 히트
+                answer += 1;
+                cache.remove(city); // 기존 위치에서 제거
+                cache.add(city); // 최신 위치로 이동
+            } else { // 캐시 미스
+                answer += 5;
+                if (cache.size() >= cacheSize && cacheSize > 0) { // 캐시 용량 초과 시 제거
+                    Iterator<String> it = cache.iterator();
+                    if (it.hasNext()) {
+                        it.next();
+                        it.remove();
                     }
                 }
-                if(!isFind) {   //같은 도시가 캐시에 없다면
-                    if(queue.size() >= cacheSize) { //캐시 용량이 이미 찼다면
-                        queue.poll();
-                    }
-                    queue.offer(city);
-                    answer += 5;
-                }
-            }
-            else {  //큐에 아무것도 없어서 넣어야함
-                if(cacheSize > 0) {
-                    queue.offer(city);
-                    answer += 5;
-                }
-                else {  //큐 사이즈 0일 때
-                    answer += 5;
+                if (cacheSize > 0) {
+                    cache.add(city); // 새로운 도시 추가
                 }
             }
         }
