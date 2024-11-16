a08adc58ef718cf4c191845a9cc22f7d880263ed
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-16 21:58:22 +0900

    [level 2] Title: [1차] 캐시, Time: 36.60 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,38 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        Queue<String> queue = new ArrayDeque<>();
-        
-        for(String city : cities) {
-            boolean isFind = false;
-            if(!queue.isEmpty()) {
-                for(String s : queue) {
-                    if(city.equalsIgnoreCase(s)) {
-                        answer += 1;
-                        isFind = true;
-                        queue.remove(s);
-                        queue.offer(city);
-                        break;
-                    }
-                }
-                if(!isFind) {   //같은 도시가 캐시에 없다면
-                    if(queue.size() >= cacheSize) { //캐시 용량이 이미 찼다면
-                        queue.poll();
-                    }
-                    queue.offer(city);
-                    answer += 5;
-                }
+
+        // LinkedHashMap으로 LRU 캐시 구현 (accessOrder = true)
+        LinkedHashMap<String, Boolean> cache = new LinkedHashMap<>(cacheSize, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<String, Boolean> eldest) {
+                return size() > cacheSize;
             }
-            else {  //큐에 아무것도 없어서 넣어야함
-                if(cacheSize > 0) {
-                    queue.offer(city);
-                    answer += 5;
-                }
-                else {  //큐 사이즈 0일 때
-                    answer += 5;
-                }
+        };
+
+        for (String city : cities) {
+            city = city.toLowerCase(); // 대소문자 구분 제거
+            if (cache.containsKey(city)) { // 캐시 히트
+                answer += 1;
+            } else { // 캐시 미스
+                answer += 5;
             }
+            cache.put(city, true); // 캐시에 추가 (이미 존재하면 위치 갱신)
         }
+
         return answer;
     }
 }
