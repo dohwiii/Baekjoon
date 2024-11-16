55b2aa9326bbf78f9c2537fa8fd4abc2d321bb02
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-16 21:41:29 +0900

    [level 2] Title: [1차] 캐시, Time: 63.87 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,25 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        LinkedHashSet<String> cache = new LinkedHashSet<>();
+        Queue<String> queue = new ArrayDeque<>();
         
-        for (String city : cities) {
-            city = city.toLowerCase(); // 대소문자 구분 제거
-            if (cache.contains(city)) { // 캐시 히트
-                answer += 1;
-                cache.remove(city); // 기존 위치에서 제거
-                cache.add(city); // 최신 위치로 이동
-            } else { // 캐시 미스
-                answer += 5;
-                if (cache.size() >= cacheSize && cacheSize > 0) { // 캐시 용량 초과 시 제거
-                    Iterator<String> it = cache.iterator();
-                    if (it.hasNext()) {
-                        it.next();
-                        it.remove();
+        for(String city : cities) {
+            boolean isFind = false;
+            if(!queue.isEmpty()) {
+                for(String s : queue) {
+                    if(city.equalsIgnoreCase(s)) {
+                        answer += 1;
+                        isFind = true;
+                        queue.remove(s);
+                        queue.offer(city);
+                        break;
                     }
                 }
-                if (cacheSize > 0) {
-                    cache.add(city); // 새로운 도시 추가
+                if(!isFind) {   //같은 도시가 캐시에 없다면
+                    if(queue.size() >= cacheSize) { //캐시 용량이 이미 찼다면
+                        queue.poll();
+                    }
+                    queue.offer(city);
+                    answer += 5;
+                }
+            }
+            else {  //큐에 아무것도 없어서 넣어야함
+                if(cacheSize > 0) {
+                    queue.offer(city);
+                    answer += 5;
+                }
+                else {  //큐 사이즈 0일 때
+                    answer += 5;
                 }
             }
         }
