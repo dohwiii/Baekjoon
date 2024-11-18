799913b38ac4f0fecaedeb9334933c38e091f9b7
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-18 13:09:49 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 21.37 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -5,22 +5,47 @@ class Solution {
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
         
-        // 다중집합 생성
+        // 유효한 2글자 쌍 추출
         extractValidPairs(str1, list1);
         extractValidPairs(str2, list2);
         
-        // 리스트 정렬
-        Collections.sort(list1);
-        Collections.sort(list2);
+        // 정렬 및 교집합, 합집합 계산
+        int[] result = mergeAndCalculate(list1, list2);
+        int intersection = result[0];
+        int union = result[1];
         
-        // Merge 과정에서 교집합, 합집합 계산
+        // 공집합 처리
+        if (union == 0) {
+            return 65536;
+        }
+        
+        // 자카드 유사도 계산
+        double similarity = (double) intersection / union;
+        return (int) (similarity * 65536);
+    }
+    
+    private void extractValidPairs(String str, List<String> list) {
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length() - 1; i++) {
+            char front = str.charAt(i);
+            char back = str.charAt(i + 1);
+            if (Character.isLetter(front) && Character.isLetter(back)) {
+                list.add("" + front + back);
+            }
+        }
+    }
+    
+    private int[] mergeAndCalculate(List<String> list1, List<String> list2) {
+        List<String> sortedList1 = mergeSort(list1);
+        List<String> sortedList2 = mergeSort(list2);
+
         int intersection = 0;
         int union = 0;
-        int i = 0, j = 0;
         
-        while (i < list1.size() && j < list2.size()) {
-            String elem1 = list1.get(i);
-            String elem2 = list2.get(j);
+        int i = 0, j = 0;
+        while (i < sortedList1.size() && j < sortedList2.size()) {
+            String elem1 = sortedList1.get(i);
+            String elem2 = sortedList2.get(j);
             
             if (elem1.equals(elem2)) {
                 intersection++;
@@ -36,35 +61,53 @@ class Solution {
             }
         }
         
-        // 남은 요소 처리
-        while (i < list1.size()) {
+        while (i < sortedList1.size()) {
             union++;
             i++;
         }
-        while (j < list2.size()) {
+        while (j < sortedList2.size()) {
             union++;
             j++;
         }
         
-        // 공집합 처리
-        if (union == 0) {
-            return 65536;
-        }
-        
-        // 유사도 계산
-        double similarity = (double) intersection / union;
-        return (int) (similarity * 65536);
+        return new int[] {intersection, union};
     }
     
-    // 유효한 2글자 쌍을 리스트에 추가하는 메서드
-    private void extractValidPairs(String str, List<String> list) {
-        str = str.toLowerCase();
-        for (int i = 0; i < str.length() - 1; i++) {
-            char front = str.charAt(i);
-            char back = str.charAt(i + 1);
-            if (Character.isLetter(front) && Character.isLetter(back)) {
-                list.add("" + front + back);
+    private List<String> mergeSort(List<String> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+        
+        int mid = list.size() / 2;
+        List<String> left = mergeSort(list.subList(0, mid));
+        List<String> right = mergeSort(list.subList(mid, list.size()));
+        
+        return merge(left, right);
+    }
+    
+    private List<String> merge(List<String> left, List<String> right) {
+        List<String> result = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareTo(right.get(j)) <= 0) {
+                result.add(left.get(i));
+                i++;
+            } else {
+                result.add(right.get(j));
+                j++;
             }
         }
+        
+        while (i < left.size()) {
+            result.add(left.get(i));
+            i++;
+        }
+        while (j < right.size()) {
+            result.add(right.get(j));
+            j++;
+        }
+        
+        return result;
     }
 }
