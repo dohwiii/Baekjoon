d602ad868fd0da773975f31c0addce105f17cd96
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-18 13:21:06 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 10.91 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,67 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
-        String[] arr1 = extractValidPairs(str1);
-        String[] arr2 = extractValidPairs(str2);
+        Map<String, int[]> map = new HashMap<>();
         
-        // 정렬
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        // Merge 과정에서 교집합, 합집합 계산
-        int[] result = mergeAndCalculate(arr1, arr2);
-        int intersection = result[0];
-        int union = result[1];
-        
+        // 소문자로 변환
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // 다중집합 생성
+        for (int i = 0; i < str1.length() - 1; i++) {
+            char front = str1.charAt(i);
+            char back = str1.charAt(i + 1);
+            if (Character.isLetter(front) && Character.isLetter(back)) {
+                String str = "" + front + back;
+                map.putIfAbsent(str, new int[2]);
+                map.get(str)[0]++;
+            }
+        }
+        for (int i = 0; i < str2.length() - 1; i++) {
+            char front = str2.charAt(i);
+            char back = str2.charAt(i + 1);
+            if (Character.isLetter(front) && Character.isLetter(back)) {
+                String str = "" + front + back;
+                map.putIfAbsent(str, new int[2]);
+                map.get(str)[1]++;
+            }
+        }
+
+        // 교집합과 합집합 계산
+        double intersection = 0;
+        double union = 0;
+        for (int[] counts : map.values()) {
+            intersection += Math.min(counts[0], counts[1]);
+            union += Math.max(counts[0], counts[1]);
+        }
+
         // 공집합 처리
         if (union == 0) {
             return 65536;
         }
-        
-        // 자카드 유사도 계산
-        double similarity = (double) intersection / union;
-        return (int) (similarity * 65536);
-    }
-    
-    // 유효한 2글자 쌍 추출
-    private String[] extractValidPairs(String str) {
-        str = str.toLowerCase();
-        List<String> tempList = new ArrayList<>();
-        for (int i = 0; i < str.length() - 1; i++) {
-            char front = str.charAt(i);
-            char back = str.charAt(i + 1);
-            if (Character.isLetter(front) && Character.isLetter(back)) {
-                tempList.add("" + front + back);
-            }
-        }
-        // 배열로 변환
-        return tempList.toArray(new String[0]);
-    }
-    
-    // 병합하여 교집합과 합집합 계산
-    private int[] mergeAndCalculate(String[] arr1, String[] arr2) {
-        int i = 0, j = 0;
-        int intersection = 0;
-        int union = 0;
-        
-        while (i < arr1.length && j < arr2.length) {
-            if (arr1[i].equals(arr2[j])) {
-                intersection++;
-                union++;
-                i++;
-                j++;
-            } else if (arr1[i].compareTo(arr2[j]) < 0) {
-                union++;
-                i++;
-            } else {
-                union++;
-                j++;
-            }
-        }
-        
-        // 남은 요소 처리
-        union += (arr1.length - i) + (arr2.length - j);
-        
-        return new int[] {intersection, union};
+
+        // 유사도 계산
+        double similar = intersection / union;
+        return (int) (similar * 65536);
     }
 }
