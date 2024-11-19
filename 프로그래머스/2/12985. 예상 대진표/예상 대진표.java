231eaa4100ca237c82fadbf544d07a65d1f138fa
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-19 12:35:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,41 +1,15 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int answer = 0;
-        int round = 1;
-        int totalRound = 1; //전체 라운드 횟수
-        
-        while(true) {
-            int value = (int) Math.pow(2, totalRound);
-            if(value == n) {
-                break;
-            }
-            totalRound++;
-        }
-        totalRound++;
-        int scale = round;
-        boolean isFind = false;
-        
-        while(round <= totalRound) {
-            int start = 1;
-            
-            //그룹 찾기
-            while(start < n) {
-                if(a >= start && a < start + scale && b >= start && b < start + scale) {
-                    answer = round - 1;
-                    isFind = true;
-                    break;
-                }
-                // System.out.println(start+" "+scale);
-                start += scale;
-            }
-            if(isFind) {
-                break;
-            }
-            round++;
-            scale *= 2;
-        }
-        
+        int round = 0;
 
-        return answer;
+        // a와 b가 서로 다른 그룹에 속할 때까지 반복
+        while (a != b) {
+            // 다음 라운드에서의 그룹 번호 계산
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        return round;
     }
 }
