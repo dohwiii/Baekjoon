feb50e6e43aa061f4a64d196917347529de6eee9
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-21 10:42:15 +0900

    [level 2] Title: N-Queen, Time: 179.88 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,38 +1,46 @@
 class Solution {
-    static int answer = 0;
-    static boolean[] col, diag1, diag2;
-
+    static int[][] map;
+    static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 0, 1, -1, 1, -1, -1, 1};
+    static boolean[][] visited;
+    static int answer;
+    static int[] arr;
+    
     public int solution(int n) {
-        answer = 0;
-        col = new boolean[n];
-        diag1 = new boolean[2 * n - 1]; // x + y
-        diag2 = new boolean[2 * n - 1]; // x - y + (n - 1)
+        map = new int[n][n];
+        arr = new int[n];
+        visited = new boolean[n][n];
         solve(0, n);
         return answer;
     }
+    public void solve(int depth, int n) {
+        if(depth == n) {
+			answer++;
+			return;
+		}
+		
+		for(int i = 0 ; i < n; i++) {
+			arr[depth] = i;
+			if(isPossible(depth)) {
+				solve(depth+1, n);
+			}
+		}
 
-    public void solve(int row, int n) {
-        if (row == n) { // 모든 퀸을 배치 완료
-            answer++;
-            return;
-        }
-
-        for (int c = 0; c < n; c++) {
-            if (col[c] || diag1[row + c] || diag2[row - c + n - 1]) {
-                continue; // 퀸을 놓을 수 없는 자리
-            }
-
-            // 상태 갱신
-            col[c] = true;
-            diag1[row + c] = true;
-            diag2[row - c + n - 1] = true;
-
-            solve(row + 1, n); // 다음 행으로 이동
-
-            // 상태 복구 (백트래킹)
-            col[c] = false;
-            diag1[row + c] = false;
-            diag2[row - c + n - 1] = false;
-        }
+        
+    }
+    public boolean isPossible(int col) {
+       for(int i = 0 ; i < col ; i++) {
+		//행에 일치하는게 있는지 판별
+		if(arr[i]==arr[col]) {
+			return false;
+		}
+		//대각선에 일치하는게 있는지 판별
+		else if(Math.abs(col-i) == Math.abs(arr[col]-arr[i])) {
+			return false;
+		}
+			
+		}
+		
+		return true;
     }
 }
