479fbf96bb62563466f181893f346c8d167c41b5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-23 18:07:26 +0900

    [level 3] Title: 주사위 고르기, Time: 528.84 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -2,82 +2,80 @@ import java.util.*;
 
 class Solution {
     static int N;
-    static Map<String, List<Integer>> cache = new HashMap<>();
-    static Map<Integer, Map<Integer, List<Integer>>> dp = new HashMap<>();
-
+    static boolean[] visited;
+    static Map<String, List<Integer>> map;
+    static double maxWinRate;
+    static int[] answer;
+    
     public int[] solution(int[][] dice) {
         N = dice.length;
-        int[] answer = new int[N / 2];
-        boolean[] used = new boolean[N + 1];
-        int[] A = new int[N / 2];
-        int[] B = new int[N / 2];
-        double[] maxRate = {0.0};
-
-        combiAndProcess(1, 0, dice, A, B, used, maxRate, answer);
+        map = new HashMap<>();
+        visited = new boolean[N];
+        answer = new int[N/2];
+        
+        selectDice(0, 0, dice);
+        
         return answer;
     }
-
-    public void combiAndProcess(int start, int depth, int[][] dice, int[] A, int[] B, boolean[] used, double[] maxRate, int[] answer) {
-        if (depth == N / 2) {
-            int bIndex = 0;
-            for (int i = 1; i <= N; i++) {
-                if (!used[i]) {
+    public void selectDice(int start, int depth, int[][] dice) {
+        if(depth == N/2) {
+            int[] A = new int[N/2];
+            int[] B = new int[N/2];
+            int aIndex = 0, bIndex = 0;
+            for(int i=0; i<N; i++) {
+                if(visited[i]) {
+                    A[aIndex++] = i;
+                }
+                else {
                     B[bIndex++] = i;
                 }
             }
-            List<Integer> aSums = calculateAllSum(A, dice);
-            List<Integer> bSums = calculateAllSum(B, dice);
-            double winRate = calculateWinRate(aSums, bSums);
-            if (winRate > maxRate[0]) {
-                maxRate[0] = winRate;
-                System.arraycopy(A, 0, answer, 0, A.length);
+            List<Integer> aSums = gameStart(A, dice);
+            List<Integer> bSums = gameStart(B, dice);
+            double winRate = calcWinRate(aSums, bSums);
+            if(maxWinRate < winRate) {
+                maxWinRate = winRate;
+                answer = Arrays.stream(A).map(i -> i+1).toArray();
             }
             return;
         }
-
-        for (int i = start; i <= N; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                A[depth] = i;
-                combiAndProcess(i + 1, depth + 1, dice, A, B, used, maxRate, answer);
-                used[i] = false;
-            }
+        for(int i=start; i<N; i++) {
+            visited[i] = true;
+            selectDice(i + 1, depth + 1, dice);
+            visited[i] = false;
         }
     }
-
-    public List<Integer> calculateAllSum(int[] arr, int[][] dice) {
+    public List<Integer> gameStart(int[] arr, int[][] dice) {
         String key = Arrays.toString(arr);
-        if (cache.containsKey(key)) {
-            return cache.get(key);
+        if(map.containsKey(key)) {
+            return map.get(key);
         }
         List<Integer> sums = new ArrayList<>();
-        calculateSums(0, 0, dice, arr, sums);
-        Collections.sort(sums);
-        cache.put(key, sums);
+        calculateSum(0, 0, arr, dice, sums);
+        Collections.sort(sums); //합 정렬
+        map.put(key, sums);
         return sums;
     }
-
-    public double calculateWinRate(List<Integer> aSums, List<Integer> bSums) {
-        int totalCases = aSums.size() * bSums.size();
-        int winCases = 0;
-
-        int aIndex = 0;
-        for (int bSum : bSums) {
-            while (aIndex < aSums.size() && aSums.get(aIndex) <= bSum) {
-                aIndex++;
-            }
-            winCases += aSums.size() - aIndex;
-        }
-        return (double) winCases / totalCases;
-    }
-
-    public void calculateSums(int depth, int sum, int[][] dice, int[] diceArr, List<Integer> sums) {
-        if (depth == diceArr.length) {
-            sums.add(sum);
+    public void calculateSum(int depth, int sum, int[] diceArr, int[][] dice, List<Integer> sumList) {
+        if(depth == diceArr.length) {
+            sumList.add(sum);
             return;
         }
-        for (int face : dice[diceArr[depth] - 1]) {
-            calculateSums(depth + 1, sum + face, dice, diceArr, sums);
+        for(int num : dice[diceArr[depth]]) {
+            calculateSum(depth + 1, sum + num, diceArr, dice, sumList);
         }
+    }
+    public double calcWinRate(List<Integer> aList, List<Integer> bList) {
+        int totalCase = aList.size() * bList.size();
+        int winCase = 0;
+        int aIndex = 0;
+        
+        for(int b : bList) {
+            while(aIndex < aList.size() && aList.get(aIndex) <= b) {
+                aIndex++;
+            }
+            winCase += aList.size() - aIndex;
+        }
+        return (double) winCase / totalCase;
     }
 }
