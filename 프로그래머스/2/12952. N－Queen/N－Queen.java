59d70967a2961129ceca795761335dd902433fc0
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-11-28 10:06:45 +0900

    [level 2] Title: N-Queen, Time: 153.79 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,30 +1,27 @@
 class Solution {
+    static int[] chess;
     static int answer;
-
     public int solution(int n) {
-        int[] board = new int[n]; // 각 행의 퀸이 위치한 열 정보를 저장
-        placeQueen(board, 0, n);
+        chess = new int[n];
+        solve(0, n);
         return answer;
     }
-
-    private void placeQueen(int[] board, int row, int n) {
-        if (row == n) {
-            answer++; // 모든 퀸을 배치한 경우
+    public void solve(int row, int n) {
+        if(row == n) {
+            answer++;
             return;
         }
-
-        for (int col = 0; col < n; col++) {
-            board[row] = col; // 현재 행(row)의 퀸을 col에 배치
-            if (isSafe(board, row)) {
-                placeQueen(board, row + 1, n); // 다음 행으로 진행
+        
+        for(int i=0; i<n; i++) {
+            chess[row] = i;
+            if(isSafe(row)) {
+                solve(row + 1, n);
             }
         }
     }
-
-    private boolean isSafe(int[] board, int row) {
-        for (int i = 0; i < row; i++) {
-            // 같은 열에 퀸이 있는 경우 또는 대각선에 퀸이 있는 경우
-            if (board[i] == board[row] || Math.abs(board[row] - board[i]) == Math.abs(row - i)) {
+    public boolean isSafe(int row) {
+        for(int i=0; i<row; i++) {
+            if(chess[i] == chess[row] || Math.abs(row - i) == Math.abs(chess[row] - chess[i])) {
                 return false;
             }
         }
