182050588c490b07286aa2c616b5f061208d0868
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-12-01 12:04:43 +0900

    [level 2] Title: 배달, Time: 3.89 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,60 +1,56 @@
 import java.util.*;
 
 class Solution {
-    static List<Node>[] list;
-    
     public int solution(int N, int[][] road, int K) {
-        int answer = 1;
-        list = new List[N+1];
-        for(int i=0;i<=N;i++) {
+        int answer = 0;
+        List<Node>[] list = new List[N + 1];
+        for(int i=0; i<=N; i++) {
             list[i] = new ArrayList<>();
         }
-        for(int[] r : road) {
-            int u = r[0], e = r[1],  v = r[2];
-            list[u].add(new Node(e, v));
-            list[e].add(new Node(u, v));
+        for(int [] r : road) {
+            int a = r[0];
+            int b = r[1];
+            int t = r[2];
+            list[a].add(new Node(b, t));
+            list[b].add(new Node(a, t));
         }
         int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(1, 0));
+        dist[1] = 0;
         
         while(!pq.isEmpty()) {
             Node now = pq.poll();
-            
-            if(dist[now.vertex] < now.cost) {
+            if(dist[now.node] < now.time) {
                 continue;
             }
             
-            for(Node next : list[now.vertex]) {
-                int nextDist = dist[now.vertex] + next.cost;
-                
-                if(nextDist <= K) { //배달 가능
-                    if(dist[next.vertex] > nextDist) {
-                        dist[next.vertex] = nextDist;
-                        pq.offer(new Node(next.vertex, nextDist));
-                    }
+            for(Node next : list[now.node]) {
+                if(dist[next.node] > dist[now.node] + next.time) {
+                    dist[next.node] = dist[now.node] + next.time;
+                    pq.offer(new Node(next.node, dist[next.node]));
                 }
             }
         }
-        for(int i=2; i<=N; i++) {
-            if(dist[i] != Integer.MAX_VALUE) {
+        for(int i=1; i<=N; i++) {
+            if(dist[i] <= K) {
                 answer++;
             }
         }
-        System.out.print(Arrays.toString(dist));
         return answer;
     }
-}
-class Node implements Comparable<Node> {
-    int vertex, cost;
-    public Node(int vertex, int cost) {
-        this.vertex = vertex;
-        this.cost = cost;
+    static class Node implements Comparable<Node> {
+        int node, time;
+        public Node(int node, int time) {
+            this.node=node;
+            this.time=time;
+        }
+        @Override
+        public int compareTo(Node n) {
+            return this.time - n.time;
+        }
     }
-    @Override
-    public int compareTo(Node n) {
-        return this.cost - n.cost;
-    }
+    
+    
 }
