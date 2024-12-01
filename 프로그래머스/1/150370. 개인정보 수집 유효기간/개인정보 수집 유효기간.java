91b38f48d2084ed4f368239a89d187ea189ce10e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-12-01 14:25:30 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.06 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         Map<String, Integer> termsMap = new HashMap<>();
@@ -8,44 +7,43 @@ class Solution {
         int nYear = Integer.parseInt(now[0]);
         int nMon = Integer.parseInt(now[1]);
         int nDay = Integer.parseInt(now[2]);
-
-        for (String t : terms) {
+        
+        for(String t : terms) {
             String[] arr = t.split(" ");
             termsMap.put(arr[0], Integer.parseInt(arr[1]));
         }
         int index = 0;
-        for (String p : privacies) {
+        for(String p : privacies) {
             String[] arr = p.split(" ");
             String[] date = arr[0].split("\\.");
-            if (!termsMap.containsKey(arr[1])) {
-                throw new IllegalArgumentException("유효하지 않은 약관 종류: " + arr[1]);
-            }
-            int expiration = termsMap.get(arr[1]); // 유효기간
+            int expiration = termsMap.get(arr[1]);  //유효기간
             int year = Integer.parseInt(date[0]);
             int month = Integer.parseInt(date[1]);
             int day = Integer.parseInt(date[2]);
-
             int endMonth = month + expiration;
             year += (endMonth - 1) / 12;
             endMonth = (endMonth - 1) % 12 + 1;
-
-            if (day == 1) { // 1일이라면 이전 달로 넘어감
-                endMonth -= 1;
-                if (endMonth == 0) {
+            
+            if(day == 1) {  //1일이라면 달 -1, 일 28
+                endMonth = endMonth - 1;
+                if(endMonth == 0) {
                     endMonth = 12;
                     year -= 1;
                 }
                 day = 28;
-            } else {
-                day -= 1;
             }
-
+            else {
+                day = day - 1;
+            }
+            // System.out.println(year+" "+endMonth+" "+day);
             index++;
-            if (nYear > year || 
-                (nYear == year && nMon > endMonth) || 
-                (nYear == year && nMon == endMonth && nDay > day)) {
+            if(nYear < year) {
+                continue;
+            }
+            if(nYear > year || (nYear == year && nMon > endMonth) || (nYear == year && nMon == endMonth && nDay > day)) {
                 answer.add(index);
             }
+            
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
