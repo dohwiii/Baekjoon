e94f132cb1f72ce1dbe67a52dda7d4e3d9bd3cab
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-12-04 10:19:54 +0900

    [Gold III] Title: 불!, Time: 460 ms, Memory: 45920 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,114 +1,109 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int R, C;
-    static char[][] map;
-    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static Queue<Pos> fQueue = new ArrayDeque<>();
-    static Queue<Pos> jQueue = new ArrayDeque<>();
+    static char[][] map;
+    static Queue<Pos> jihoon = new ArrayDeque<>();
+    static Queue<Pos> fire = new ArrayDeque<>();
+    static boolean[][] visitedJihoon;
+    static boolean[][] visitedFire;
+    static boolean isPossible;
     static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        R = Integer.parseInt(st.nextToken());   //행
-        C = Integer.parseInt(st.nextToken());   //열
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         map = new char[R][C];
-        visited = new boolean[R][C];
+        visitedJihoon = new boolean[R][C];
+        visitedFire = new boolean[R][C];
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
                 map[i][j] = str.charAt(j);
-                if (map[i][j] == 'J') { //시작위치
-                    jQueue.offer(new Pos(i, j));
-                    visited[i][j] = true;
-                }
-                else if (map[i][j] == 'F') {    //불 위치
-                    fQueue.offer(new Pos(i, j));
+                if (map[i][j] == 'J') { //지훈
+                    jihoon.offer(new Pos(i, j, 1));
+                } else if (map[i][j] == 'F') {    //불
+                    fire.offer(new Pos(i, j));
                 }
             }
         }
-        fire();
-        if (answer == 0) {
-            System.out.println("IMPOSSIBLE ");
-        }
-        else {
+        solve();
+        if (isPossible) {
             System.out.println(answer);
         }
+        else {
+            System.out.println("IMPOSSIBLE");
+        }
+
+        br.close();
 
     }
 
-    public static void fire() {
+    private static void solve() {
+        while (!jihoon.isEmpty() || !fire.isEmpty()) {
+            int jSize = jihoon.size();
+            int fSize = fire.size();
 
-        while (jQueue.size() > 0 || fQueue.size() > 0) {
-            int jSize = jQueue.size();
-            int fSize = fQueue.size();
-
-            //불
+            //불 확산
             while (fSize-- > 0) {
-                Pos now = fQueue.poll();
-
+                Pos now = fire.poll();
                 for (int i = 0; i < 4; i++) {
                     int nx = now.x + dx[i];
                     int ny = now.y + dy[i];
 
-                    if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
-                        if (map[nx][ny] == '.') {
-                            map[nx][ny] = 'F';  //확산
-                            fQueue.offer(new Pos(nx, ny));
-                        }
+                    if (nx < 0 || nx >= R || ny < 0 || ny >= C || map[nx][ny] == '#' || visitedFire[nx][ny]) {
+                        continue;
                     }
+                    visitedFire[nx][ny] = true;
+                    fire.offer(new Pos(nx, ny));
                 }
             }
-
-            //지훈
+            //지훈 이동
             while (jSize-- > 0) {
-                Pos now = jQueue.poll();
-                //가장자리
+                Pos now = jihoon.poll();
                 if (now.x == 0 || now.x == R - 1 || now.y == 0 || now.y == C - 1) {
-                    answer = now.cnt + 1;
+                    isPossible = true;
+                    answer = now.cnt;
                     return;
                 }
-
                 for (int i = 0; i < 4; i++) {
                     int nx = now.x + dx[i];
                     int ny = now.y + dy[i];
 
-                    if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
-                        if (!visited[nx][ny] && map[nx][ny] == '.') {
-                            jQueue.offer(new Pos(nx, ny, now.cnt + 1));
-                            visited[nx][ny] = true;
-                        }
+                    //벽이거나 불 만나면 가면 안됨
+                    if (nx < 0 || nx >= R || ny < 0 || ny >= C || map[nx][ny] != '.' || visitedJihoon[nx][ny] || visitedFire[nx][ny]) {
+                        continue;
                     }
+                    visitedJihoon[nx][ny] = true;
+                    jihoon.offer(new Pos(nx, ny, now.cnt + 1));
                 }
             }
-
-
 
         }
     }
 
-}
+    static class Pos {
+        int x, y, cnt;
 
-class Pos {
-    int x, y, cnt;
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-    public Pos(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Pos(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
+        public Pos(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
     }
 }
