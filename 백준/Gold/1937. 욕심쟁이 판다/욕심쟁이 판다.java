35c5008ddaf91eaf36917b24ae2e0a4918b6c1f6
dohwiii <102406112+dohwiii@users.noreply.github.com>
2024-12-27 18:07:48 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 376 ms, Memory: 40816 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -5,19 +5,16 @@ import java.util.*;
 public class Main {
     static int N;
     static int[][] map;
-    static int[][] dp;
-    static boolean[][] visited;
+    static int[][] bambooMap;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        dp = new int[N][N];
-        visited = new boolean[N][N];
+        bambooMap = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,27 +22,22 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                dp[i][j] = -1;
-            }
-        }
         int max = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                max = Math.max(max, dfs(i, j));
+                max = Math.max(max, eatBamboo(i, j));
             }
         }
+
         System.out.println(max);
 
     }
 
-    public static int dfs(int x, int y) {
-        if (dp[x][y] != -1) {
-            return dp[x][y];
+    public static int eatBamboo(int x, int y) {
+        if (bambooMap[x][y] != 0) { //이미 방문한 곳이라면 기존 값 반환
+            return bambooMap[x][y];
         }
-
-        dp[x][y] = 1;
+        bambooMap[x][y] = 1;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
@@ -54,10 +46,12 @@ public class Main {
             if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] <= map[x][y]) {
                 continue;
             }
-            dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            bambooMap[x][y] = Math.max(bambooMap[x][y], eatBamboo(nx, ny) + 1);
+
         }
 
-        return dp[x][y];
+
+        return bambooMap[x][y];
     }
 
 }
