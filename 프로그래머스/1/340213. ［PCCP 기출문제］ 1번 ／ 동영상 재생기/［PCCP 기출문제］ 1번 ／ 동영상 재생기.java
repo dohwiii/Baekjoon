b39ac5ec65a9daa9cc8e20affbcef99e386d6478
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-02 11:54:56 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.07 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -2,39 +2,44 @@ import java.util.*;
 
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String answer = "";
-        int hour = Integer.parseInt(pos.substring(0, 2));
-        int min = Integer.parseInt(pos.substring(3, 5));
-        int nowPos = hour*60 + min;
-        
-        int osHour = Integer.parseInt(op_start.substring(0, 2));
-        int osMin = Integer.parseInt(op_start.substring(3, 5));
-        int oeHour = Integer.parseInt(op_end.substring(0, 2));
-        int oeMin = Integer.parseInt(op_end.substring(3, 5));
-        int oe = oeHour*60 + oeMin;
-        int os = osHour*60 + osMin;
-        int total = Integer.parseInt(video_len.substring(0, 2)) * 60 + Integer.parseInt(video_len.substring(3, 5));
-        
-        for(int i=0; i<commands.length; i++) {
-            String command = commands[i];   //명령어
-            
-            if(nowPos >= os && nowPos <= oe) {
-                nowPos = oe;
+        // 초 단위로 변환
+        int videoLength = toSeconds(video_len);
+        int currentPos = toSeconds(pos);
+        int opStart = toSeconds(op_start);
+        int opEnd = toSeconds(op_end);
+
+        // 명령어 처리
+        for (String command : commands) {
+            // 오프닝 구간 건너뛰기
+            if (currentPos >= opStart && currentPos <= opEnd) {
+                currentPos = opEnd;
             }
-            
-            if(command.equals("next")) {
-                nowPos = Math.min(total, nowPos + 10);
-            }
-            else {  //prev
-                nowPos = Math.max(0, nowPos-10);
-            }
+
+            // switch 표현식 사용
+            currentPos = switch (command) {
+                case "next" -> Math.min(videoLength, currentPos + 10);
+                case "prev" -> Math.max(0, currentPos - 10);
+                default -> currentPos; // 다른 명령은 무시
+            };
         }
-        if(nowPos >= os && nowPos <= oe) {
-            nowPos = oe;
+
+        // 최종적으로 오프닝 구간 확인
+        if (currentPos >= opStart && currentPos <= opEnd) {
+            currentPos = opEnd;
         }
-        int ahour = nowPos/60;
-        int amin = nowPos%60;
-        answer = String.format("%02d:%02d", ahour, amin);
-        return answer;
+
+        return toTimeString(currentPos);
+    }
+
+    // 시간 문자열을 초 단위로 변환하는 메서드
+    private static int toSeconds(String time) {
+        return Integer.parseInt(time.substring(0, 2)) * 60 + Integer.parseInt(time.substring(3, 5));
+    }
+
+    // 초를 시간 문자열로 변환하는 메서드
+    private static String toTimeString(int seconds) {
+        int minutes = seconds / 60;
+        int sec = seconds % 60;
+        return String.format("%02d:%02d", minutes, sec);
     }
 }
