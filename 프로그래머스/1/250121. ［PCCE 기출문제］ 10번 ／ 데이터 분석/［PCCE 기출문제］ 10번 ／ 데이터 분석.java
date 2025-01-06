9c017fbad3c5b7db50bc09c488e5a8b2e617140f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-06 16:24:03 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 8.21 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -2,31 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        List<int[]> list = new ArrayList<>();
-        Map<String, Integer> map = Map.of("code", 0, "date", 1, "maximum", 2, "remain", 3);
 
-        int standardIndex = map.get(ext);
+        String[] arr = {"code","date","maximum","remain"};
+        List<String> columnList = Arrays.asList(arr);
+        int extIdx = columnList.indexOf(ext);
+        int sortIdx = columnList.indexOf(sort_by);
+        int[][] answer = Arrays.stream(data).filter(o1 -> o1[extIdx] < val_ext)
+            .sorted((o1 ,o2) -> o1[sortIdx]-o2[sortIdx]).toArray(int[][]::new);
 
-        // 데이터 필터링
-        for (int[] d : data) {
-            if (ext.equals("date")) {
-                // 날짜 필터링: 숫자로 비교
-                if (d[1] < val_ext) {
-                    list.add(d);
-                }
-            } else {
-                // 다른 필드 필터링
-                if (d[standardIndex] < val_ext) {
-                    list.add(d);
-                }
-            }
-        }
-
-        // 정렬
-        int sortIndex = map.get(sort_by);
-        list.sort(Comparator.comparingInt(a -> a[sortIndex]));
-
-        // 결과를 배열로 변환
-        return list.toArray(new int[0][]);
+        return answer;
     }
 }
