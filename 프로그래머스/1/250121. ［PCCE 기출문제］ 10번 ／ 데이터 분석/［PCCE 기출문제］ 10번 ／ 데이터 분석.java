89cc5a5abcfccc3f0adda53046d310c54721307e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-06 16:22:48 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 11.37 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -2,54 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer = {};
         List<int[]> list = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-        map.put("code", 0);
-        map.put("date", 1);
-        map.put("maximum", 2);
-        map.put("remain", 3);
-        
-        if(ext.equals("date")) {
-            String vext = String.valueOf(val_ext);
-            int year = Integer.parseInt(vext.substring(0, 4));
-            int month = Integer.parseInt(vext.substring(4, 6));
-            int day = Integer.parseInt(vext.substring(6, 8));
-            
-            for(int[] d : data) {
-                String date = String.valueOf(d[1]);
-                int dyear = Integer.parseInt(date.substring(0, 4));
-                int dmonth = Integer.parseInt(date.substring(4, 6));
-                int dday = Integer.parseInt(date.substring(6, 8));
-                
-                if(dyear < year) {
+        Map<String, Integer> map = Map.of("code", 0, "date", 1, "maximum", 2, "remain", 3);
+
+        int standardIndex = map.get(ext);
+
+        // 데이터 필터링
+        for (int[] d : data) {
+            if (ext.equals("date")) {
+                // 날짜 필터링: 숫자로 비교
+                if (d[1] < val_ext) {
                     list.add(d);
                 }
-                else if((dyear == year && month > dmonth) || (dyear == year && month == dmonth && day > dday)) {
-                    list.add(d);
-                }
-            }
-            
-        }
-        else {
-            int standard = map.get(ext);
-            
-            for(int[] d : data) {
-                if(d[standard] < val_ext) {
+            } else {
+                // 다른 필드 필터링
+                if (d[standardIndex] < val_ext) {
                     list.add(d);
                 }
             }
         }
+
+        // 정렬
         int sortIndex = map.get(sort_by);
-        
-        Collections.sort(list, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[sortIndex] - b[sortIndex];
-            }
-        });
-        answer = list.toArray(new int[list.size()][]);
-        
-        return answer;
+        list.sort(Comparator.comparingInt(a -> a[sortIndex]));
+
+        // 결과를 배열로 변환
+        return list.toArray(new int[0][]);
     }
 }
