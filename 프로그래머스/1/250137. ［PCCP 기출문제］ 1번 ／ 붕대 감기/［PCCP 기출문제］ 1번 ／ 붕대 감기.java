6c8a3c54cad36d71bb082a2a4a991a5ff47c3f08
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-06 15:16:23 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.12 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -2,38 +2,63 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = 0;
-        int strength = health;
-        int straight = 0;
-        int second = 1;
-        int lastTime = attacks[attacks.length - 1][0];
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int[] attack : attacks) {
-            map.put(attack[0], attack[1]);  //공격 시간, 피해량
-        }
-        
-        while(second <= lastTime && strength > 0) {
-            if(map.containsKey(second)) {   //지금 공격시간이라면
-                strength -= map.get(second);  //공격
-                straight = 0;
-            }
-            else {  //공격 X
-                straight++; //연속
-                strength = Math.min(health, strength + bandage[1]);
-                
-                if(straight == bandage[0]) {    //추가회복
-                    strength = Math.min(health, strength + bandage[2]);
-                    straight = 0;
+        // 변수 초기화
+        int currentHealth = health;
+        int consecutiveSuccess = 0;
+        int attackIndex = 0;
+        int time = 1;
+
+        // 공격 시간 처리
+        while (currentHealth > 0 && attackIndex < attacks.length) {
+            int nextAttackTime = attacks[attackIndex][0];
+
+            // 공격 시간 전까지 가능한 최대 연속 회복 시간 계산
+            int recoveryTime = nextAttackTime - time;
+
+            if (recoveryTime > 0) {
+                // 가능한 회복 횟수 계산
+                int fullCycles = Math.min(recoveryTime / bandage[0], (health - currentHealth) / (bandage[0] * bandage[1] + bandage[2]));
+
+                // 체력을 회복할 수 있는 경우
+                if (fullCycles > 0) {
+                    currentHealth = Math.min(health, currentHealth + fullCycles * (bandage[0] * bandage[1] + bandage[2]));
+                    consecutiveSuccess = 0;
+                    time += fullCycles * bandage[0];
+                }
+
+                // 남은 시간 동안의 회복 계산
+                while (time < nextAttackTime && currentHealth < health) {
+                    currentHealth = Math.min(health, currentHealth + bandage[1]);
+
+                    if (++consecutiveSuccess == bandage[0]) {
+                        currentHealth = Math.min(health, currentHealth + bandage[2]);
+                        consecutiveSuccess = 0;
+                    }
+
+                    time++;
                 }
             }
-            System.out.println(second+" "+ straight+" "+strength);
-            second++;
+
+            // 공격 처리
+            currentHealth -= attacks[attackIndex][1];
+            consecutiveSuccess = 0;
+            attackIndex++;
+            time = nextAttackTime + 1;
         }
-        if(strength <= 0) {
-            strength = -1;
+
+        // 마지막 공격 이후 남은 시간 동안 회복
+        while (currentHealth > 0 && time <= attacks[attacks.length - 1][0]) {
+            currentHealth = Math.min(health, currentHealth + bandage[1]);
+
+            if (++consecutiveSuccess == bandage[0]) {
+                currentHealth = Math.min(health, currentHealth + bandage[2]);
+                consecutiveSuccess = 0;
+            }
+
+            time++;
         }
-        
-        return strength;
+
+        // 체력이 0 이하라면 -1 반환
+        return currentHealth > 0 ? currentHealth : -1;
     }
 }
