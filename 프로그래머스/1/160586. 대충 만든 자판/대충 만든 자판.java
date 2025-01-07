9117668a6eaaae05b9bcc502ae3f9501258a8cc6
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-07 19:58:52 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.80 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -4,34 +4,29 @@ class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         Map<Character, Integer> alpMap = new HashMap<>();
-        
-        for(int i=0; i<keymap.length; i++) {
-            String key = keymap[i];
-            for(int j=0; j<key.length(); j++) {
+
+        // 최소 키 입력 횟수 저장
+        for (String key : keymap) {
+            for (int j = 0; j < key.length(); j++) {
                 char c = key.charAt(j);
-                if(alpMap.get(c) != null) {
-                    int value = alpMap.get(c);
-                    alpMap.put(c, Math.min(value, j+1));
-                }
-                else {
-                    alpMap.put(c, j+1);
-                }
+                alpMap.put(c, Math.min(alpMap.getOrDefault(c, j + 1), j + 1));
             }
         }
 
-        for(int i=0; i<targets.length; i++) {
+        // 각 target에 대한 최소 입력 횟수 계산
+        for (int i = 0; i < targets.length; i++) {
             String target = targets[i];
-            for(int j=0; j<target.length(); j++) {
-                if(alpMap.get(target.charAt(j)) == null) {
-                    answer[i] = -1;
+            int total = 0;
+            for (char c : target.toCharArray()) {
+                if (!alpMap.containsKey(c)) {
+                    total = -1;
                     break;
                 }
-                int index = alpMap.get(target.charAt(j));
-                answer[i] += index;
+                total += alpMap.get(c);
             }
+            answer[i] = total;
         }
-        
-        
+
         return answer;
     }
 }
