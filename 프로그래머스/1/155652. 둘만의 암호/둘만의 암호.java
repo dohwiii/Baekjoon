4d3a3db67ba4adc04916684ba7d348619470eea5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-08 10:00:49 +0900

    [level 1] Title: 둘만의 암호, Time: 1.48 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,40 +1,20 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public String solution(String s, String skip, int index) {
-        // Skip 문자열을 Set으로 변환하여 검색 속도를 최적화
-        Set<Character> skipSet = new HashSet<>();
-        for (char c : skip.toCharArray()) {
-            skipSet.add(c);
-        }
-
-        // 결과 문자열을 저장할 StringBuilder
-        StringBuilder result = new StringBuilder();
-
-        // 문자열 변환
-        for (char c : s.toCharArray()) {
-            result.append(shiftChar(c, index, skipSet));
-        }
-
-        return result.toString();
-    }
-
-    // 특정 문자를 index만큼 이동, skipSet에 포함된 문자 건너뛰기
-    private char shiftChar(char start, int index, Set<Character> skipSet) {
-        int count = 0; // 유효한 이동 횟수
-        char current = start;
-
-        while (count < index) {
-            current++; // 다음 문자로 이동
-            if (current > 'z') {
-                current = 'a'; // 'z'를 넘어가면 'a'로 순환
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()) {
+            int idx = 0;
+            char temp = c;
+            while(idx < index) {
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if(!skip.contains(String.valueOf(temp))) {
+                    idx++;
+                }
             }
-            if (!skipSet.contains(current)) {
-                count++; // 유효한 이동
-            }
+            sb.append(temp);
         }
-
-        return current;
+        
+        return sb.toString();
     }
 }
