c1f89ae4aec4badd53a26148ca878ea84cccced2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-09 18:22:01 +0900

    [level 1] Title: 문자열 나누기, Time: 0.89 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,42 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        List<Character> list = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
+        char first = '1';
+        int same = 0, diff = 0;
         
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
             
-            if(list.isEmpty()) {    //맨 첫번째 (x)
-                list.add(c);
-                map.put(c, 1);
+            if(first == '1') {  //맨 처음
+                first = c;
+                same++;
+                answer++;
             }
             else {
-                char first = list.get(0);
                 if(first == c) {
-                    map.put(c, map.getOrDefault(c, 0) + 1);
+                    same++;
                 }
-                else {  //다른 문자
-                    map.put(c, map.getOrDefault(c, 0) + 1);
-                }
-                
-                int firstCnt = map.get(first);
-                int otherCnt = 0;
-                for(char key : map.keySet()) {
-                    if(first != key) {
-                        otherCnt += map.get(key);
-                    }
-                }
-                if(firstCnt == otherCnt) {
-                    answer++;
-                    list.clear();   //list 초기화
-                    map.clear();    //map 초기화
+                else {
+                    diff++;
                 }
             }
-        
-        }
-        if(list.size() != 0) {
-            answer++;
+            if(same == diff) {
+                first = '1';
+                same = 0;
+                diff = 0;
+            }
         }
                 
         return answer;
