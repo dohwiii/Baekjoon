c83fb626e189ac03b437eb7711b4000d6193a8d9
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-10 10:44:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 8.40 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,30 +1,24 @@
 class Solution {
     public int solution(int number, int limit, int power) {
+        int[] divisorCounts = new int[number + 1];
         int answer = 0;
-        
-        for(int i=1; i<=number; i++) {
-            int divisor = getDivisor(i);
-            if(divisor > limit) {
+
+        // 1. 모든 숫자의 약수 개수를 미리 계산
+        for (int i = 1; i <= number; i++) {
+            for (int j = i; j <= number; j += i) {
+                divisorCounts[j]++;
+            }
+        }
+
+        // 2. 약수 개수에 따라 점수 계산
+        for (int i = 1; i <= number; i++) {
+            if (divisorCounts[i] > limit) {
                 answer += power;
-            }
-            else {
-                answer += divisor;
+            } else {
+                answer += divisorCounts[i];
             }
         }
-        
+
         return answer;
-    }
-    public int getDivisor(int num) {
-        int cnt = 0;
-        
-        for(int i=1; i*i <= num; i++) {
-            if(i * i == num) {
-                cnt++;
-            }
-            else if(num % i == 0) {
-                cnt+=2;
-            }
-        }
-        return cnt;
     }
 }
