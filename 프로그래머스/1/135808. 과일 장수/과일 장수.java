4a18dc99f505c3e9ddfc9121ca256814c6b99605
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-11 13:12:04 +0900

    [level 1] Title: 과일 장수, Time: 68.51 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,27 +4,14 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         int appleAmount = score.length; //사과 개수
-        int boxAmount = appleAmount / m;    //포장 상자 개수
 
         Arrays.sort(score);
 
-        if(appleAmount % m == 0) {  //모든 사과를 포장할 수 있다면
-            int index = 0;
+        int remain = appleAmount % m;   //남는 사과(버림)
             
-            while(index < score.length) {
-                int min = score[index]; //제일 최소 점수
-                int s = min * m;
-                answer += s;
-                index += m;
-            }
-        }
-        else {
-            int remain = appleAmount % m;
-            
-            for(int i=score.length - 1; i>remain; i-=m) {
-                int start = score[i - m + 1];
-                answer += (start * m);
-            }
+        for(int i=score.length - 1; i>remain; i-=m) {
+            int start = score[i - m + 1];
+            answer += (start * m);
         }
         
         return answer;
