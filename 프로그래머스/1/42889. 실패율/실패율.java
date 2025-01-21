45364ce8998eb0d1d743adc3e92af217e9fb3f7a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-21 11:31:36 +0900

    [level 1] Title: 실패율, Time: 4.44 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,39 +1,42 @@
 import java.util.*;
+
 class Solution {
-    static class Score {
-        double percent;
-        int stage;
-        public Score(double percent, int stage) {
-            this.percent = percent;
-            this.stage=stage;
-        }
-    }
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        int people = stages.length;
-        int[] progress = new int[N + 2];
-        Score[] ratio = new Score[N];
-        
-        for(int i=0; i<stages.length; i++) {
-            progress[stages[i]]++;
+        int[] progress = new int[N + 2]; // 각 스테이지에 머물러 있는 플레이어 수 저장
+        for (int stage : stages) {
+            progress[stage]++;
         }
-        for(int i=1; i<=N; i++) {
-            double s = progress[i] / (double) people;
-            if(people == 0) {
-                s = 0.0;
+
+        int players = stages.length; // 전체 플레이어 수
+        double[] failureRates = new double[N]; // 각 스테이지의 실패율 저장
+        for (int i = 1; i <= N; i++) {
+            if (players > 0) {
+                failureRates[i - 1] = (double) progress[i] / players;
+                players -= progress[i];
+            } else {
+                failureRates[i - 1] = 0.0; // 플레이어가 없는 경우 실패율 0
             }
-            ratio[i - 1] = new Score(s, i);
-            people -= progress[i];
         }
-        Arrays.sort(ratio, new Comparator<Score>() {
-            @Override
-            public int compare(Score s1, Score s2) {
-                return Double.compare(s2.percent, s1.percent);
+
+        // 스테이지 번호와 실패율을 쌍으로 묶어 정렬
+        Integer[] stageOrder = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            stageOrder[i] = i + 1; // 스테이지 번호 저장
+        }
+
+        Arrays.sort(stageOrder, (a, b) -> {
+            if (failureRates[b - 1] != failureRates[a - 1]) {
+                return Double.compare(failureRates[b - 1], failureRates[a - 1]);
             }
+            return a - b; // 실패율이 같다면 스테이지 번호가 작은 순서대로
         });
-        for(int i=0; i<N; i++) {
-            answer[i] = ratio[i].stage;
+
+        // 결과 배열 생성
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = stageOrder[i];
         }
+
         return answer;
     }
 }
