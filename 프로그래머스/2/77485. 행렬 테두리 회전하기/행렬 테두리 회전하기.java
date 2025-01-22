1d1935494bb2963bfb54cd7168b619fafe98262c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-22 17:37:07 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 16.08 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,59 +1,65 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
-        int[][] map = new int[rows][columns];
-        int index = 0;
-        
-        for(int i=1; i<=rows; i++) {
-            for(int j=1; j<=columns; j++) {
-                int value = (i - 1) * columns + j;
-                map[i-1][j-1] = value; 
+        int[] matrix = new int[rows * columns];
+
+        // 1. 1차원 배열로 초기화
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i * columns + j] = i * columns + j + 1;
             }
         }
-        
-        for(int[] query : queries) {
-            int x1 = query[0] - 1;
-            int y1 = query[1] - 1;
-            int x2 = query[2] - 1;
-            int y2 = query[3] - 1;
-            
-            int prev = map[x1][y1];
+
+        // 2. 쿼리 처리
+        for (int q = 0; q < queries.length; q++) {
+            int x1 = queries[q][0] - 1;
+            int y1 = queries[q][1] - 1;
+            int x2 = queries[q][2] - 1;
+            int y2 = queries[q][3] - 1;
+
+            int prev = matrix[x1 * columns + y1];
             int minVal = prev;
 
-            //상단 행 이동
-            for(int y=y1 + 1; y<=y2; y++) {
-                int temp = map[x1][y];
-                map[x1][y] = prev;
+            // 상단 행 이동
+            for (int y = y1 + 1; y <= y2; y++) {
+                int idx = x1 * columns + y;
+                int temp = matrix[idx];
+                matrix[idx] = prev;
                 prev = temp;
                 minVal = Math.min(minVal, prev);
             }
-            //오른쪽 열 이동
-            for(int x=x1 + 1; x<=x2; x++) {
-                int temp = map[x][y2];
-                map[x][y2] = prev;
+
+            // 오른쪽 열 이동
+            for (int x = x1 + 1; x <= x2; x++) {
+                int idx = x * columns + y2;
+                int temp = matrix[idx];
+                matrix[idx] = prev;
                 prev = temp;
                 minVal = Math.min(minVal, prev);
             }
-            //하단 행 이동
-            for(int i=y2-1; i>=y1; i--) {
-                int temp = map[x2][i];
-                map[x2][i] = prev;
+
+            // 하단 행 이동
+            for (int y = y2 - 1; y >= y1; y--) {
+                int idx = x2 * columns + y;
+                int temp = matrix[idx];
+                matrix[idx] = prev;
                 prev = temp;
                 minVal = Math.min(minVal, prev);
             }
-            //왼쪽 열 이동
-            for(int i=x2-1; i>=x1; i--) {
-                int temp = map[i][y1];
-                map[i][y1] = prev;
+
+            // 왼쪽 열 이동
+            for (int x = x2 - 1; x >= x1; x--) {
+                int idx = x * columns + y1;
+                int temp = matrix[idx];
+                matrix[idx] = prev;
                 prev = temp;
                 minVal = Math.min(minVal, prev);
             }
-            answer[index++] = minVal;
+
+            // 결과 저장
+            answer[q] = minVal;
         }
 
-        
         return answer;
     }
 }
