48f9b29617eef369ba3457160495af4112cce853
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-23 17:41:10 +0900

    [level 2] Title: [3차] 압축, Time: 30.28 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,36 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        HashMap<String, Integer> map = new HashMap<>();
-        char c = 'A';
-        for(int i=1; i<=26; i++) {
-            String s  = String.valueOf(c);
-            map.put(s, i);
-            c++;
-        }
+        int[] answer = {};
         List<Integer> list = new ArrayList<>();
+        Map<String, Integer> alp = new HashMap<>();
+        //기존 알파벳 사전 추가
+        for(int i=0; i<26; i++) {
+            char c = (char) (65 + i);
+            alp.put(String.valueOf(c), i + 1);
+        }
         StringBuilder sb = new StringBuilder();
-        int alpIndex = 27;
+        int order = 27;
         int index = 0;
         
         while(index < msg.length()) {
-            char now = msg.charAt(index);
-            sb.append(now);
-            // System.out.println(sb.toString());
-            if(!map.containsKey(sb.toString())) {   //사전에 일치하는 문자 없음
-                map.put(sb.toString(), alpIndex++); //새로운 문자열 사전에 입력
-                sb.deleteCharAt(sb.length() - 1);   //일치하지 않은 알파벳 삭제
-                int dicIndex = map.get(sb.toString());  //사전 인덱스
-                list.add(dicIndex); //사전 색인번호 출력
-                sb.setLength(0);
-                sb.append(now);
+            sb.setLength(0);
+            char c = msg.charAt(index++);
+            sb.append(c);
+            
+            while(index < msg.length()) {
+                sb.append(msg.charAt(index));
+                
+                if(!alp.containsKey(sb.toString())) {
+                    alp.put(sb.toString(), order++);
+                    sb.deleteCharAt(sb.length() - 1);   //마지막 문자 제거
+                    list.add(alp.get(sb.toString()));
+                    break;
+                }
+                else {
+                    index++;    //다음 문자
+                }
             }
-            index++;
+            
         }
-        if(sb.length() > 0) {
-            list.add(map.get(sb.toString()));
-        }
+        list.add(alp.get(sb.toString())); //마지막 문자 추가
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
