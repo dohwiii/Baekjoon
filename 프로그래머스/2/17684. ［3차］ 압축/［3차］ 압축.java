3cefc66de022995a045461ad8a02b3439acd96e3
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-23 17:44:49 +0900

    [level 2] Title: [3차] 압축, Time: 9.96 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,40 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        int[] answer = {};
         List<Integer> list = new ArrayList<>();
         Map<String, Integer> alp = new HashMap<>();
-        //기존 알파벳 사전 추가
-        for(int i=0; i<26; i++) {
-            char c = (char) (65 + i);
-            alp.put(String.valueOf(c), i + 1);
-        }
-        StringBuilder sb = new StringBuilder();
-        int order = 27;
-        int index = 0;
-        
-        while(index < msg.length()) {
-            sb.setLength(0);
-            char c = msg.charAt(index++);
-            sb.append(c);
-            
-            while(index < msg.length()) {
-                sb.append(msg.charAt(index));
-                
-                if(!alp.containsKey(sb.toString())) {
-                    alp.put(sb.toString(), order++);
-                    sb.deleteCharAt(sb.length() - 1);   //마지막 문자 제거
-                    list.add(alp.get(sb.toString()));
-                    break;
-                }
-                else {
-                    index++;    //다음 문자
-                }
-            }
-            
-        }
-        list.add(alp.get(sb.toString())); //마지막 문자 추가
 
+        // 기존 알파벳 사전 추가
+        for (int i = 0; i < 26; i++) {
+            alp.put(String.valueOf((char) ('A' + i)), i + 1);
+        }
+
+        int order = 27; // 새로운 단어의 번호
+        int start = 0;  // 현재 단어의 시작 인덱스
+
+        while (start < msg.length()) {
+            int end = start + 1; // 현재 단어의 끝 인덱스
+
+            // 사전에 존재하지 않는 단어를 찾을 때까지 확장
+            while (end <= msg.length() && alp.containsKey(msg.substring(start, end))) {
+                end++;
+            }
+
+            // 마지막으로 발견된 단어의 번호를 추가
+            list.add(alp.get(msg.substring(start, end - 1)));
+
+            // 새로운 단어를 사전에 추가
+            if (end <= msg.length()) {
+                alp.put(msg.substring(start, end), order++);
+            }
+
+            // 다음 단어로 이동
+            start = end - 1;
+        }
+
+        // 결과를 배열로 변환
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
