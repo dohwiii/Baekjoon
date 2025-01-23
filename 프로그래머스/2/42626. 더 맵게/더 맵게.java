4f357b51448942d4e60ada6b5a29a0128d11bac5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-23 11:35:16 +0900

    [level 2] Title: 더 맵게, Time: 1465.31 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,27 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        // 초기 조건으로 모든 음식의 스코빌 지수가 이미 K 이상인 경우 바로 0 반환
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int scov : scoville) {
-            pq.offer(scov);
-        }
-        if (pq.peek() >= K) return 0;
-
         int answer = 0;
-
-        // 조기 종료 조건을 루프 내에서 설정하여 계산
-        while (pq.size() > 1) {
-            int leastSpicy = pq.poll();
-            if (leastSpicy >= K) return answer; // 조기 종료: 모두 K 이상
-
-            int secondLeastSpicy = pq.poll();
-            int newScoville = leastSpicy + 2 * secondLeastSpicy;
-            pq.offer(newScoville);
-            answer++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int s : scoville) {
+            pq.offer(s);
         }
-
-        // 최종 체크: 모든 음식을 섞었을 때도 K 미달인 경우
-        return pq.peek() >= K ? answer : -1;
+        while(pq.size() >= 2) {
+            int first = pq.poll();
+            if(first >= K) {
+                break;
+            }
+            int second = pq.poll();
+            int score = first + second * 2;
+            pq.offer(score);
+            answer++;   //섞음
+            
+            
+        }
+        if(pq.poll() < K) {
+            return -1;
+        }
+        return answer;
     }
 }
