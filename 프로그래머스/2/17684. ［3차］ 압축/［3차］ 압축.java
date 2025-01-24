878c64278bb7c6af000307d427149a9de5267646
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-24 10:26:13 +0900

    [level 2] Title: [3차] 압축, Time: 3.88 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,38 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> list = new ArrayList<>();
-        Map<String, Integer> alp = new HashMap<>();
-
-        // 기존 알파벳 사전 추가
-        for (int i = 0; i < 26; i++) {
-            alp.put(String.valueOf((char) ('A' + i)), i + 1);
+        int[] answer = {};
+        Map<String, Integer> dictionary = new HashMap<>();  //사전
+        List<Integer> indexList = new ArrayList<>();    //결과 인덱스 담을 리스트
+        int start = 0;
+        int order = 27;
+        
+        for(int i=0; i<26; i++) {
+            char c = (char) ('A' + i);
+            dictionary.put(String.valueOf(c), i+1);
         }
 
-        int order = 27; // 새로운 단어의 번호
-        int start = 0;  // 현재 단어의 시작 인덱스
-
-        while (start < msg.length()) {
-            int end = start + 1; // 현재 단어의 끝 인덱스
-
-            // 사전에 존재하지 않는 단어를 찾을 때까지 확장
-            while (end <= msg.length() && alp.containsKey(msg.substring(start, end))) {
+        
+        while(start < msg.length()) {
+            int end = start + 1;
+            
+            while(end <= msg.length() && dictionary.containsKey(msg.substring(start, end))) {
                 end++;
             }
-
-            // 마지막으로 발견된 단어의 번호를 추가
-            list.add(alp.get(msg.substring(start, end - 1)));
-
-            // 새로운 단어를 사전에 추가
-            if (end <= msg.length()) {
-                alp.put(msg.substring(start, end), order++);
+            if(end <= msg.length()) {
+                indexList.add(dictionary.get(msg.substring(start, end - 1)));    //색인 번호 출력
+                dictionary.put(msg.substring(start, end), order++); //사전에 추가
             }
-
-            // 다음 단어로 이동
+            else {  //범위 초과
+                indexList.add(dictionary.get(msg.substring(start, end - 1)));
+            }
+            
             start = end - 1;
+            
         }
 
-        // 결과를 배열로 변환
-        return list.stream().mapToInt(i -> i).toArray();
+        return indexList.stream().mapToInt(i -> i).toArray();
     }
 }
