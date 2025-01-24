64e050a7af834cf283755205ef0a7dcaa7f10d31
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-24 14:56:34 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.34 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -7,15 +7,11 @@ class Solution
         int answer = 0;
 
         Arrays.sort(A);
-        Integer b[] = Arrays.stream(B).boxed().toArray(Integer[]::new);
-        Arrays.sort(b, new Comparator<Integer>(){
-            @Override
-            public int compare(Integer a, Integer b) {
-                return b - a;
-            }
-        });
-        for(int i=0; i<A.length; i++) {
-            answer += A[i] * b[i];
+        Arrays.sort(B);
+        int length = A.length;
+        
+        for(int i=0; i<length; i++) {
+            answer += A[i] * B[length-1-i];
         }
         return answer;
     }
