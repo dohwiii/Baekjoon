894d8a053a1d352426eb49f191a5e0a24a4dab02
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-25 15:36:57 +0900

    [level 2] Title: 숫자의 표현, Time: 0.93 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int s = 1, e = 2;
+        if(n == 1) {
+            return 1;
+        }
+        //누적합
+        int[] sum = new int[n + 1];
+        for(int i=1; i<=n; i++) {
+            sum[i] = i + sum[i-1];
+        }
+        
+        while(e <= n) {
+            int total = sum[e] - sum[s - 1];
+            
+            if(total <= n) {
+                if(total == n) {
+                    answer++;
+                }
+                e++;
+            }
+            else {
+                s++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
