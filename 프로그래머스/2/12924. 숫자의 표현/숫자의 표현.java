42a81c4333fecdd3e5c5e2ea36b39dba2131dc63
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-25 15:39:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.92 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int s = 1, e = 2;
         if(n == 1) {
             return 1;
         }
+        int answer = 0;
+        int s = 1, e = 2;
         //누적합
         int[] sum = new int[n + 1];
         for(int i=1; i<=n; i++) {
