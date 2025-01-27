4f1b33d655da5c674d40e8933475bbb90d94939b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-27 18:46:40 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 16.38 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,40 +1,25 @@
-class Solution
-{
-    static int N, M;
-    static int[][] cnt;
-    
-    public int solution(int[][] board)
-    {
-        int answer = 0;
-        N = board.length;
-        M = board[0].length;
-        cnt = new int[N][M];
+class Solution {
+    public int solution(int[][] board) {
+        int maxSide = 0;
+        int N = board.length;
+        int M = board[0].length;
+        int[][] dp = new int[N][M];
 
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
                 if(board[i][j] == 1) {
-                    solve(i, j, board);
+                    if(i == 0 || j == 0) {  //가장자리
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
                 }
             }
+            
         }
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                answer = Math.max(answer, cnt[i][j]);
-            }
-        }
-        return answer * answer;
-    }
-    public void solve(int x, int y, int[][] board) {
-        cnt[x][y] = 1;
         
-        if(isBound(x - 1, y - 1) && isBound(x - 1, y) && isBound(x, y - 1)) {
-            cnt[x][y] = Math.min(cnt[x - 1][y - 1], Math.min(cnt[x - 1][y], cnt[x][y - 1])) + 1;
-        }
-    }
-    public boolean isBound(int x, int y) {
-        if(x < 0 || x >= N || y < 0 || y >= M) {
-            return false;
-        }
-        return true;
+        return maxSide * maxSide;
     }
 }
