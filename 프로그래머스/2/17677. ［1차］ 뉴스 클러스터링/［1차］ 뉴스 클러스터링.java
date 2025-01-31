73fe04ed4791d99a0ade618832c289e5bf38528f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-01-31 16:03:52 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 13.43 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,35 +2,67 @@ import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        String[] arr1 = extractValidPairs(str1);
+        String[] arr2 = extractValidPairs(str2);
         
-        // 다중집합 생성
-        addElements(map1, str1);
-        addElements(map2, str2);
-
-        int intersection = 0, union = 0;
+        // 정렬
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         
-        // 합집합 & 교집합 계산
-        Set<String> keySet = new HashSet<>(map1.keySet());
-        keySet.addAll(map2.keySet());  // 두 개의 키를 합침
-
-        for (String key : keySet) {
-            int count1 = map1.getOrDefault(key, 0);
-            int count2 = map2.getOrDefault(key, 0);
-            
-            intersection += Math.min(count1, count2);
-            union += Math.max(count1, count2);
+        // Merge 과정에서 교집합, 합집합 계산
+        int[] result = mergeAndCalculate(arr1, arr2);
+        int intersection = result[0];
+        int union = result[1];
+        
+        // 공집합 처리
+        if (union == 0) {
+            return 65536;
         }
-
-        return (union == 0) ? 65536 : (int) ((double) intersection / union * 65536);
+        
+        // 자카드 유사도 계산
+        double similarity = (double) intersection / union;
+        return (int) (similarity * 65536);
     }
-
-    private void addElements(Map<String, Integer> map, String str) {
+    
+    // 유효한 2글자 쌍 추출
+    private String[] extractValidPairs(String str) {
+        str = str.toLowerCase();
+        List<String> tempList = new ArrayList<>();
         for (int i = 0; i < str.length() - 1; i++) {
-            String sub = str.substring(i, i + 2).toLowerCase();
-            if (sub.matches(".*[^a-z].*")) continue; // 특수문자 포함 시 제외
-            map.put(sub, map.getOrDefault(sub, 0) + 1);
+            char front = str.charAt(i);
+            char back = str.charAt(i + 1);
+            if (Character.isLetter(front) && Character.isLetter(back)) {
+                tempList.add("" + front + back);
+            }
         }
+        // 배열로 변환
+        return tempList.toArray(new String[0]);
+    }
+    
+    // 병합하여 교집합과 합집합 계산
+    private int[] mergeAndCalculate(String[] arr1, String[] arr2) {
+        int i = 0, j = 0;
+        int intersection = 0;
+        int union = 0;
+        
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i].equals(arr2[j])) {
+                intersection++;
+                union++;
+                i++;
+                j++;
+            } else if (arr1[i].compareTo(arr2[j]) < 0) {
+                union++;
+                i++;
+            } else {
+                union++;
+                j++;
+            }
+        }
+        
+        // 남은 요소 처리
+        union += (arr1.length - i) + (arr2.length - j);
+        
+        return new int[] {intersection, union};
     }
 }
