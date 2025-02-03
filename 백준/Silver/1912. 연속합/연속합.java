98f59c19addd5e9017a16e0d42785dc8e3ac9d70
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-03 19:54:33 +0900

    [Silver II] Title: 연속합, Time: 172 ms, Memory: 20936 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,25 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] arr, sum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        sum = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int max = -1001;
+        int sum = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        sum[0] = arr[0];
-        int max = arr[0];
-
-        for (int i = 1; i < N; i++) {
-            sum[i] = Math.max(arr[i] + sum[i - 1], arr[i]);
-            max = Math.max(max, sum[i]);
+            int current = Integer.parseInt(st.nextToken());
+            sum = Math.max(sum + current, current);
+            max = Math.max(max, sum);
         }
 
         System.out.println(max);
