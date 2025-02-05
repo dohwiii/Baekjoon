9a4fdbdbf7a131d770dbeaad5aaacaf7f8727d35
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-05 10:15:32 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 64 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -2,30 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MOD = 1000000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N + 1][10];
-        for (int i = 1; i < 10; i++) {
+        int[][] dp = new int[101][10];
+        int mod = 1_000_000_000;
+
+        for (int i = 1; i <= 9; i++) {
             dp[1][i] = 1;
         }
-
-        for (int i = 2; i <= N; i++)
-        {
-            dp[i][0] = dp[i - 1][1] % MOD;
-            dp[i][9] = dp[i - 1][8] % MOD;
-
-            for (int j = 1; j <= 8; j++) {
-
-                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1];
+                }
+                else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8];
+                }
+                else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+                }
             }
         }
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum = (sum + dp[N][i]) % MOD;
+        int result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result = (result + dp[N][i]) % mod;
         }
-        System.out.println(sum);
-
+        System.out.println(result);
     }
+
 }
