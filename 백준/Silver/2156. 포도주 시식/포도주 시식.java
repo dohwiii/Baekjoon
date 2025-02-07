c78e826034c81ec22abf60714c33462c02ca5727
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-07 10:46:37 +0900

    [Silver I] Title: 포도주 시식, Time: 88 ms, Memory: 13136 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,34 +1,28 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
-    static int[] wine;
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 포도주 잔 개수
 
-        int N = Integer.parseInt(br.readLine()); //포도주 잔의 개수
-        wine = new int[N + 1];
-        dp = new int[N + 1];
+        int[] wine = new int[n + 1]; // 포도주 양
+        int[] dp = new int[n + 1]; // DP 테이블
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             wine[i] = Integer.parseInt(br.readLine());
         }
 
+        // 초기값 설정
         dp[1] = wine[1];
-        if (N > 1) {
-            dp[2] = wine[1] + wine[2];
+        if (n >= 2) dp[2] = wine[1] + wine[2];
+        if (n >= 3) dp[3] = Math.max(wine[1] + wine[2], Math.max(wine[1] + wine[3], wine[2] + wine[3]));
+
+        // 점화식 적용
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
         }
 
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max((dp[i - 2] + wine[i]), (dp[i - 3] + wine[i - 1] + wine[i])));
-        }
-
-        System.out.println(dp[N]);
-
+        // 결과 출력
+        System.out.println(dp[n]);
     }
 }
