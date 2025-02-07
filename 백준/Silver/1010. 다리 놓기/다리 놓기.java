5c655bb27d1a8326703e12bc43455543442c392b
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-07 20:20:22 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,34 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        for (int t = 0; t < N; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken()); //M개 중에서 N개를 뽑아야 함
-            dp = new int[M + 1][M + 1];
+        int T = Integer.parseInt(br.readLine());
+        int[][] q = new int[T][2];
+        dp = new int[31][31];
 
-            for (int i = 1; i <= M; i++) {
-                dp[i][0] = 1;
-                dp[i][i] = 1;
-                dp[i][1] = i;
-            }
-            for (int i = 2; i <= M; i++) {
-                for (int j = 1; j < i; j++) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-                }
-            }
-            System.out.println(dp[M][N]);
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            q[i][0] = W;
+            q[i][1] = E;
         }
 
+        for (int i = 1; i < 30; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 2; i < 30; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+        for (int[] p : q) {
+            System.out.println(dp[p[1]][p[0]]);
+        }
+
+
+
+//        for (int i = 0; i < N; i++) {
+//            System.out.println(Arrays.toString(dp[i]));
+//        }
+
     }
+
 }
