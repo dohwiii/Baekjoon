d638fdf29d21ecb86be9e13f8b181956f50b7c5a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-09 19:57:27 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-
-            int N = Integer.parseInt(br.readLine());
-            int M = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());   //k층
+            int n = Integer.parseInt(br.readLine());   //n호
             int[][] dp = new int[15][15];
+            int[][] sum = new int[15][15];
 
-            for (int j = 0; j <= 14; j++) {
-                dp[0][j] = j;
-                dp[j][1] = 1;
+            //0층
+            for (int i = 1; i < 15; i++) {
+                dp[0][i] = i;
+                sum[0][i] = sum[0][i - 1] + dp[0][i];
             }
-            for (int j = 1; j <= 14; j++) {
-                for (int k = 2; k <= 14; k++) {
-                    dp[j][k] = dp[j - 1][k] + dp[j][k - 1];
+            for (int i = 1; i <= 14; i++) {
+                for (int j = 1; j <= 14; j++) {
+                    dp[i][j] = sum[i - 1][j];
+                    sum[i][j] = sum[i][j - 1] + dp[i][j];
                 }
             }
-            System.out.println(dp[N][M]);
+            System.out.println(dp[k][n]);
         }
 
     }
+
 }
