cfbd1faade810046e0703e479bcb3a84d48aa7d9
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-15 17:38:59 +0900

    [Silver II] Title: 이항 계수 2, Time: 80 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -1,27 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    public static void main(String[] args) throws IOException {
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[N + 1][N + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int mod = 10007;
+        dp = new int[N + 1][N + 1];
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             dp[i][1] = i;
-            dp[i][0] = 1;
             dp[i][i] = 1;
+            dp[i][0] = 1;
         }
+
         for (int i = 2; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % 10007;
+            for (int j = 2; j <= i; j++) {
+                if (dp[i][j] == 0) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                }
             }
         }
-        System.out.println(dp[N][M]);
+        System.out.println(dp[N][K]);
+
+
+
     }
+
 }
