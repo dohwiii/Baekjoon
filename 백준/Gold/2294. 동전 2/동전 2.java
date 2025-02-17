bd15bb564768f2894d25bc01556e2f5b654861fa
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-17 11:19:21 +0900

    [Gold V] Title: 동전 2, Time: 84 ms, Memory: 12520 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,41 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        // 빠른 입출력을 위한 BufferedReader 사용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] dp = new int[100001];
-        int[] coins = new int[N];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            dp[num] = 1;
-            coins[i] = num;
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
+        
+        // dp[i] : i원을 만들기 위해 필요한 최소 동전 개수
+        // dp[0] = 0으로 시작, 나머지는 매우 큰 값으로 초기화
+        int INF = Integer.MAX_VALUE - 1; // 오버플로우 방지를 위해 -1 해줌
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, INF);
         dp[0] = 0;
-        for (int i = 1; i <= K; i++) {
-            if (dp[i] == 1) {
-                continue;
-            }
-            int money = i;
-            for (int j = 0; j < N; j++) {
-                int coin = coins[j];    //현재 코인
-                if (money < coin) { //어짜피 이 코인 못 씀
-                    continue;
+        
+        // Bottom-Up 방식으로 dp 배열 채우기
+        // 1원부터 k원까지 각각 가능한 동전들로 최소 개수를 구함
+        for (int i = 1; i <= k; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
-                if (dp[money - coin] <= 0 || dp[coin] <= 0 || dp[money - coin] == 2147483647 || dp[coin] == 2147483647) {
-                    continue;
-                }
-                dp[i] = Math.min(dp[i], dp[money - coin] + dp[coin]);
             }
-
         }
-        int answer = dp[K] == Integer.MAX_VALUE ? -1 : dp[K];
-        System.out.println(answer);
+        
+        // dp[k]가 INF이면 만들 수 없는 경우이므로 -1 출력
+        System.out.println(dp[k] == INF ? -1 : dp[k]);
     }
 }
