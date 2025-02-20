a857e679464bca99106dfef99f18a6e61162f6f2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-20 16:20:06 +0900

    [Silver I] Title: 오르막 수, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,30 +1,31 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[][] dp = new long[N + 1][10];
+        int MOD = 10007;
 
-        //한 자리 수
-        for (int i = 0; i <= 9; i++) {
-            dp[1][i] = 1;
-        }
-        for (int i = 1; i <= N; i++) {
-            dp[i][0] = 1;
-        }
+        int[][] dp = new int[N+1][10];
 
+        // 초기화: 길이가 1인 오르막 수는 각각 1개
+        Arrays.fill(dp[1], 1);
+
+        // DP 계산
         for (int i = 2; i <= N; i++) {
-            for (int j = 1; j <= 9; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
+            dp[i][0] = 1;
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
             }
         }
-        long sum = 0;
-        for (int i = 0; i <= 9; i++) {
-            sum += dp[N][i];
-        }
-        System.out.println(sum % 10007);
 
+        // 최종 결과 (0~9까지 모두 더한 값)
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + dp[N][i]) % MOD;
+        }
+
+        System.out.println(sum);
     }
 }
