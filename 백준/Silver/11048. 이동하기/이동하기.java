27a00d4ed805c2e52efca5b906f3a0771ad1b9c7
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-24 11:43:30 +0900

    [Silver II] Title: 이동하기, Time: 352 ms, Memory: 76876 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -3,40 +3,24 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[][] map, dp;
+    static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        dp = new int[N][M];
+        dp = new int[N + 1][M + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        dp[0][0] = map[0][0];
-
-        if (N > 1 || M > 1) {
-            for (int i = 1; i < N; i++) {
-                dp[i][0] = dp[i - 1][0] + map[i][0];
-            }
-            for (int i = 1; i < M; i++) {
-                dp[0][i] = dp[0][i - 1] + map[0][i];
-            }
-            for (int i = 1; i < N; i++) {
-                for (int j = 1; j < M; j++) {
-                    dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1])) + map[i][j];
-                }
+            for (int j = 1; j <= M; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                dp[i][j] = Math.max(dp[i - 1][j - 1], Math.max(dp[i - 1][j], dp[i][j - 1])) + num;
             }
         }
 
-
-        System.out.println(dp[N - 1][M - 1]);
+        System.out.println(dp[N][M]);
     }
 
 }
