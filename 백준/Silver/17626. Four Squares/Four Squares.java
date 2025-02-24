29f8835091d0523c8c6e13c7647aa0bdfca27595
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-02-24 15:35:43 +0900

    [Silver III] Title: Four Squares, Time: 88 ms, Memory: 11948 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,27 +1,19 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
 
-        for (int i = 2; i <= N; i++) {
-            int now = i;
-            int max = (int) Math.sqrt(i);
-            int min = Integer.MAX_VALUE;
-
-            while (max > 0) {
-                int num = max * max;
-                min = Math.min(min, dp[now - num] + dp[num]);
-                max--;
+        // DP 초기화
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;  // 최악의 경우, 모두 1^2로 구성 (ex: 5 = 1^2 + 1^2 + 1^2 + 1^2 + 1^2)
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
-            dp[now] = min;
         }
-        System.out.println(dp[N]);
-    }
 
+        System.out.println(dp[n]);
+    }
 }
