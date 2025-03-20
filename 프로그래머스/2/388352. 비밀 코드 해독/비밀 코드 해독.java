e728be2eb09b20aa43ed034ac39a6fece43b6639
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-03-20 23:11:29 +0900

    [level 2] Title: 비밀 코드 해독, Time: 74.81 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,38 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static List<int[]> arrList;
     static int answer;
+    
     public int solution(int n, int[][] q, int[] ans) {
-        arrList = new ArrayList<>();
+        answer = 0;
         int[] arr = new int[5];
         combi(0, 1, n, arr, q, ans);
         return answer;
     }
+
     public void combi(int depth, int start, int n, int[] arr, int[][] q, int[] ans) {
-        if(depth == 5) {
-            if(checkMatch(arr, q, ans)) {    //정답
+        if (depth == 5) {
+            if (checkMatch(arr, q, ans)) {  
                 answer++;
             }
             return;
         }
-        for(int i=start; i<=n; i++) {
+        for (int i = start; i <= n; i++) {
             arr[depth] = i;
-            combi(depth+1, i+1, n, arr, q, ans);
+            combi(depth + 1, i + 1, n, arr, q, ans);
         }
     }
-    public boolean checkMatch(int[] arr, int[][] q, int[] ans) {  //주어진 q배열과 비교
-        for(int k=0; k<q.length; k++) {
-            int[] qr = q[k];
-            int cnt = 0;
-            for(int i=0; i<5; i++) {
-                for(int j=0; j<5; j++) {
-                    if(qr[i] == arr[j]) {
-                        cnt++;
-                    }
-                }
+
+    public boolean checkMatch(int[] arr, int[][] q, int[] ans) {
+        Set<Integer> codeSet = new HashSet<>();
+        for (int num : arr) codeSet.add(num);  // 현재 비밀 코드 후보를 Set에 저장
+
+        for (int k = 0; k < q.length; k++) {
+            int matchCount = 0;
+            for (int num : q[k]) {
+                if (codeSet.contains(num)) matchCount++;  // 빠른 검색 O(1)
             }
-            if(cnt != ans[k]) {
+            if (matchCount != ans[k]) {
                 return false;
             }
         }
