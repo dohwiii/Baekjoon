141aa2590212faeda7215d14289cca852efe11d6
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-03-20 17:34:12 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10092 ms, Memory: 144776 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,86 +1,58 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
-import java.sql.Time;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] list;
-    static boolean[] visited;
-    static int[] answer;
+    static List<Integer>[] graph;
+    static int N;
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); //컴퓨터 개수
-        int M = Integer.parseInt(st.nextToken()); //신뢰 관계 개수
-
-        list = new ArrayList[N + 1];
-        answer = new int[N + 1];
-        ArrayList<Integer> success = new ArrayList<>();
-
+        graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++)
-        {
+        // B를 해킹하면 A도 해킹 가능하므로, B -> A 방향으로 저장
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-
-            list[A].add(B);
-        }
-        for (int i = 1; i <= N; i++)
-        {
-            visited = new boolean[N + 1];
-            BFS(i);
-        }
-        int max = -1;
-        for (int i = 1; i <= N; i++) {
-            if (max < answer[i]) {
-                max = answer[i];
-            }
+            graph[B].add(A);
         }
 
-
-        for (int i = 1; i <= N; i++) {
-            if (max == answer[i]) {
-                success.add(i);
-            }
-        }
-        Collections.sort(success);
+        int[] counts = new int[N + 1];
+        int maxCount = 0;
         
-        for (int i : success) {
-            System.out.print(i + " ");
+        // 각 노드를 시작점으로 독립적인 DFS 실행
+        for (int i = 1; i <= N; i++) {
+            boolean[] visited = new boolean[N + 1];
+            counts[i] = dfs(i, visited);
+            maxCount = Math.max(maxCount, counts[i]);
         }
-
-    }
-
-    public static void BFS(int node)
-    {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(node);
-        visited[node] = true;
-
-        while (!queue.isEmpty())
-        {
-            int next = queue.poll();
-
-            for (int i : list[next])
-            {
-                if (visited[i] == false) {
-
-                    visited[i] = true;
-                    answer[i]++;
-                    queue.add(i);
-
-                }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (counts[i] == maxCount) {
+                sb.append(i).append(" ");
             }
-
         }
+        
+        System.out.println(sb);
     }
 
+    // DFS: start 노드에서 해킹할 수 있는 컴퓨터의 수를 반환
+    static int dfs(int node, boolean[] visited) {
+        visited[node] = true;
+        int count = 0;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                count += dfs(next, visited) + 1;
+            }
+        }
+        return count;
+    }
 }
