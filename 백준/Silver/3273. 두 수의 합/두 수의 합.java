2ace3373d17cdcee5c1ce88d85e11f9e270100b5
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-01 09:46:27 +0900

    [Silver III] Title: 두 수의 합, Time: 232 ms, Memory: 26084 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,43 +1,47 @@
-import java.io.*;
-import java.text.ParseException;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+    static int N, X;
 
-    public static void main(String[] args) throws IOException, ParseException {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int X = Integer.parseInt(br.readLine());
+        X = Integer.parseInt(br.readLine());
         Arrays.sort(arr);
+        System.out.println(solve());
 
-        int answer = 0;
-        int start = 0;
-        int end = N - 1;
 
-        while (start < end) {
-
-            int sum = arr[start] + arr[end];
-
-            if (sum == X) {
-                answer++;
-                start++;
-                end--;
-            }
-            else if (sum > X) {
-                end--;
-            }
-            else if (sum < X) {
-                start++;
-            }
-        }
-        System.out.println(answer);
     }
 
+    public static int solve() {
+        int s = 0;
+        int e = N - 1;
+        int cnt = 0;
+
+        while (s < e) {
+            int result = arr[s] + arr[e];
+
+            if (result == X) {
+                cnt++;
+                s++;
+                e--;
+            } else if (result > X) {
+                e--;
+            } else {
+                s++;
+            }
+        }
+        return cnt;
+    }
 }
