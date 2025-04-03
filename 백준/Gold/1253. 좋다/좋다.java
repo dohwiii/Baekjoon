e4a51066284579d3c4b316372b94ac6309485c4d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-03 18:23:21 +0900

    [Gold IV] Title: 좋다, Time: 124 ms, Memory: 12680 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,54 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-
-        for (int i = 0; i < n; i++)
-        {
-            long find = arr[i];
-            int start = 0;
-            int end = n - 1;
-
-            while (start < end)
-            {
-                if (arr[start] + arr[end] == find)
-                {
-                    if (start != i && end != i) {
-                        count++;
-                        break;
-                    } else if (start == i) {
-                        start++;
-
-                    } else if (end == i) {
-                        end--;
-                    }
-
-                }
-                else if(arr[start] + arr[end] < find)
-                {
-                    start++;
-
-                }
-                else
-                {
-                    end--;
-                }
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            if (solve(arr, N, arr[i], i)) {
+//                System.out.println(arr[i]);
+                ans++;
             }
+
+        }
+        System.out.println(ans);
+    }
+
+    public static boolean solve(int[] arr, int n, int target, int index) {
+        int s = 0, e = n - 1;
+        if (s == index) {
+            s++;
+        } else if (e == index) {
+            e--;
         }
 
-        System.out.println(count);
+        while (s < e) {
+            long re = arr[s] + arr[e];
 
-
+            if (re == target) {
+                return true;
+            } else if (re < target) {
+                s++;
+            } else {
+                e--;
+            }
+            if (s == index) {
+                s++;
+            }
+            else if (e == index) {
+                e--;
+            }
+        }
+        return false;
     }
+
 }
