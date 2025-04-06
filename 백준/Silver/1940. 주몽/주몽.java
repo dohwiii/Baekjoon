5f009d3f62068fb3a0a48c8871fc1e177a272f9e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-06 10:18:29 +0900

    [Silver IV] Title: 주몽, Time: 140 ms, Memory: 15592 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,43 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int N, M;
+    static int[] arr;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n];
-        int start = 0;
-        int end = arr.length - 1;
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
+        System.out.println(solve());
 
-        while (start < end)
-        {
-            if (arr[start] + arr[end] == m)
-            {
-                count++;
-                start++;
-                end--;
 
-            }
-            else if(arr[start] + arr[end] < m)
-            {
-                start++;
+    }
 
-            }
-            else
-            {
-                end--;
+    public static int solve() {
+        int s = 0, e = N - 1;
+        int ans = 0;
+
+        while (s < e) {
+            int result = arr[s] + arr[e];
+
+            if (result > M) {
+                e--;
+            } else {
+                if (result == M) {
+                    ans++;
+                }
+                s++;
             }
         }
-        System.out.println(count);
-
-
+        return ans;
     }
 }
