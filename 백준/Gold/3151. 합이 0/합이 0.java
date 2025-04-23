4827b5f3a262627f161859b5ec377412969b2b54
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-23 11:24:23 +0900

    [Gold IV] Title: 합이 0, Time: 196 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -1,63 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+
+        //범위가 -10000 ~ 10000 => 세 수의 합의 범위는 -30000 ~ 30000
+        int[] student = new int[40001];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        long ans = 0;
 
-        for (int i = 0; i < N - 2; i++) {
-            int fix = arr[i];
-            ans += solve(i + 1, N - 1, fix, arr);
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += student[20000 - num[i]];
+            for (int j = 0; j < i; j++) {
+                student[20000 + num[i] + num[j]]++;
+            }
         }
-        System.out.println(ans);
+        System.out.println(cnt);
     }
-
-    public static long solve(int s, int e, int fix, int[] arr) {
-        long ans = 0;
-        while (s < e) {
-            long sum = fix + arr[s] + arr[e];
-
-            if (sum == 0) {
-                //중복값 처리
-                if (arr[s] == arr[e]) {
-                    int len = e - s + 1;
-                    ans += (long) len * (len - 1) / 2;
-                    break;
-                }
-                int countLeft = 1;
-                int countRight = 1;
-
-                while (s + 1 < e && arr[s] == arr[s + 1]) {
-                    countLeft++;
-                    s++;
-                }
-                while (e - 1 > s && arr[e] == arr[e - 1]) {
-                    countRight++;
-                    e--;
-                }
-                ans += (long) countLeft * countRight;
-                s++;
-                e--;
-            }
-            else if (sum > 0) {
-                e--;
-            }
-            else {
-                s++;
-            }
-        }
-        return ans;
-    }
-
-
 }
