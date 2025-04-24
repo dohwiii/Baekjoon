9e61f210b1bbdca7983ce38f7856e481081876ab
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-24 15:39:36 +0900

    [Silver IV] Title: 올바른 배열, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/1337. 올바른 배열/올바른 배열.java
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.java
@@ -11,22 +11,22 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
-        Arrays.sort(arr);  // 정렬 필수
-
-        int answer = 5;  // 최대로 5개를 추가할 수도 있으므로 초기값은 5
-
-        int left = 0;
-        for (int right = 0; right < N; right++) {
-            // 연속 범위가 5를 넘으면 왼쪽 포인터 이동
-            while (arr[right] - arr[left] >= 5) {
-                left++;
+        Arrays.sort(arr);
+        int min = 5;
+        int s = 0, e = 0;
+        while (e < N) {
+            int diff = arr[e] - arr[s];
+            if (diff < 5) {
+                int len = e - s + 1;
+                min = Math.min(min, 5 - len);
+                e++;
+            } else {
+                s++;
             }
 
-            int inRange = right - left + 1;       // 현재 구간 안에 이미 있는 수
-            answer = Math.min(answer, 5 - inRange); // 최소 추가할 숫자 수
         }
+        System.out.println(min);
 
-        System.out.println(answer);
+
     }
 }
