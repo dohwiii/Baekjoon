c00c2ca43e7ca62df74c455d9a0626ccac577dd3
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-27 21:25:06 +0900

    [Gold IV] Title: 고냥이, Time: 88 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,39 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.security.Key;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
-        String s = br.readLine().trim();
-        int L = s.length();
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        char[] arr = str.toCharArray();
 
-        int[] freq = new int[26];
-        int distinct = 0, maxLen = 0;
-        int left = 0;
+        int len = arr.length;
+        int maxLen = 0;
+        int s = 0, e = 0;
+        int[] freq = new int[27];
+        int distinct = 0;
 
-        for (int right = 0; right < L; right++) {
-            int c = s.charAt(right) - 'a';
-            if (freq[c] == 0) {
-                distinct++;
-            }
-            freq[c]++;
-
-            // distinct가 N을 초과하면 left를 움직여 줄인다
-            while (distinct > N) {
-                int d = s.charAt(left) - 'a';
-                freq[d]--;
-                if (freq[d] == 0) {
-                    distinct--;
+        while (s < len && e < len) {
+            int ch = arr[e] - 'a';
+            if (freq[ch] == 0) {    //아직 추가하지 않은 알파벳
+                if (distinct < N) { //추가 횟수 남아있음
+                    freq[ch]++;
+                    distinct++;
+                    e++;
                 }
-                left++;
+                else {  //추가 못함
+                    freq[arr[s] - 'a']--;
+                    if (freq[arr[s] - 'a'] == 0) {  //시작 알파벳 문장에서 싹 빠졌다면
+                        distinct--;
+                    }
+                    s++;
+
+                }
             }
+            else {  //이미 추가한 알파벳
+                freq[ch]++;
+                e++;
+            }
+            maxLen = Math.max(maxLen, e - s);
 
-            // 현재 [left..right] 구간의 길이 갱신
-            maxLen = Math.max(maxLen, right - left + 1);
         }
-
         System.out.println(maxLen);
+
+
     }
+
 }
