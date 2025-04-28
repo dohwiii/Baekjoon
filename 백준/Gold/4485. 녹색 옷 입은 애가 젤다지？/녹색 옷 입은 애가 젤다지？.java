7538209b2027799a907c9759668933c3e8f96a6c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-28 16:39:33 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 152 ms, Memory: 17276 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,98 +1,74 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] map;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static boolean[][] visited;
-    static StringBuilder sb = new StringBuilder();
-    static int result;
+    static int[][] map, dp;
+    static final int INF = 200000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-
+        int index = 1;
         while (true) {
-            N = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
             if (N == 0) {
-                System.out.println(sb.toString());
                 break;
             }
-            result = 0;
-            index++;
             map = new int[N][N];
-            visited = new boolean[N][N];
-
+            dp = new int[N][N];
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
+                    dp[i][j] = INF;
                 }
             }
-            solve(0, 0);
-            sb.append("Problem " + index + ": " + result);
-            sb.append("\n");
-        }
+            dp[0][0] = map[0][0];
+            PriorityQueue<Pos> queue = new PriorityQueue<>();
+            queue.offer(new Pos(0, 0, map[0][0]));
 
+            while (!queue.isEmpty()) {
+                Pos now = queue.poll();
+                if (dp[now.x][now.y] < now.cost) {
+                    continue;
+                }
 
-    }
+                for (int i = 0; i < 4; i++) {
+                    int nx = now.x + dx[i];
+                    int ny = now.y + dy[i];
 
-    public static void solve(int x, int y) {
-        PriorityQueue<Pos> pqueue = new PriorityQueue<>();
-        pqueue.add(new Pos(x, y, 0));
-        visited[x][y] = true;
-
-        int[][] dist = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        dist[0][0] = map[0][0];
-
-        while (!pqueue.isEmpty()) {
-            Pos now = pqueue.poll();
-            //도착지점
-            if (now.x == N - 1 && now.y == N - 1) {
-                result = dist[now.x][now.y];
-                return;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                    if (!visited[nx][ny]) {
-                        if (dist[nx][ny] > dist[now.x][now.y] + map[nx][ny]) {
-                            dist[nx][ny] = dist[now.x][now.y] + map[nx][ny];
-                            pqueue.add(new Pos(nx, ny, dist[now.x][now.y] + map[nx][ny]));
-                        }
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                        continue;
+                    }
+                    if (dp[nx][ny] > dp[now.x][now.y] + map[nx][ny]) {
+                        dp[nx][ny] = dp[now.x][now.y] + map[nx][ny];
+                        queue.offer(new Pos(nx, ny, dp[nx][ny]));
                     }
                 }
-
             }
+            sb.append("Problem ").append(index + ": ");
+            sb.append(dp[N - 1][N - 1] + "\n");
+            index++;
         }
+        System.out.println(sb.toString());
+
     }
 
+    static class Pos implements Comparable<Pos> {
+        int x, y, cost;
 
-}
+        public Pos(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
 
-class Pos implements Comparable<Pos> {
-    int x, y, money;
-
-    public Pos(int x, int y, int money) {
-        this.x = x;
-        this.y = y;
-        this.money = money;
-    }
-
-    @Override
-    public int compareTo(Pos o) {
-        return this.money - o.money;
+        @Override
+        public int compareTo(Pos o) {
+            return this.cost - o.cost;
+        }
     }
 }
