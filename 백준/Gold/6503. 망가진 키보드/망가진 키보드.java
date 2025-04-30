c0019a431f2e7e543287f0f0911c4a627e8c8f28
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-04-30 12:27:37 +0900

    [Gold V] Title: 망가진 키보드, Time: 280 ms, Memory: 45672 KB -BaekjoonHub

--- 백준/Gold/6503. 망가진 키보드/망가진 키보드.java
+++ 백준/Gold/6503. 망가진 키보드/망가진 키보드.java
@@ -21,17 +21,17 @@ public class Main {
 
             while (s < arr.length && e < arr.length) {
                 char alp = arr[e];
-                if (cnt[alp - ' '] > 0) {   //이미 문자열에 포함됐다면
-                    cnt[alp - ' ']++;
+                if (cnt[alp] > 0) {   //이미 문자열에 포함됐다면
+                    cnt[alp]++;
                     e++;
                 } else {  //처음 등장
                     if (distinct < M) { //알파벳 추가 가능
-                        cnt[alp - ' ']++;
+                        cnt[alp]++;
                         distinct++;
                         e++;
                     } else {  //추가 불가능
-                        cnt[arr[s] - ' ']--;
-                        if (cnt[arr[s] - ' '] == 0) {   //arr[s]의 문자가 다 사라졌다면
+                        cnt[arr[s]]--;
+                        if (cnt[arr[s]] == 0) {   //arr[s]의 문자가 다 사라졌다면
                             distinct--;
                         }
                         s++;
