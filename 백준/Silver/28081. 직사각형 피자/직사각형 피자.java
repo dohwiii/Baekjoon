7b95373dfde67f73db0bfca33295fe2bf198155a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-05-06 22:45:06 +0900

    [Silver I] Title: 직사각형 피자, Time: 316 ms, Memory: 23148 KB -BaekjoonHub

--- 백준/Silver/28081. 직사각형 피자/직사각형 피자.java
+++ 백준/Silver/28081. 직사각형 피자/직사각형 피자.java
@@ -1,52 +1,65 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int readInt() throws IOException {
+        int c, n = System.in.read() & 15; // 첫 숫자 처리
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15); // n * 10 + 숫자
+        }
+        return n;
+    }
+
+    static long readLong() throws IOException {
+        int c = System.in.read();
+        while (c <= 32) c = System.in.read(); // 공백 넘기기
+
+        long n = c & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int W = Integer.parseInt(st.nextToken());   //가로
-        int H = Integer.parseInt(st.nextToken());   //세로
-        long K = Long.parseLong(st.nextToken());   //최대 K크기까지
-        int N = Integer.parseInt(br.readLine());
-        int tempW = W, tempH = H;
-        long[] widths = new long[N + 1];   //가로 (y)
-        st = new StringTokenizer(br.readLine());
-        int prevW = 0;
+        int W = readInt();   // 가로
+        int H = readInt();   // 세로
+        long K = readLong(); // 최대 조각 크기
+
+        int N = readInt();   // y 커팅 개수
+        long[] heights = new long[N + 1];
+        int prevY = 0;
         for (int i = 0; i < N; i++) {
-            int w = Integer.parseInt(st.nextToken());
-            widths[i] = w - prevW;
-            prevW = w;
+            int y = readInt();
+            heights[i] = y - prevY;
+            prevY = y;
         }
-        widths[N] = H - prevW;
-        int M = Integer.parseInt(br.readLine());
-        long[] heights = new long[M + 1];  //세로 (x)
-        st = new StringTokenizer(br.readLine());
-        int prevH = 0;
+        heights[N] = H - prevY;
+
+        int M = readInt();   // x 커팅 개수
+        long[] widths = new long[M + 1];
+        int prevX = 0;
         for (int i = 0; i < M; i++) {
-            int h = Integer.parseInt(st.nextToken());
-            heights[i] = h - prevH;
-            prevH = h;
+            int x = readInt();
+            widths[i] = x - prevX;
+            prevX = x;
         }
-        heights[M] = W - prevH;
+        widths[M] = W - prevX;
+
         Arrays.sort(widths);
         Arrays.sort(heights);
 
-        int j = M;
+        int j = heights.length - 1;
         long ans = 0;
-        for (int i = 0; i <= N; i++) {
-            while (j >= 0 && 1L * widths[i] * heights[j] > K) {
+        for (int i = 0; i < widths.length; i++) {
+            while (j >= 0 && widths[i] * heights[j] > K) {
                 j--;
             }
             if (j >= 0) {
-                ans += (j + 1);
+                ans += j + 1;
             }
-
         }
+
         System.out.println(ans);
-
-
     }
-
 }
