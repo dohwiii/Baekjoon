378621e90ee136af97fe1e453eb0f2145a4c430c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-05-11 18:43:01 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 132 ms, Memory: 11968 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -5,16 +5,8 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Pair {
-        long sum;
-        int i, j;
+    static long minDiff = 2_000_000_000;
 
-        public Pair(long sum, int i, int j) {
-            this.sum = sum;
-            this.i = i;
-            this.j = j;
-        }
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -23,45 +15,38 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int M = N * (N - 1) / 2;    //2개씩 뽑을 때 나올 수 있는 조합 개수
-        Pair[] pairSum = new Pair[M];
-        long[] sumArr = new long[M];
-        int index = 0;
-        //합 쌍 구하기
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                pairSum[index++] = new Pair(arr[i] + arr[j], i, j);
-            }
-        }
-        //sum 기준으로 오름차순
-        Arrays.sort(pairSum, new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                return Long.compare(o1.sum, o2.sum);
-            }
-        });
-
-        for (int i = 0; i < M; i++) {
-            sumArr[i] = pairSum[i].sum;
-        }
-        long minDiff = 1_000_000_000;
-
-        for (int i = 0; i < M; i++) {
-            Pair p = pairSum[i];
-
-            for (int j = i + 1; j < M; j++) {
-                Pair q = pairSum[j];
-                if (p.i == q.i || p.i == q.j || p.j == q.i || p.j == q.j) {
-                    continue;
+        Arrays.sort(arr);
+        //(i, j)쌍 고정시키기
+        a:
+        for (int i = 0; i < N - 3; i++) {
+            for (int j = i + 3; j < N; j++) {
+                snow(i, j, arr);
+                if (minDiff == 0) {
+                    break a;
                 }
-                long result = Math.abs(p.sum - q.sum);
-                minDiff = Math.min(minDiff, result);
-                break;
             }
         }
 
         System.out.println(minDiff);
 
 
+    }
+
+    public static void snow(int i, int j, int[] arr) {
+        long comp = arr[i] + arr[j];
+        int s = i + 1, e = j - 1;
+
+        while (s < e) {
+            long sum = arr[s] + arr[e];
+            minDiff = Math.min(minDiff, Math.abs(comp - sum));
+
+            if (sum < comp) {
+                s++;
+            } else if (sum > comp) {
+                e--;
+            } else {
+                return;
+            }
+        }
     }
 }
