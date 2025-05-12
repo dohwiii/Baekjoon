ffc6d114922e1e97150ea3f2113e0b5673233191
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-05-12 09:18:52 +0900

    [Silver II] Title: K보다 큰 구간, Time: 180 ms, Memory: 21536 KB -BaekjoonHub

--- 백준/Silver/14246. K보다 큰 구간/K보다 큰 구간.java
+++ 백준/Silver/14246. K보다 큰 구간/K보다 큰 구간.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int K = Integer.parseInt(br.readLine());
-
+        int s = 0, e = 0;
         long sum = 0;
-        int s = 1, e = 1;
         long ans = 0;
-
-        while (s <= N && e <= N) {
-            while (e <= N && sum + arr[e] <= K) {
-                sum += arr[e];
-                e++;
+        while (s < N && e < N) {
+            sum += arr[e];
+            while (sum > K) {
+                sum -= arr[s];
+                s++;
             }
-            ans += (N - e + 1);
-            sum -= arr[s];
-            s++;
+            e++;
+            ans += s;
         }
-
         System.out.println(ans);
-
-
     }
 }
