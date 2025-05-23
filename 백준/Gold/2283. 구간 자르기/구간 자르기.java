761ff75775afada5155cadaacd4cb95f2c3aa73d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-05-23 10:59:18 +0900

    [Gold II] Title: 구간 자르기, Time: 136 ms, Memory: 16628 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.java
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.java
@@ -3,68 +3,44 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Line {
-        int s, e;
-
-        public Line(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Line[] lines = new Line[N];
+        int[] lines = new int[1_000_001];
         int max = 0;
-
+        int min = 1_000_000;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            lines[i] = new Line(s, e);
-//            start = Math.min(start, s);
-//            end = Math.max(end, e);
+            lines[s]++;
+            lines[e]--;
+            min = Math.min(min, s);
             max = Math.max(max, e);
         }
-        int start = 0, end = 1;
-
-        StringBuilder sb = new StringBuilder();
-        boolean isFound = false;
-        while (start <= max && end <= max && start < end) {
-            long len = checkLength(start, end, lines);
-            if (len == K) {
-                isFound = true;
-                sb.append(start + " " + end);
-                break;
-            } else if (len > K) {
+        //누적합
+        for (int i = min + 1; i <= max; i++) {
+            lines[i] += lines[i - 1];
+        }
+        long sum = 0;
+        int start = 0, end = min;
+        while (end <= max) {
+            if (sum == K) {
+                System.out.println(start + " " + end);
+                return;
+            }
+            else if (sum > K) {
+                sum -= lines[start];
                 start++;
-                if (start == end) {
-                    end = start + 1;
-                }
-            } else if (len < K) {
+            }
+            else if (sum < K) {
+                sum += lines[end];
                 end++;
             }
         }
-        if (!isFound) {
-            System.out.println("0 0");
-            return;
-        }
-        System.out.println(sb);
-
-
-    }
-
-    public static long checkLength(int start, int end, Line[] lines) {
-        long length = 0;
-        for (int i = 0; i < lines.length; i++) {
-            int s = lines[i].s;
-            int e = lines[i].e;
-            int overlap = Math.max(0, Math.min(e, end) - Math.max(s, start));
-            length += overlap;
-        }
-        return length;
+        System.out.println("0 0");
     }
 }
