d3afdd28168efa2fbf9e193cccac16013d6af76e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-05-27 00:09:04 +0900

    [Gold V] Title: Closest Pair, Time: 904 ms, Memory: 165576 KB -BaekjoonHub

---
+++ 백준/Gold/14746. Closest Pair/Closest Pair.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   //P 개수
+        int M = Integer.parseInt(st.nextToken());   //Q 개수
+        st = new StringTokenizer(br.readLine());
+        int c1 = Integer.parseInt(st.nextToken());
+        int c2 = Integer.parseInt(st.nextToken());
+        int[] P = new int[N];
+        int[] Q = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            Q[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(P);
+        Arrays.sort(Q);
+        int dist = Math.abs(c1 - c2);
+        int s1 = 0, s2 = 0;
+        int minDiff = 200_000_000;
+        int pair = 0;
+
+        while (s1 < N && s2 < M) {
+            int p = P[s1];
+            int q = Q[s2];
+            int result = Math.abs(p - q);
+            if (minDiff > result) {
+                minDiff = result;
+            }
+            if (minDiff == 0) {
+                break;
+            }
+            if (p > q) {
+                s2++;
+            }
+            else if (p < q) {
+                s1++;
+            }
+
+        }
+        s1 = 0;
+        s2 = 0;
+        while (s1 < N && s2 < M) {
+            int p = P[s1];
+            int q = Q[s2];
+            int diff = Math.abs(p - q);
+
+            if (diff == minDiff) {
+                pair++;
+                if (p < q) s1++;
+                else s2++;
+            }
+            else if (p < q) {
+                s1++;
+            } else {
+                s2++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(dist + minDiff + " " + pair);
+
+        System.out.println(sb);
+
+
+    }
+}
