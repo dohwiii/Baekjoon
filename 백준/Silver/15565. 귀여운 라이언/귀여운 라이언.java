1810be24035e795c58523f4828b892ccfd291b70
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-07 21:04:35 +0900

    [Silver I] Title: 귀여운 라이언, Time: 324 ms, Memory: 76636 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,39 +1,43 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static int[] dolls;
-    static List<Integer> list = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        dolls = new int[N];
-        int idx = 0;
-        int result = 0;
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            if (Integer.parseInt(st.nextToken()) == 1) {    //라이언
-                dolls[idx++] = i;    //라이언 인형의 인덱스를 저장
-            }
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        if (idx < K) {
-            result = -1;
+        int cnt = 0;
+        int s = 0, e = 0;
+        int minLen = 1_000_000;
+
+        while (e < N) {
+            if (arr[e] == 1) {  //라이언
+                cnt++;
+            }
+            e++;
+            while (cnt >= K) {
+                minLen = Math.min(minLen, e - s);
+                if (arr[s] == 1) {
+                    cnt--;
+                }
+                s++;
+            }
+
+        }
+        if (minLen == 1_000_000) {
+            System.out.println(-1);
         }
         else {
-            result = dolls[K - 1] - dolls[0] + 1;
-            for (int i = 1; i < idx - K + 1; i++) {
-                int end = i + K - 1;
-                result = Math.min(result, dolls[end] - dolls[i] + 1);
-            }
+            System.out.println(minLen);
         }
-        System.out.println(result);
 
     }
 }
