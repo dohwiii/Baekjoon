99efc2ca748becbff008a8befdf208dcbc3576e2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-10 22:21:30 +0900

    [Gold V] Title: 흙길 보수하기, Time: 160 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -16,34 +16,32 @@ public class Main {
             puddles[i][0] = Integer.parseInt(st.nextToken());
             puddles[i][1] = Integer.parseInt(st.nextToken());
         }
-        
-        // 시작 위치 기준으로 정렬
-        Arrays.sort(puddles, (o1, o2) -> o1[0] - o2[0]);
-        
-        int lastPos = -1;    // 널빤지가 덮는 마지막 위치 (초기값은 -1)
-        int cnt = 0;
-        
+        Arrays.sort(puddles, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        int lastPos = -1;    //널빤지의 마지막 위치
+        long cnt = 0;
         for (int i = 0; i < N; i++) {
-            int start = puddles[i][0];
-            int end = puddles[i][1];
-            
-            if (lastPos < start) {
-                // 새로운 널빤지가 필요한 경우
-                int len = end - start;    // 웅덩이 길이
-                int need = (len + L - 1) / L;    // 필요한 널빤지 개수 (올림 계산)
-                lastPos = start + L * need - 1;  // 마지막 널빤지의 끝 위치
+            int[] temp = puddles[i];
+            if (lastPos < temp[0]) {    //새로운 널빤지 설치
+                int len = temp[1] - temp[0];    //길이
+                int need = (int) Math.ceil(len / (double) L);    //필요한 널빤지 개수
+                lastPos = temp[0] + L * need - 1;
                 cnt += need;
-            } else if (lastPos < end - 1) {
-                // 기존 널빤지로 일부만 덮이는 경우, 추가 널빤지 필요
-                int uncoveredStart = lastPos + 1;  // 덮이지 않은 부분의 시작
-                int len = end - uncoveredStart;    // 덮이지 않은 길이
-                int need = (len + L - 1) / L;      // 필요한 널빤지 개수
-                lastPos = uncoveredStart + L * need - 1;  // 마지막 널빤지의 끝 위치
+            } else if (lastPos < temp[1] - 1) {  //전 웅덩이에 설치한 널빤지를 사용할 수 있음
+                int len = temp[1] - (lastPos + 1);
+//                int need = (int) Math.ceil(len / L);    //필요한 널빤지 개수
+                int need = (len + L - 1) / L;
+                lastPos = lastPos + L * need;
                 cnt += need;
             }
-            // lastPos >= end - 1 인 경우, 이미 완전히 덮여있으므로 추가 널빤지 불필요
+
         }
-        
         System.out.println(cnt);
+
     }
+
 }
