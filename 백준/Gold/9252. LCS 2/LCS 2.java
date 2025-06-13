0e9782f393aaa8212a0c12f72958eb07a08a70ba
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-13 16:15:51 +0900

    [Gold IV] Title: LCS 2, Time: 96 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,55 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1 = br.readLine();
-        String str2 = br.readLine();
-        long[][] dp = new long[str1.length() + 1][str2.length() + 1]; //6x6
-        char[] A = str1.toCharArray();
-        char[] B = str2.toCharArray();
+        char[] A = br.readLine().toCharArray();
+        char[] B = br.readLine().toCharArray();
+        int N = A.length, M = B.length;
+        int[][] dp = new int[N + 1][M + 1];
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
                 if (A[i - 1] == B[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else
-                {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                } else if (A[i - 1] != B[j - 1]) {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
-
-        long ansLength = dp[str1.length()][str2.length()];
-        Stack<Character> stack = new Stack<>();
-        int i = str1.length();
-        int j = str2.length(); //5
-
-        while (i >= 1 && j >= 1)
-        {
+        int i = N, j = M;
+        while (i > 0 && j > 0) {
             if (A[i - 1] == B[j - 1]) {
-                stack.push(A[i - 1]);
-                i = i - 1;
-                j = j - 1;
+                sb.append(A[i - 1]);
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
             } else {
-                if (dp[i - 1][j] > dp[i][j - 1]) {
-                    i = i - 1;
-                } else {
-                    j = j - 1;
-                }
+                j--;
             }
+        }
+        sb.reverse();
 
+        System.out.println(dp[N][M]);
+        if (dp[N][M] != 0) {
+            System.out.println(sb);
         }
-        if (ansLength > 0) {
-            System.out.println(ansLength);
-            while (!stack.isEmpty()) {
-                System.out.print(stack.pop());
-            }
-        }
-        else
-            System.out.println(ansLength);
+
 
     }
 }
