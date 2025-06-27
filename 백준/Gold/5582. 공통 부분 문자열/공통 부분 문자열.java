20e989175db1d858d216ee7c273690b14a2a2c44
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-27 12:53:26 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 176 ms, Memory: 74348 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -16,11 +16,9 @@ public class Main {
             for (int j = 1; j <= B.length; j++) {
                 if (A[i - 1] == B[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
                 }
-//                else {
-//                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-//                }
-                maxLen = Math.max(maxLen, dp[i][j]);
+
             }
         }
         System.out.println(maxLen);
