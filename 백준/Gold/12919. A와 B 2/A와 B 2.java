f09234c6614efeea2c9b59ed3d4a49ec10d4fd11
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-28 11:19:22 +0900

    [Gold V] Title: A와 B 2, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -10,28 +10,27 @@ public class Main {
         S = br.readLine();
         T = br.readLine();
 
-        dfs(T);
+        solve(T);
         System.out.println(possible ? 1 : 0);
     }
 
-    public static void dfs(String curr) {
-        if (curr.length() == S.length()) {
-            if (curr.equals(S)) possible = true;
+    public static void solve(String str) {
+        if (str.length() == S.length()) {
+            if (str.equals(S)) {
+                possible = true;
+            }
             return;
         }
 
-        if (possible) return; // 이미 찾았으면 더 안 봐도 됨
-
-        // case 1: 마지막이 A면 A를 제거
-        if (curr.charAt(curr.length() - 1) == 'A') {
-            dfs(curr.substring(0, curr.length() - 1));
+        if (str.charAt(str.length() - 1) == 'A') {  //맨 뒤가 A라면
+            solve(str.substring(0, str.length() - 1));
+        }
+        if (str.charAt(0) == 'B') { //맨 앞이 B라면
+            StringBuilder temp = new StringBuilder(str).reverse();
+            solve((temp.substring(0, temp.length() - 1)));
         }
 
-        // case 2: 첫 번째가 B면 뒤집고 A 제거한 경우
-        if (curr.charAt(0) == 'B') {
-            StringBuilder sb = new StringBuilder(curr).reverse();
-            sb.deleteCharAt(sb.length() - 1);
-            dfs(sb.toString());
-        }
+
     }
+
 }
