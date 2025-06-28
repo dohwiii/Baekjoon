d1cba77f7916ca49c6b2a33dc7a3884b0b98caff
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-28 22:21:58 +0900

    [Silver V] Title: 단어 나누기, Time: 72 ms, Memory: 12400 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -7,9 +7,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder input = new StringBuilder(br.readLine());
-        List<String> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-
+        StringBuilder r = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            r.append("z");
+        }
+        String result = r.toString();
         for (int s = 1; s < input.length() - 2; s++) {
             for (int e = input.length() - 1; e > s; e--) {
                 String front = input.substring(0, s);
@@ -20,13 +23,15 @@ public class Main {
                         .append(new StringBuilder(middle).reverse())
                         .append(new StringBuilder(back).reverse());
 
-                list.add(sb.toString());
+                String newStr = sb.toString();
+                if (newStr.compareTo(result) < 0) {
+                    result = newStr;
+                }
                 sb.setLength(0);
             }
         }
 
-        Collections.sort(list);
-        System.out.println(list.get(0));
+        System.out.println(result);
 
     }
 
