d078b823e27f868ee2a2d6a780bdc6e0fd94519f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-29 20:00:09 +0900

    [Gold V] Title: 0 만들기, Time: 84 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,9 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    static List<String> result = new ArrayList<>();
     static int N;
-    static List<String> ans;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,59 +14,30 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            ans = new ArrayList<>();
-
-            solve(2, new StringBuilder("1"));
-            Collections.sort(ans);
-            for (String a : ans) {
-                sb.append(a + "\n");
-            }
+            result.clear();
+            dfs(2, 1, 1, "1");
+            Collections.sort(result);
+            for (String s : result) sb.append(s).append("\n");
             sb.append("\n");
         }
         System.out.println(sb);
     }
 
-    public static void solve(int depth, StringBuilder result) {
-        if (depth == N + 1) {
-            String r = result.toString().replace("0", "");;
-            List<Character> oper = new ArrayList<>();
-            for (char c : r.toCharArray()) {
-                if (c == '+' || c == '-') {
-                    oper.add(c);
-                }
-            }
-            String[] arr = r.split("[+|-]");
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < arr.length; i++) {
-                list.add(Integer.parseInt(arr[i]));
-            }
-            int index = 0;
-            int sum = list.get(0);
-            for (int i = 1; i < list.size(); i++) {
-                int b = list.get(i);
-                Character operator = oper.get(index);
-                if (operator == '+') {
-                    sum += b;
-                }
-                else {
-                    sum -= b;
-                }
-                index++;
-            }
-            if (sum == 0) {
-                String re = result.toString();
-                re = re.replace("0", " ");
-                ans.add(re);
-            }
-
+    static void dfs(int num, int sum, int prev, String expr) {
+        if (num > N) {
+            if (sum == 0) result.add(expr);
             return;
         }
-        //더하기
-        solve(depth + 1, new StringBuilder(result).append("+").append(depth));
-        //빼기
-        solve(depth + 1, new StringBuilder(result).append("-").append(depth));
-        //공백
-        solve(depth + 1, new StringBuilder(result).append("0").append(depth));
-    }
 
+        // 1. '+' 연산
+        dfs(num + 1, sum + num, num, expr + "+" + num);
+
+        // 2. '-' 연산
+        dfs(num + 1, sum - num, -num, expr + "-" + num);
+
+        // 3. 공백: 숫자 이어붙이기
+        int joined = prev >= 0 ? prev * 10 + num : prev * 10 - num;
+        int newSum = sum - prev + joined;
+        dfs(num + 1, newSum, joined, expr + " " + num);
+    }
 }
