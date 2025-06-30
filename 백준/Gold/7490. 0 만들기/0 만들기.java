6bc0fd5d0bf1bf62329cc5e964d10cd531d9f9e8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2025-06-30 11:12:44 +0900

    [Gold V] Title: 0 만들기, Time: 76 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-    static List<String> result = new ArrayList<>();
+    static List<String> result;
     static int N;
 
     public static void main(String[] args) throws Exception {
@@ -14,7 +14,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            result.clear();
+            result = new ArrayList<>();
             dfs(2, 1, 1, "1");
             Collections.sort(result);
             for (String s : result) sb.append(s).append("\n");
@@ -23,19 +23,20 @@ public class Main {
         System.out.println(sb);
     }
 
-    static void dfs(int num, int sum, int prev, String expr) {
+    private static void dfs(int num, int sum, int prev, String expr) {
         if (num > N) {
-            if (sum == 0) result.add(expr);
+            if (sum == 0) {
+                result.add(expr);
+            }
             return;
         }
-
-        // 1. '+' 연산
+        //'+' 연산
         dfs(num + 1, sum + num, num, expr + "+" + num);
 
-        // 2. '-' 연산
+        //'-' 연산
         dfs(num + 1, sum - num, -num, expr + "-" + num);
 
-        // 3. 공백: 숫자 이어붙이기
+        //' ' 연산
         int joined = prev >= 0 ? prev * 10 + num : prev * 10 - num;
         int newSum = sum - prev + joined;
         dfs(num + 1, newSum, joined, expr + " " + num);
