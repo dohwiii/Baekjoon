f181fbb8a4efb163779a7b8647458ad1186204d8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-06 22:34:52 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,77 +1,80 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[][] map;
     static boolean[][] visited;
+    static char[][] arr;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static int result;
 
-    static int min = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); //행
-        M = Integer.parseInt(st.nextToken()); //열
-
-        map = new int[N][M];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new char[N][M];
         visited = new boolean[N][M];
+        result = 10_000;
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
+            arr[i] = str.toCharArray();
         }
+//        dfs(0, 0, 1);
         bfs(0, 0);
-        System.out.println(min);
+        System.out.println(result);
 
     }
 
+    public static void dfs(int x, int y, int depth) {
+        if (x == N - 1 && y == M - 1) {
+            result = Math.min(result, depth);
+            return;
+        }
+        if (x < 0 || x >= N || y < 0 || y >= M) {
+            return;
+        }
+        if (visited[x][y] || arr[x][y] == '0') {
+            return;
+        }
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || arr[nx][ny] == '0') {
+                continue;
+            }
+            dfs(nx, ny, depth + 1);
+        }
+        visited[x][y] = false;
+    }
+
     public static void bfs(int x, int y) {
-        Queue<Pos> queue = new ArrayDeque<>();
-        queue.add(new Pos(x, y, 1));
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{x, y, 1});
         visited[x][y] = true;
 
         while (!queue.isEmpty()) {
-            Pos now = queue.poll();
-
-            if (now.x == N - 1 && now.y == M - 1) { //현재 위치가 도착지라면
-                min = Math.min(min, now.cnt);
+            int[] now = queue.poll();
+            if (now[0] == N - 1 && now[1] == M - 1) {
+                result = now[2];
                 return;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (!visited[nx][ny] && map[nx][ny] == 1) {
-                        queue.offer(new Pos(nx, ny, now.cnt + 1));
-                        visited[nx][ny] = true;
-                    }
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || arr[nx][ny] == '0') {
+                    continue;
                 }
+                queue.offer(new int[]{nx, ny, now[2] + 1});
+                visited[nx][ny] = true;
             }
 
         }
-    }
-}
-
-class Pos {
-    int x, y, cnt;
-
-    public Pos(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
     }
 }
