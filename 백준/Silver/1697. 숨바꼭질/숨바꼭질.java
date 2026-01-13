c51c78afa6e2d2b1289cfd5d0eef84a46d88387c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-13 13:58:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,73 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
-    static boolean[] visited;
-    static int N, K;
-    static int[] count;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        visited = new boolean[100001];
-        count = new int[100001];
-
-
-        if (N == K) {
-            System.out.println("0");
-
-        } else
-            DFS(N);
-
-
-    }
-
-    public static void DFS(int node) {
-        if (visited[node]) {
-            return;
-        }
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(node);
-        visited[node] = true;
-
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(N);
+        int cnt = 0;
+        boolean[] visited = new boolean[100_001];
+        visited[N] = true;
 
         while (!queue.isEmpty()) {
-            int now = queue.poll();
+            int size = queue.size();
+            cnt++;
 
-            for (int i = 0; i < 3; i++) {
-                int next = 0;
+            while (size-- > 0) {
+                int now = queue.poll();
 
-                if (i == 0) {
-                    next = now + 1;
-
-                } else if (i == 1) {
-                    next = now - 1;
-                } else {
-                    next = now * 2;
-                }
-                if (next == K) {
-                    System.out.println(count[now] + 1);
+                if (now == K) {
+                    System.out.println(cnt - 1);
                     return;
                 }
-                if (next >= 0 && next < count.length && !visited[next]) {
-                    queue.add(next);
-                    visited[next] = true;
-                    count[next] = count[now] + 1;
+                int r1 = now - 1;
+                if (r1 >= 0 && r1 <= 100_000 && !visited[r1]) {
+                    visited[r1] = true;
+                    queue.offer(r1);
                 }
-
+                int r2 = now + 1;
+                if (r2 >= 0 && r2 <= 100_000 && !visited[r2]) {
+                    visited[r2] = true;
+                    queue.offer(r2);
+                }
+                int r3 = 2 * now;
+                if (r3 >= 0 && r3 <= 100_000 && !visited[r3]) {
+                    visited[r3] = true;
+                    queue.offer(r3);
+                }
             }
-
-
         }
-
 
     }
 
