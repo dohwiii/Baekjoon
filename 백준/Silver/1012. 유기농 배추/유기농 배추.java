f0fc923ec1bbb3a3d469b21e01767847111ead39
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-13 09:18:11 +0900

    [Silver II] Title: 유기농 배추, Time: 104 ms, Memory: 13548 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,97 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.sql.Array;
+import java.util.*;
 
-public class Main
-{
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static boolean[][] visited;
-    static int N, M, K;
-    static int[][] map;
-    static int result;
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        while (T-- > 0)
-        {
+        int T = Integer.parseInt(br.readLine());    // 테스트 케이스
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken()); //가로
-            N = Integer.parseInt(st.nextToken()); //세로
-            K = Integer.parseInt(st.nextToken()); //배추 위치의 개수
+            M = Integer.parseInt(st.nextToken());   // 가로길이
+            N = Integer.parseInt(st.nextToken());   // 세로길이
+            int K = Integer.parseInt(st.nextToken());   // 배추가 심어져 있는 위치 개수
+            map = new int[N][M];
+            visited = new boolean[N][M];
 
-            map = new int[M][N];
-            visited = new boolean[M][N];
-
-            for (int i = 0; i < K; i++)
-            {
+            while (K-- > 0) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                map[x][y] = 1;
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                map[Y][X] = 1;  // 배추
             }
-            int ans = 0;
-            for (int i = 0; i < M; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    if (!visited[i][j] && map[i][j] == 1)
-                    {
+            int bugs = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
                         bfs(i, j);
-                        ans++;
+                        bugs++;
                     }
                 }
             }
-            System.out.println(ans);
+            sb.append(bugs + "\n");
+
         }
+        System.out.println(sb);
 
 
     }
 
-    public static void bfs(int x, int y)
-    {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.add(new Pos(x, y, 1));
+    public static void bfs(int x, int y) {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(new Pos(x, y));
         visited[x][y] = true;
 
-        while (!queue.isEmpty())
-        {
+        while (!queue.isEmpty()) {
             Pos now = queue.poll();
 
-            for (int i = 0; i < 4; i++)
-            {
+            for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N)
-                {
-                    //아직 방문하지 않았고, 1의 칸이라면
-                    if (!visited[nx][ny] && map[nx][ny] == 1)
-                    {
-                        queue.add(new Pos(nx, ny, now.cnt + 1));
-                        visited[nx][ny] = true;
-                    }
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] || map[nx][ny] == 0) {
+                    continue;
                 }
+                queue.offer(new Pos(nx, ny));
+                visited[nx][ny] = true;
             }
         }
-
-
     }
+
 }
 
-class Pos
-{
-    int x, y, cnt;
+class Pos {
+    int x, y;
 
-    public Pos(int x, int y, int cnt)
-    {
+    public Pos(int x, int y) {
         this.x = x;
         this.y = y;
-        this.cnt = cnt;
     }
 }
