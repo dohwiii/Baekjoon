2fc6891c4510ec6b1b8f32063de32fd442f5f74a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-14 16:52:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 144708 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,63 +1,53 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] list;
     static boolean[] visited;
+    static List<Integer>[] list;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken()); //노드 수
-        int M = Integer.parseInt(st.nextToken()); //엣지 수
-
-        list = new ArrayList[N + 1];
-        visited = new boolean[N + 1]; //방문했으면 T, 방문 안했으면 F
-        int count = 0;
-
-        for (int i = 1; i < N + 1; i++) {
-
-            list[i] = new ArrayList<Integer>();
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        list = new List[N + 1];
+        for (int i = 0; i <= N; i++) {
+            list[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-
-            list[s].add(e);
-            list[e].add(s);
-
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list[u].add(v);
+            list[v].add(u);
         }
-        for (int i = 1; i <= N; i++)
-        {
-            if (!visited[i])
-            {
-                DFS(i);
-                count++;
+        visited = new boolean[N + 1];
+        int edge = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                edge++;
             }
         }
-        System.out.println(count);
+        System.out.println(edge);
 
 
     }
 
-    public static void DFS(int v)
-    {
-        if (visited[v])
-        {
+    private static void dfs(int node) {
+        if (visited[node]) {
             return;
         }
-        visited[v] = true;
+        visited[node] = true;
 
-        for (int i : list[v])
-        {
-            if (!visited[i]) {
-                DFS(i);
+        for (int next : list[node]) {
+            if (!visited[next]) {
+                dfs(next);
             }
-
         }
     }
+
 }
