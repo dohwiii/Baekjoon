2ac84da98c61b5561b7257b172499acfbf20b41a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-22 12:32:37 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 452 ms, Memory: 84888 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,49 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static ArrayList<Integer>[] list;
     static int[] parent;
+    static List<Integer>[] list;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        list = new ArrayList[N + 1];
+        int N = Integer.parseInt(br.readLine());
         parent = new int[N + 1];
         visited = new boolean[N + 1];
-
-        for (int i = 0; i <= N; i++) {
+        list = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list[x].add(y);
-            list[y].add(x);
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            list[n1].add(n2);
+            list[n2].add(n1);
         }
-        DFS(1);
+        StringBuilder sb = new StringBuilder();
+        dfs(1);
         for (int i = 2; i <= N; i++) {
-            System.out.println(parent[i]);
+            sb.append(parent[i] + "\n");
         }
 
+        System.out.println(sb);
     }
-    public static void DFS(int node) {
+
+    public static void dfs(int node) {
         if (visited[node]) {
             return;
         }
         visited[node] = true;
 
-        for (int i : list[node]) {
-            if (!visited[i]) {
-                parent[i] = node;
-                DFS(i);
+        for (int next : list[node]) {
+            if (visited[next]) {
+                continue;
             }
+            parent[next] = node;    // 현재 노드가 부모
+            dfs(next);
         }
     }
-
 }
