138e603e34f89cc18f1e146d1d78bb330f71ad02
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-22 18:39:00 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 444 ms, Memory: 92860 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -3,88 +3,85 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[][] map;
-    static boolean[][][] visited;
+    static char[][] map;
+    static int[][][] dist;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int result = -1;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M][2];
+        map = new char[N][M];
+        dist = new int[N][M][2];
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
+            map[i] = str.toCharArray();
+        }
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                Arrays.fill(dist[i][j], -1);
             }
         }
-        System.out.println(solve());
+
+        bfs();
+        if (dist[N - 1][M - 1][0] != -1 && dist[N - 1][M - 1][1] != -1) {
+            System.out.println(Math.min(dist[N - 1][M - 1][0], dist[N - 1][M - 1][1]));
+        } else if (dist[N - 1][M - 1][0] != -1) {
+            System.out.println(dist[N - 1][M - 1][0]);
+        } else {
+            System.out.println(dist[N - 1][M - 1][1]);
+
+        }
+
     }
 
-    public static int solve() {
-        Queue<Pos> queue = new ArrayDeque<>();
-        queue.offer(new Pos(0, 0, 0, false));
-        visited[0][0][0] = true;
+    private static void bfs() {
+        Queue<Root> queue = new ArrayDeque<>();
+        queue.offer(new Root(0, 0, 0));
+        dist[0][0][0] = 1; // 시작 지점
 
         while (!queue.isEmpty()) {
-            Pos now = queue.poll();
+            Root now = queue.poll();
             if (now.x == N - 1 && now.y == M - 1) {
-                return now.cnt + 1;
+                return;
             }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = now.x + dx[dir];
+                int ny = now.y + dy[dir];
 
                 if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
                     continue;
                 }
-                if (now.isWallUsed && map[nx][ny] == 1) {   //벽을 이미 사용했는데 벽을 만난 경우
-                    continue;
+
+                if (map[nx][ny] == '1') { // 벽
+                    if (now.wall < 1 && dist[nx][ny][now.wall + 1] == -1) {
+                        queue.offer(new Root(nx, ny, now.wall +1));   // 부수기
+                        dist[nx][ny][now.wall + 1] = dist[now.x][now.y][now.wall] + 1;
+                    }
+                } else {
+                    // 이동가능
+                    if (dist[nx][ny][now.wall] == -1) {
+                        queue.offer(new Root(nx, ny, now.wall));
+                        dist[nx][ny][now.wall] = dist[now.x][now.y][now.wall] + 1;
+                    }
                 }
 
-                //벽이라면 1
-                if (map[nx][ny] == 1) {
-                    if (!now.isWallUsed && !visited[nx][ny][1]) {  //벽을 아직 사용안함
-                        queue.offer(new Pos(nx, ny, now.cnt + 1, true));
-                        visited[nx][ny][1] = true;
-                    }
-                } else { //빈 공간 0
-                    if (now.isWallUsed) {   //이미 벽 사용함
-                        if (!visited[nx][ny][1]) {
-                            queue.offer(new Pos(nx, ny, now.cnt + 1, now.isWallUsed));
-                            visited[nx][ny][1] = true;
-                        }
-                    } else {    //아직 벽 사용 안함
-                        if (!visited[nx][ny][0]) {
-                            queue.offer(new Pos(nx, ny, now.cnt + 1, now.isWallUsed));
-                            visited[nx][ny][0] = true;
-                        }
-                    }
-
-                }
             }
         }
-        return -1;
     }
 
-}
+    static class Root {
+        int x, y, wall;
 
-class Pos {
-    int x, y, cnt;
-    boolean isWallUsed;
-
-    public Pos(int x, int y, int cnt, boolean isWallUsed) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
-        this.isWallUsed = isWallUsed;
+        public Root(int x, int y, int wall) {
+            this.x = x;
+            this.y = y;
+            this.wall = wall;
+        }
     }
+
 }
