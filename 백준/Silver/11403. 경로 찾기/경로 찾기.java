1d5e3fbcaa101e2bc758dbe0ad8cdb698cb0ac95
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-23 14:24:10 +0900

    [Silver I] Title: 경로 찾기, Time: 128 ms, Memory: 14968 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -5,59 +5,39 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static List<Integer>[] list;
-    static boolean[] visited;
-    static int[][] dist;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-
-        dist = new int[N][N];
-        list = new List[N + 1];
-        for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
+        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int v = Integer.parseInt(st.nextToken());
-                if (v == 1) {   // 간선이 있음
-                    list[i + 1].add(j + 1);
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {   // 경유지
+            for (int i = 0; i < N; i++) {   // 출발
+                for (int j = 0; j < N; j++) {   // 도착
+                    if (map[i][k] == 1 && map[k][j] == 1) {
+                        map[i][j] = 1;
+                    }
                 }
             }
         }
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            bfs(i);
-        }
+
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                sb.append(dist[i][j] + " ");
+                sb.append(map[i][j] + " ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static void bfs(int node) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(node);
-
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-
-            for (int next : list[now]) {
-                if (!visited[next]) {
-                    dist[node - 1][next - 1] = 1;
-                    visited[next] = true;
-                    queue.offer(next);
-                }
-            }
-
-        }
     }
 
 }
