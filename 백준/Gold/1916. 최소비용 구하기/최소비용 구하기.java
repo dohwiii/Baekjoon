1172d000575f27be1e926f653849fbcd80c135ca
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-23 15:09:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 576 ms, Memory: 54904 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -4,76 +4,70 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static int[][] map;
+    static List<Node>[] list;
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        ArrayList<Node>[] list = new ArrayList[N + 1];
-        int[] shortArray = new int[N + 1];
-        boolean[] visited = new boolean[N + 1];
-
-        for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());    // 도시의 개수
+        M = Integer.parseInt(br.readLine());    // 버스의 개수
+        map = new int[N][N];
+        list = new List[N + 1];
         for (int i = 1; i <= N; i++) {
-            shortArray[i] = Integer.MAX_VALUE;
+            list[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            list[x].add(new Node(y, w));
-
+            int s = Integer.parseInt(st.nextToken());   // 출발
+            int e = Integer.parseInt(st.nextToken());   // 도착
+            int v = Integer.parseInt(st.nextToken());   // 비용
+            list[s].add(new Node(e, v));
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        shortArray[start] = 0;
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(start, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        long[] dist = new long[N + 1];
+        Arrays.fill(dist, 100_000_000_001L);
+        dist[start] = 0;
 
-        while (!queue.isEmpty()) {
-            Node now = queue.poll();
-            int now_node = now.node;
-            int now_value = now.value;
-            if(visited[now_node])
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (dist[now.node] < now.value) {
                 continue;
-            visited[now_node] = true;
+            }
 
-            for (Node next : list[now_node]) {
-                int node = next.node;
-                int weight = next.value;
-                if (shortArray[node] > shortArray[now_node] + weight) {
-                    shortArray[node] = shortArray[now_node] + weight;
-                    queue.add(new Node(node, shortArray[node]));
+            for (Node next : list[now.node]) {
+                if (dist[next.node] > dist[now.node] + next.value) {
+                    dist[next.node] = dist[now.node] + next.value;
+                    pq.offer(new Node(next.node, dist[next.node]));
                 }
             }
         }
-        System.out.println(shortArray[end]);
+        System.out.println(dist[end]);
 
 
     }
 
+    static class Node implements Comparable<Node> {
+        int node;
+        long value;
 
-}
-class Node implements Comparable<Node>
-{
-    int node;
-    int value;
 
-    public Node(int node, int value)
-    {
-        this.node = node;
-        this.value = value;
+        public Node(int node, long value) {
+            this.node = node;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(o.value, this.value);
+        }
+
     }
 
-    @Override
-    public int compareTo(Node o) {
-        return this.value - o.value;
-    }
 }
