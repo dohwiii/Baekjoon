26e599ba96cb665ee49eaac2dd6c1ef38538b275
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-23 13:35:54 +0900

    [Silver I] Title: 경로 찾기, Time: 148 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,39 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[][] map;
+    static int N;
+    static List<Integer>[] list;
+    static boolean[] visited;
+    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
+        dist = new int[N][N];
+        list = new List[N + 1];
+        for (int i = 0; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    if (map[j][i] == 1 && map[i][k] == 1) {
-                        map[j][k] = 1;
-                    }
-
+                int v = Integer.parseInt(st.nextToken());
+                if (v == 1) {   // 간선이 있음
+                    list[i + 1].add(j + 1);
                 }
             }
         }
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
+            bfs(i);
+        }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(map[i][j] + " ");
+                sb.append(dist[i][j] + " ");
             }
-            System.out.println();
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int next : list[now]) {
+                if (!visited[next]) {
+                    dist[node - 1][next - 1] = 1;
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+
         }
     }
+
 }
