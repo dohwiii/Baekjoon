5c388261e8fd13a3c7e031b24013ec236844f08c
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-24 19:21:51 +0900

    [Silver I] Title: 영역 구하기, Time: 76 ms, Memory: 12696 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,98 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, K;
-    static int[][] map;
+    static int M, N;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    public static void main(String[] args) throws IOException {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int areaSize;
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        M = Integer.parseInt(st.nextToken()); //5
-        N = Integer.parseInt(st.nextToken()); //7
-        K = Integer.parseInt(st.nextToken());
-        map = new int[M + 1][N + 1]; //5x7
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         visited = new boolean[M][N];
-        int count = 0;
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int leftX = Integer.parseInt(st.nextToken());
-            int leftY = Integer.parseInt(st.nextToken());
-            int rightX = Integer.parseInt(st.nextToken());
-            int rightY = Integer.parseInt(st.nextToken());
+            int lx = Integer.parseInt(st.nextToken());
+            int ly = Integer.parseInt(st.nextToken());
+            int rx = Integer.parseInt(st.nextToken());
+            int ry = Integer.parseInt(st.nextToken());
 
-            for (int j = leftY; j < rightY; j++) {
-                for (int k = leftX; k < rightX; k++) {
-                    map[j][k] = 1;
+            int ltx = Math.abs(M - 1 - ly);
+            int lty = lx;
+            int rtx = Math.abs(M - ry);
+            int rty = Math.abs(rx - 1);
+
+            for (int x = rtx; x <= ltx; x++) {
+                for (int y = lty; y <= rty; y++) {
+                    visited[x][y] = true;
                 }
             }
         }
-        ArrayList<Integer> list = new ArrayList<>();
+        int area = 0;
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && map[i][j] != 1) {
-                    count++;
-                    list.add(BFS(i, j));
-
+                if (!visited[i][j]) {
+                    area++;
+                    areaSize = 0;
+                    dfs(i, j);
+                    list.add(areaSize);
                 }
             }
         }
-        System.out.println(count);
         Collections.sort(list);
-        for (int i : list) {
-            System.out.print(i + " ");
+        sb.append(area + "\n");
+        for (int n : list) {
+            sb.append(n + " ");
         }
-
+        System.out.println(sb);
     }
 
-    public static int BFS(int x, int y)
-    {
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(x, y));
+    private static void dfs(int x, int y) {
+        if (visited[x][y]) {
+            return;
+        }
+        areaSize++;
         visited[x][y] = true;
-        int count = 1;
 
-        while (!queue.isEmpty())
-        {
-            Coordinate now = queue.poll();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
 
-            for (int i = 0; i < 4; i++) {
-                int x1 = now.x + dx[i];
-                int y1 = now.y + dy[i];
-
-                if (x1 >= 0 && y1 >= 0 && x1 < M && y1 < N) {
-                    if (!visited[x1][y1] && map[x1][y1] != 1) {
-
-                        visited[x1][y1] = true;
-                        queue.add(new Coordinate(x1, y1));
-                        count++;
-                    }
-                }
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
+                continue;
             }
-
-
+            if (visited[nx][ny]) {
+                continue;
+            }
+            dfs(nx, ny);
         }
-        return count;
-
-
-    }
-
-}
-class Coordinate
-{
-    int x;
-    int y;
-
-    public Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
-
     }
 }
