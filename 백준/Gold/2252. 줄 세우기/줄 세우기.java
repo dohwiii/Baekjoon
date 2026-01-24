d39cfa8d821bc7753ef81b9131d5d268722f31a2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-24 17:50:27 +0900

    [Gold III] Title: 줄 세우기, Time: 372 ms, Memory: 44888 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,61 +1,53 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static ArrayList<Integer>[] list;
-    static int[] degree;
+    static List<Integer>[] list;
+    static StringBuilder sb = new StringBuilder();
+    static Queue<Integer> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); //사람 수
-        M = Integer.parseInt(st.nextToken()); //키 비교 횟수
+        int N = Integer.parseInt(st.nextToken()); //N명
+        int M = Integer.parseInt(st.nextToken()); //비교 횟수
         list = new ArrayList[N + 1];
-        degree = new int[N + 1];
-        boolean[] visited = new boolean[N + 1];
+        int[] arr = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            list[x].add(y);
-            degree[y]++;
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            list[A].add(B);
+            arr[B]++;
         }
-        Queue<Integer> queue = new LinkedList<>();
-
-        int count = 0;
-        while (count < N)
-        {
-            for (int i = 1; i <= N; i++)
-            {
-                if (degree[i] == 0 && !visited[i])
-                {
-                    queue.add(i);
-                    visited[i] = true;
-                    count++;
-                    for (int x : list[i])
-                    {
-                        degree[x]--;
-                    }
-
-                }
-
+        for (int i = 1; i <= N; i++) {
+            if (arr[i] == 0) {
+                queue.offer(i);
             }
-
         }
-        Iterator iter = queue.iterator();
+        int cnt = 0; //N명을 비교했다면
+        while (cnt < N) {
+            while (!queue.isEmpty()) {
+                int now = queue.poll();
+                sb.append(now).append(" ");
+                for (Integer i : list[now]) {
+                    arr[i]--;
 
-        while(iter.hasNext())
-            System.out.print(iter.next() + " ");
+                    if (arr[i] == 0) {
+                        queue.offer(i);
+                    }
+                }
+            }
+            cnt++;
+        }
 
-
+        System.out.println(sb);
     }
-
 }
