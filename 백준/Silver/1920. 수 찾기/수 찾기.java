bc0803a18c8cef711cbc0bb429fa357bae6e0041
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-25 18:28:57 +0900

    [Silver IV] Title: 수 찾기, Time: 440 ms, Memory: 59828 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,71 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int[] find;
-    static int N, M;
-    static boolean result;
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine()); //노드 수
-        arr = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        result = false;
+        for (int i = 0; i < N; i++) {
 
-        for (int i = 0; i < N; i++)
-        {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            set.add(num);
         }
-        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
 
-        M = Integer.parseInt(br.readLine());
-        find = new int[M];
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < M; i++)
-        {
-            find[i]= Integer.parseInt(st.nextToken());
-
-        }
-        for (int i = 0; i < M; i++)
-        {
-            binarySearch(0, N - 1, find[i]);
-
-            if (result) {
-                System.out.println("1");
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (set.contains(num)) {
+                sb.append("1");
+            } else {
+                sb.append("0");
             }
-            else
-                System.out.println("0");
-
+            sb.append("\n");
         }
+        System.out.println(sb);
+
 
     }
-
-    public static void binarySearch(int start, int end, int value)
-    {
-        if (start <= end)
-        {
-            int mid = (start + end) / 2;
-
-            if (value > arr[mid]) {
-                start = mid + 1;
-                binarySearch(start, end, value);
-
-            } else if (value == arr[mid]) {
-                result = true;
-            } else if (value < arr[mid]) {
-
-                end = mid - 1;
-                binarySearch(start, end, value);
-            }
-        }
-        else
-            result = false;
-
-    }
-
 }
