426979eb9539ee8f6aaeada0547bcaad9e0d9c0f
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-26 17:24:21 +0900

    [Silver II] Title: 촌수계산, Time: 68 ms, Memory: 11704 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,78 +1,52 @@
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static ArrayList<Integer>[] list;
+    static List<Integer>[] list;
     static boolean[] visited;
-    static int p1, p2;
-    static int count;
-    static boolean isPossible;
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        p1 =Integer.parseInt(st.nextToken());
-        p2 =Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(br.readLine());
-        list = new ArrayList[N + 1];
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
         visited = new boolean[N + 1];
-        count = 0;
-        isPossible = false;
-
-        for (int i = 0; i <= N; i++) {
+        list = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < M; i++) {
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());   // 부모
             int y = Integer.parseInt(st.nextToken());
             list[x].add(y);
             list[y].add(x);
-
         }
-        DFS(p1, 0);
-        if(!isPossible)
-        {
-            System.out.println("-1");
-        }
-        else
-        {
-            System.out.println(count);
-
-        }
-
-
+        System.out.println(bfs(A, B));
 
     }
 
-    public static void DFS(int node, int depth)
-    {
-        if (visited[node]) {
-            return;
-        }
-        if (node == p2) {
-            count = depth;
-            isPossible = true;
-            return;
-        }
+    private static int bfs(int node, int B) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{node, 0});
         visited[node] = true;
-        for (int i : list[node])
-        {
-            if (!visited[i]) {
-                DFS(i, depth + 1);
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            if (now[0] == B) {
+                return now[1];
             }
-
+            for (int next : list[now[0]]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(new int[]{next, now[1] + 1});
+                }
+            }
         }
-
-
+        return -1;
     }
-
 }
