384b5be6c5dffa93bc8a669b78bd3c7834fe3304
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-26 21:52:23 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 524 ms, Memory: 51252 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,83 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dist;
-    static PriorityQueue<Edge> queue;
-    public static void main(String[] args) throws IOException {
+    static int[] parent;
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        dist = new int[V + 1];
-        queue = new PriorityQueue<>();
-
+        parent = new int[V + 1];
         for (int i = 1; i <= V; i++) {
-            dist[i] = i;
+            parent[i] = i;
         }
+        List<Edge> edgeList = new ArrayList<>();
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            queue.add(new Edge(a, b, c));
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            edgeList.add(new Edge(A, B, C));
         }
-        int useEdge = 0;
+        Collections.sort(edgeList);
         int result = 0;
-        while (useEdge < V - 1)
-        {
-            Edge now = queue.poll();
-            if (find(now.start) != find(now.end)) {
-                union(now.start, now.end);
-                result += now.value;
-                useEdge++;
-            }
+        int count = 0;
 
+        for (Edge edge : edgeList) {
+            if (union(edge.from, edge.to)) {
+                result += edge.weight;
+                count++;
+
+                if (count == V - 1) {
+                    break;
+                }
+            }
         }
         System.out.println(result);
+
+
     }
 
-    public static void union(int a, int b)
-    {
-        int a1 = find(a);
-        int b1 = find(b);
-
-        if (a1 != b1) {
-            dist[b1] = a1;
+    private static boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa == pb) {
+            return false;
         }
 
+        if (pa != pb) {
+            parent[pb] = pa;
+        }
+        return true;
     }
-    public static int find(int x)
-    {
-        if (x == dist[x]) {
+
+    private static int find(int x) {
+        if (x == parent[x]) {
             return x;
         }
-        else
-            return dist[x] = find(dist[x]);
-
+        parent[x] = find(parent[x]);
+        return parent[x];
     }
-}
-class Edge implements Comparable<Edge>
-{
-    int start;
-    int end;
-    int value;
 
-    public Edge(int start, int end, int value) {
-        this.start = start;
-        this.end = end;
-        this.value = value;
+
+}
+
+class Edge implements Comparable<Edge> {
+    int from, to, weight;
+
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
     }
 
     @Override
     public int compareTo(Edge o) {
-        return this.value - o.value;
+        return this.weight - o.weight;
     }
+
 }
