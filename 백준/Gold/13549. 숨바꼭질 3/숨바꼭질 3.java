87ad09ca31d97b92d35ef4b4023906b836fbd4b4
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-01-26 16:51:12 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,72 +1,66 @@
-import java.io.*;
-import java.text.ParseException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main
-{
-    static int N, K;
-    static int[] visited;
-
-    public static void main(String[] args) throws IOException, ParseException
-    {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        visited = new int[100001];
-        visited[N] = 1;
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dir = {1, -1};
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        pq.offer(new Pos(N, 0));
+        int[] dp = new int[100_001];
+        Arrays.fill(dp, 100_001);
+        dp[N] = 0;
 
-        bfs();
-        
-        System.out.println(visited[K] - 1);
+        while (!pq.isEmpty()) {
+            Pos now = pq.poll();
+            if (dp[now.pos] < now.cnt) {
+                continue;
+            }
+            if (now.pos == K) {
+                break;
+            }
+
+            for (int i = 0; i < 2; i++) {
+                int nx = now.pos + dir[i];
+                if (nx < 0 || nx > 100_000) {
+                    continue;
+                }
+                if (dp[nx] > dp[now.pos] + 1) {
+                    dp[nx] = dp[now.pos] + 1;
+                    pq.offer(new Pos(nx, dp[nx]));
+                }
+            }
+            int nx = now.pos * 2;
+            if (nx < 0 || nx > 100_000) {
+                continue;
+            }
+            if (dp[nx] > dp[now.pos]) {
+                dp[nx] = dp[now.pos];
+                pq.offer(new Pos(nx, now.cnt));
+            }
+
+        }
+        System.out.println(dp[K]);
+
+
     }
+    static class Pos implements Comparable<Pos> {
+        int pos, cnt;
 
-    public static void bfs()
-    {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.add(new Pos(N, 1));
-        int[] dx = {1, -1};
-
-        while (!queue.isEmpty())
-        {
-            Pos now = queue.poll();
-
-            //이동해도 초 증가안함
-            int nx1 = now.node * 2;
-            if (nx1 >= 0 && nx1 <= 100000)
-            {
-                if (visited[nx1] == 0 || visited[nx1] > now.cnt)
-                {
-                    queue.add(new Pos(nx1, now.cnt));
-                    visited[nx1] = now.cnt;
-                }
-            }
-            //이동하면 1초 증가
-            for (int i = 0; i < 2; i++)
-            {
-                int nx2 = now.node + dx[i];
-                if (nx2 >= 0 && nx2 <= 100000)
-                {
-                    if (visited[nx2] == 0 || visited[nx2] > now.cnt + 1)
-                    {
-                        queue.add(new Pos(nx2, now.cnt + 1));
-                        visited[nx2] = now.cnt + 1;
-                    }
-                }
-            }
+        public Pos(int pos, int cnt) {
+            this.pos = pos;
+            this.cnt = cnt;
         }
 
+        @Override
+        public int compareTo(Pos o) {
+            return this.cnt - o.cnt;
+        }
     }
-}
 
-class Pos
-{
-    int node;
-    int cnt;
-
-    public Pos(int node, int cnt)
-    {
-        this.node = node;
-        this.cnt = cnt;
-    }
 }
