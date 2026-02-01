9189eeec148060125c063578d57664f38684f595
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-02-01 13:15:35 +0900

    [Gold IV] Title: 뱀, Time: 64 ms, Memory: 12148 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -2,125 +2,83 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] board;
-    static int[] dx = {1, -1, 0, 0}; //남, 북, 동, 서
-    static int[] dy = {0, 0, 1, -1};
-    static Dir[] snake;
-    static int N, ans;
+    static int N;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0,};
+    static int[][] map;
+    static Map<Integer, Character> dirMap;
+    static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); //보드 크기
-        int K = Integer.parseInt(br.readLine()); //사과 개수
-        board = new int[N + 1][N + 1];
+        N = Integer.parseInt(br.readLine());    // 보드 크기
+        int K = Integer.parseInt(br.readLine());    // 사과의 개수
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        dirMap = new HashMap<>();
 
-        for (int i = 0; i < K; i++) { //사과 위치
+        for (int i = 0; i < K; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            int y = Integer.parseInt(st.nextToken()) - 1;
-            board[x][y] = 1; //사과가 있는 곳은 1
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[r - 1][c - 1] = 1;  // 사과
         }
-        int L = Integer.parseInt(br.readLine()); //방향 변환 횟수
-        snake = new Dir[L];
-
+        int L = Integer.parseInt(br.readLine());    // 방향 변환 횟수
         for (int i = 0; i < L; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            char y = st.nextToken().charAt(0);
-            snake[i] = new Dir(x, y);
+            int X = Integer.parseInt(st.nextToken());
+            char C = st.nextToken().charAt(0);
+            dirMap.put(X, C);
         }
-        SnakeGame(0, 0);
-        System.out.println(ans);
+        System.out.println(game());
+
     }
-    public static void SnakeGame(int x, int y) {
-        Deque<Pos> deque = new ArrayDeque<>();
-        deque.add(new Pos(x, y, 2));
-        ans = 0;
 
-        while (!deque.isEmpty()) {
-            Pos now = deque.peekFirst();
+    private static int game() {
+        int sec = 0;
+        int x = 0, y = 0;
+        int dir = 0;
+        int[] head = new int[2];
+        int[] tail = new int[2];
+        int preX = 0, preY = 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0});
 
-            int nowDir = now.dir; //2
-            int nx = now.x + dx[nowDir];
-            int ny = now.y + dy[nowDir];
-            ans++;
+        while (true) {
+            x += dx[dir];
+            y += dy[dir];
+            sec++;  // 초 증가
 
-            //벽에 부딪히면 바로 초 return
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-                return;
+            // 벽 부딪힘
+            if (x < 0 || x >= N || y < 0 || y >= N || visited[x][y]) {
+                return sec;
             }
-            for (Pos elem : deque) {
-                if (elem.x == nx && elem.y == ny) {
-                    return;
+            visited[x][y] = true;
+            queue.offer(new int[]{x, y});
+            // 머리
+            head[0] = x;
+            head[1] = y;
+
+            if (map[x][y] == 1) {   // 사과가 있다면
+                map[x][y] = 0;  // 사과 먹음
+            }
+            else {  // 사과가 없다면 -> 꼬리있는 칸 한칸 앞으로
+                visited[tail[0]][tail[1]] = false;
+                queue.poll();
+                int[] arr = queue.peek();
+                tail[0] = arr[0];
+                tail[1] = arr[1];
+            }
+            if (dirMap.containsKey(sec)) {
+                char changeDir = dirMap.get(sec);
+                if (changeDir == 'L') { // 왼쪽
+                    dir = (dir + 3) % 4;
+                } else if (changeDir == 'D') {  // 오른쪽
+                    dir = (dir + 1) % 4;
                 }
             }
-            //벽에 부딪히지 않았을 때
-            if (board[nx][ny] == 1) {  //사과 있다면
-                for (Dir c : snake) {
-                    if (c.time == ans) {
-                        nowDir = changeDir(nowDir, c.dir);
-                    }
-                }
-                board[nx][ny] = 0;
-                deque.addFirst(new Pos(nx, ny, nowDir));
-            }
-            else if (board[nx][ny] == 0) { //사과 없음
-                for (Dir c : snake) {
-                    if (c.time == ans) {
-                        nowDir = changeDir(nowDir, c.dir);
-                    }
-                }
-                deque.addFirst(new Pos(nx, ny, nowDir));
-                deque.pollLast();
-
-            }
         }
     }
-    public static int changeDir(int nowDir, char chDir) {
-        int resultDirIndex = 0;
 
-        if (chDir == 'D') //우회전
-        {
-            switch (nowDir) {
-                case 0:
-                    resultDirIndex = 3; break;
-                case 1:
-                    resultDirIndex = 2; break;
-                case 2: //북쪽 우회전하면 오른쪽
-                    resultDirIndex = 0; break;
-                case 3: //남쪽 우회전 서쪽 왼쪽
-                    resultDirIndex = 1; break;
-            }
-        } else if (chDir == 'L')  //좌회전
-        {
-            switch (nowDir) {
-                case 0:
-                    resultDirIndex = 2; break;
-                case 1:
-                    resultDirIndex = 3; break;
-                case 2: //북쪽 우회전하면 오른쪽
-                    resultDirIndex = 1; break;
-                case 3: //남쪽 우회전 서쪽 왼쪽
-                    resultDirIndex = 0; break;
-            }
-        }
-        return resultDirIndex;
-    }
-}
 
-class Pos {
-    int x, y, dir;
-    public Pos(int x, int y, int dir) {
-        this.x = x;
-        this.y = y;
-        this.dir = dir;
-    }
-}
-class Dir {
-    int time;
-    char dir;
-    public Dir(int time, char dir) {
-        this.time = time;
-        this.dir = dir;
-    }
 }
