f67a520f555ac9fe3e9603a83047aeb09a6d0977
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-03-12 16:10:09 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 13016 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,59 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] arr;
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int good = 0;
         Arrays.sort(arr);
-        int ans = 0;
-        for (int i = 0; i < N; i++) {
-            if (solve(arr, N, arr[i], i)) {
-//                System.out.println(arr[i]);
-                ans++;
+        for (int i = N - 1; i >= 0; i--) {
+            int now = arr[i];
+            int s = 0, e = N - 1;
+            while (s < e) {
+                if (s == i) {
+                    s++;
+                }
+                if (e == i) {
+                    e--;
+                }
+                if (s == e) {
+                    break;
+                }
+                int sum = arr[s] + arr[e];
+                if (sum == now) {
+                    good++;
+                    break;
+                } else if (sum > now) {
+                    e--;
+                } else if (sum < now) {
+                    s++;
+                }
             }
-
         }
-        System.out.println(ans);
+        System.out.println(good);
+
+
+
+
     }
-
-    public static boolean solve(int[] arr, int n, int target, int index) {
-        int s = 0, e = n - 1;
-        if (s == index) {
-            s++;
-        } else if (e == index) {
-            e--;
-        }
-
-        while (s < e) {
-            long re = arr[s] + arr[e];
-
-            if (re == target) {
-                return true;
-            } else if (re < target) {
-                s++;
-            } else {
-                e--;
-            }
-            if (s == index) {
-                s++;
-            }
-            else if (e == index) {
-                e--;
-            }
-        }
-        return false;
-    }
-
 }
