a9e0ca64576f64d31221eaa5614335ac9ded5435
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-01 11:14:16 +0900

    [Gold III] Title: 파티, Time: 2436 ms, Memory: 196324 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,89 +1,80 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-    static List<Party>[] list;
-    static int[][] dist;
-    static boolean[] visited;
+    static int N, X;
+    static List<Node>[] list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-
+        list = new List[N + 1];
         for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
-        }
-        dist = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-
-            list[s].add(new Party(e, time));
-//            dist[s][e] = time;
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+            // 단방향
+            list[S].add(new Node(E, T));
         }
+        int max = 0;
         for (int i = 1; i <= N; i++) {
-            depart(i);
-        }
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i][X] + dist[X][i]);
+            if (i == X) {
+                continue;
+            }
+            int go = bfs(i, X);
+            int back = bfs(X, i);
+            max = Math.max(max, go + back);
         }
 
         System.out.println(max);
 
+        // 가장 많은 시간을 소비하는 학생
     }
 
-    //집 -> X
-    public static void depart(int start) {
-        PriorityQueue<Party> pqueue = new PriorityQueue<>();
-        pqueue.add(new Party(start, 0));
-//        visited = new boolean[N + 1];
-//        visited[start] = true;
+    private static int bfs(int node, int dest) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        boolean[] visited = new boolean[N + 1];
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[node] = 0;
+        visited[node] = true;
+        queue.offer(node);
 
-        while (!pqueue.isEmpty()) {
-            Party now = pqueue.poll();
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
 
-            for (Party next : list[now.node]) {
-                if (next.node == start) {
-                    continue;
-                }
-                if (dist[start][next.node] > now.time + next.time) {
-                    dist[start][next.node] = now.time + next.time;
-                    pqueue.add(new Party(next.node, now.time + next.time));
+            for (Node next : list[now]) {
+                if (dp[next.end] > dp[now] + next.time) {
+                    dp[next.end] = dp[now] + next.time;
+                    visited[next.end] = true;
+                    queue.offer(next.end);
                 }
             }
+
+        }
+        return dp[dest];
+    }
+
+    static class Node implements Comparable<Node> {
+        int end, time;
+
+        public Node(int end, int time) {
+            this.end = end;
+            this.time = time;
         }
 
-    }
-}
-
-class Party implements Comparable<Party> {
-    int node, time;
-
-
-    public Party(int node, int time) {
-        this.node = node;
-        this.time = time;
+        @Override
+        public int compareTo(Node o) {
+            return this.time - o.time;
+        }
     }
 
-    @Override
-    public int compareTo(Party o) {
-        return this.time - o.time;
-    }
 }
