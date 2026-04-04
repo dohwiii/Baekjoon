454d6db97408fff831513e34ede3498bee957f93
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-04 14:17:28 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 544 ms, Memory: 70468 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,68 +2,89 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, E, u, v;
-    static int[][] dist;
-    static boolean[] visited;
+    static int N, E;
+    static List<Node>[] graph;
+    static final int INF = 799_000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1];
-        dist = new int[N + 1][N + 1];
-
+        N = Integer.parseInt(st.nextToken());  // 정점 개수
+        E = Integer.parseInt(st.nextToken());  // 간선 개수
+        graph = new List[N + 1];
         for (int i = 1; i <= N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        for (int i = 1; i <= N; i++) {
-            dist[i][i] = 0;
+            graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());   // 거리
+            int c = Integer.parseInt(st.nextToken());
             // 양방향
-            dist[a][b] = Math.min(dist[a][b], c);
-            dist[b][a] = Math.min(dist[b][a], c);
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
         }
         st = new StringTokenizer(br.readLine());
-        u = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-        // 1번 ~ N번 이동할 때 반드시 두 정점을 거쳐야 함
-        // 한번 이동했던 정점은 물론, 한번 이동했던 간선도 다시 이동할 수 있다 -> 하지만 최단 경로
+        int u = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {  // 중간 다리
-                for (int j = 1; j <= N; j++) {
-                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
+        // 다익스트라: 한 정점으로부터 출발해서 그 정점에서부터 모든 정점까지의 최단거리 (1->N)
+        int[][] dist = new int[N + 1][N + 1];
+        dijkstra(1, dist);
+        dijkstra(u, dist);
+        dijkstra(v, dist);
 
+        // 1 -> u -> v -> N
+        // 1 -> v -> u -> N
+        int result = -1;
+        if ((dist[1][u] != INF && dist[u][v] != INF && dist[v][N] != INF) && (dist[1][v] != INF && dist[v][u] != INF && dist[u][N] != INF)) {
+            int route1 = dist[1][u] + dist[u][v] + dist[v][N];
+            int route2 = dist[1][v] + dist[v][u] + dist[u][N];
+            result = Math.min(route1, route2);
+        } else if ((dist[1][u] != INF && dist[u][v] != INF && dist[v][N] != INF)) {
+            result = dist[1][u] + dist[u][v] + dist[v][N];
+        }
+        else if(dist[1][v] != INF && dist[v][u] != INF && dist[u][N] != INF){
+            result = dist[1][v] + dist[v][u] + dist[u][N];
+        }
+        System.out.println(result);
 
+    }
+
+    private static void dijkstra(int node, int[][] dist) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Arrays.fill(dist[node], INF);
+        dist[node][node] = 0;
+        pq.offer(new Node(node, 0));
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (dist[node][now.node] < now.dist) {
+                continue;
+            }
+
+            for (Node next : graph[now.node]) {
+                if (dist[node][next.node] > dist[node][now.node] + next.dist) {
+                    dist[node][next.node] = dist[node][now.node] + next.dist;
+                    pq.offer(new Node(next.node, dist[node][next.node]));
                 }
             }
         }
-        long path1 = Long.MAX_VALUE;  // 불가능이라고 일단 가정
-        long path2 = Long.MAX_VALUE;
-        int MAX = Integer.MAX_VALUE;
-
-        // path1이 가능할 때만 계산
-        if (dist[1][u] != MAX && dist[u][v] != MAX && dist[v][N] != MAX) {
-            path1 = (long)dist[1][u] + dist[u][v] + dist[v][N];
-        }
-
-        // path2가 가능할 때만 계산
-        if (dist[1][v] != MAX && dist[v][u] != MAX && dist[u][N] != MAX) {
-            path2 = (long)dist[1][v] + dist[v][u] + dist[u][N];
-        }
-
-        // 둘 다 불가능하면 -1, 아니면 작은 값
-        long result = Math.min(path1, path2);
-        System.out.println(result == Long.MAX_VALUE ? -1 : result);
-
     }
+
+    static class Node implements Comparable<Node> {
+        int node, dist;
+
+        public Node(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.dist - o.dist;
+        }
+    }
+
 }
