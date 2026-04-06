aa099a9d1eff0a295a58624c4010f5ede229d2e1
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-06 13:28:55 +0900

    [Gold III] Title: 파티, Time: 144 ms, Memory: 17156 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,87 +2,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, X;
-    static List<Node>[] graph;
-    static List<Node>[] reverseGraph;;
+    static int N, M, X;
+    static final int INF = 100_001;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        List<Node>[] graph = new List[N + 1];
-        List<Node>[] reverseGraph = new List[N + 1];
-
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());   // 파티
+        List<Town>[] graph = new List[N + 1];
+        List<Town>[] revGraph = new List[N + 1];
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
-            reverseGraph[i] = new ArrayList<>();
+            revGraph[i] = new ArrayList<>();
         }
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            int T = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
             // 단방향
-            graph[S].add(new Node(E, T));
-            // 역방향
-            reverseGraph[E].add(new Node(S, T));
+            graph[s].add(new Town(e, t));
+            revGraph[e].add(new Town(s, t));
         }
-        int max = 0;
-        int[] dp = new int[N + 1];
-        int[] dp2 = new int[N + 1];
-        bfs(X, reverseGraph, dp);
-        bfs(X, graph, dp2);
+        // 1 -> X -> 1
+        int[] go = dijkstra(X, revGraph);
+        int[] back = dijkstra(X, graph);
+        int maxSum = 0;
+
         for (int i = 1; i <= N; i++) {
+            int sum = 0;
             if (i == X) {
                 continue;
             }
-            int sum = dp[i] + dp2[i];
-            max = Math.max(max, sum);
+            sum += go[i] + back[i];
+            maxSum = Math.max(maxSum, sum);
         }
+        System.out.println(maxSum);
 
-        System.out.println(max);
 
-        // 가장 많은 시간을 소비하는 학생
     }
 
-    private static void bfs(int node, List<Node>[] graph, int[] dp) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        boolean[] visited = new boolean[N + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[node] = 0;
-        visited[node] = true;
-        queue.offer(new Node(node, 0));
+    private static int[] dijkstra(int start, List<Town>[] graph) {
+        PriorityQueue<Town> pq = new PriorityQueue<>();
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        pq.offer(new Town(start, 0));
 
-        while (!queue.isEmpty()) {
-            Node now = queue.poll();
-            if (dp[now.end] < now.time) {
-                continue;
-            }
+        while (!pq.isEmpty()) {
+            Town now = pq.poll();
 
-            for (Node next : graph[now.end]) {
-                if (dp[next.end] > dp[now.end] + next.time) {
-                    dp[next.end] = dp[now.end] + next.time;
-                    visited[next.end] = true;
-                    queue.offer(new Node(next.end, dp[next.end]));
+            for (Town next : graph[now.town]) {
+                if (dist[next.town] > dist[now.town] + next.time) {
+                    dist[next.town] = dist[now.town] + next.time;
+                    pq.offer(new Town(next.town, dist[next.town]));
                 }
             }
-
         }
+        return dist;
+
     }
 
-    static class Node implements Comparable<Node> {
-        int end, time;
+    static class Town implements Comparable<Town> {
+        int town, time;
 
-        public Node(int end, int time) {
-            this.end = end;
+        public Town(int town, int time) {
+            this.town = town;
             this.time = time;
         }
 
         @Override
-        public int compareTo(Node o) {
+        public int compareTo(Town o) {
             return this.time - o.time;
         }
     }
