5f0cce4a7dbcfac33d7824fc56ec6eaf1c3ee9a8
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-07 13:42:06 +0900

    [Gold IV] Title: 타임머신, Time: 236 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,75 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static Edge edges[];
-    static int N, M;
-    static long[] dist;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        edges = new Edge[M + 1];
-        dist = new long[N + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] edges = new int[M][3];
+        long[] dist = new long[N + 1];
+        int INF = 5_000_001;
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(a, b, c);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            edges[i] = new int[]{A, B, C};
         }
-        for (int i = 0; i <= N; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, INF);
         dist[1] = 0;
 
-        for (int i = 1; i < N; i++)
-        {
-            for (int j = 0; j < M; j++) {
-                Edge edge = edges[j];
-                if (dist[edge.start] != Integer.MAX_VALUE && dist[edge.end] > dist[edge.start] + edge.time) {
-                    dist[edge.end] = dist[edge.start] + edge.time;
+        // N-1 반복
+        for (int i = 0; i < N - 1; i++) {
+            for (int[] edge : edges) {
+                int a = edge[0];
+                int b = edge[1];
+                int t = edge[2];    // 시간
+                if (dist[a] != INF && dist[b] > dist[a] + t) {
+                    dist[b] = dist[a] + t;
                 }
             }
         }
-        boolean mCycle = false;
-        for (int i = 0; i < M; i++)
-        {
-            Edge edge = edges[i];
-            if (dist[edge.start] != Integer.MAX_VALUE && dist[edge.end] > dist[edge.start] + edge.time)
-            {
-                mCycle = true;
+        boolean hasNegCycle = false;    // 음수 사이클 유무
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            int t = edge[2];    // 시간
+            if (dist[a] != INF && dist[b] > dist[a] + t) {
+                hasNegCycle = true;
+                dist[b] = dist[a] + t;
+                break;
             }
         }
-        if (!mCycle) {
+        if (hasNegCycle) {
+            System.out.println(-1);
+        } else {
             for (int i = 2; i <= N; i++) {
-                if (dist[i] == Integer.MAX_VALUE) {
-                    System.out.println("-1");
+                if (dist[i] == INF) {
+                    sb.append("-1\n");
+                    continue;
                 }
-                else
-                    System.out.println(dist[i]);
+                sb.append(dist[i] + "\n");
             }
+            System.out.print(sb.toString().trim());
         }
-        else
-            System.out.println("-1");
+
+
     }
 
-}
-class Edge
-{
-    int start, end, time;
-
-    public Edge(int start, int end, int time) {
-
-        this.start = start;
-        this.end = end;
-        this.time = time;
-    }
 }
