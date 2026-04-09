4ffad1d8325227fd592f85671b85594e1e370c23
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-09 10:27:35 +0900

    [Gold III] Title: 줄 세우기, Time: 380 ms, Memory: 49468 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,53 +1,50 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static List<Integer>[] list;
-    static StringBuilder sb = new StringBuilder();
-    static Queue<Integer> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //N명
-        int M = Integer.parseInt(st.nextToken()); //비교 횟수
-        list = new ArrayList[N + 1];
-        int[] arr = new int[N + 1];
-
-        for (int i = 0; i <= N; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] indegree = new int[N + 1];
+        list = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             list[A].add(B);
-            arr[B]++;
+            indegree[B]++;
         }
+        Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
-            if (arr[i] == 0) {
+            if (indegree[i] == 0) { // 앞에 서야 하는 사람이 없는 사람부터 차례로 뽑는다
                 queue.offer(i);
             }
         }
-        int cnt = 0; //N명을 비교했다면
-        while (cnt < N) {
-            while (!queue.isEmpty()) {
-                int now = queue.poll();
-                sb.append(now).append(" ");
-                for (Integer i : list[now]) {
-                    arr[i]--;
 
-                    if (arr[i] == 0) {
-                        queue.offer(i);
-                    }
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            sb.append(now + " ");
+
+            for (int next : list[now]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
                 }
             }
-            cnt++;
         }
-
         System.out.println(sb);
+
+
     }
+
+
 }
