3c8f32bef926f670f4d353d191c45c6657fcae5d
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-04-14 17:31:19 +0900

    [Silver II] Title: 나무 자르기, Time: 464 ms, Memory: 168164 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,47 +1,53 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
     static int N, M;
+    static int[] trees;
+    static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());    // 나무의 개수
-        M = Integer.parseInt(st.nextToken());    // 챙겨갈 나무 높이
-        arr = new int[N];   // 나무의 높이
-        int maxHeight = 0;
+        N = Integer.parseInt(st.nextToken());   // 정점 개수
+        M = Integer.parseInt(st.nextToken());   // 간선 개수
+        trees = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            maxHeight = Math.max(maxHeight, arr[i]);
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
         }
 
-        int l = 1;
-        int r = maxHeight;
-        int ans = 0;
 
-        while (l <= r) {
-            int cutter = (l + r) / 2;  // 절단기 높이
-            long sum = 0;
-
-            for (int i = 0; i < N; i++) {
-                if (arr[i] > cutter) {
-                    sum += (arr[i] - cutter);
-                }
-            }
-            if (sum >= M) {
-                ans = cutter;
-                l = cutter + 1;
-            }
-            else {
-                r = cutter - 1;
-            }
-        }
+        int ans = binarySearch();
         System.out.println(ans);
 
 
-
     }
+
+    private static int binarySearch() {
+        int s = 0;
+        int e = max;
+        int maxCut = 0;
+
+        while (s < e) {
+            int mid = (s + e) / 2;
+            long sum = 0;
+
+            for (int i = 0; i < N; i++) {
+                if (trees[i] > mid) {
+                    sum += (trees[i] - mid);
+                }
+            }
+            if (sum >= M) {
+                s = mid + 1;
+                maxCut = mid;
+            } else {
+                e = mid;
+            }
+        }
+        return maxCut;
+    }
+
 }
