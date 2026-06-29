e620eac46700a073b9ccdfe90ae0cfd6e644fc80
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-06-29 21:41:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.90 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,28 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> map = new HashMap<>();
-
-        for(int i=0; i<participant.length; i++)
-        {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
-        for(int i=0; i<completion.length; i++)
-        {
-            map.put(completion[i], map.get(completion[i]) - 1);
-        }
-        for(String key : map.keySet())
-        {
-            int value = map.get(key);
-            if(value != 0)
-            {
-                answer = key;
+        
+        for(String c : completion) {
+            int cnt = map.get(c);   // 해당 이름의 명수
+            if(cnt > 1) {
+                map.put(c, cnt-1);
+            }
+            else {
+                map.remove(c);
             }
         }
-       
+        for(String s : map.keySet()) {
+            answer = s;
+        }
+        
         return answer;
     }
+    
 }
