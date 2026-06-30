6cf13c66b16406b5b101d802ec3bf542c8dc7a1e
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-06-30 18:56:18 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,14 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] dayOfend = new int[100];
-        int day = 0;
+        int[] answer = {};
+        Queue<Integer> queue = new ArrayDeque<>();
+        
         for(int i=0; i<progresses.length; i++) {
-            while(progresses[i] + (day*speeds[i]) < 100) {
-                day++;
-            }
-            dayOfend[day]++;
+            int remain = 100 - progresses[i];
+            int left = (int) Math.ceil((remain*1.0) / speeds[i]);
+            queue.offer(left);  // 배포까지 남은 완료일
         }
-        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+        List<Integer> list = new ArrayList<>();
+        
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            int cnt = 1;
+            
+            while(!queue.isEmpty()) {
+                if(now >= queue.peek()) {
+                    queue.poll();
+                    cnt++;
+                }
+                else {
+                    break;
+                }
+            }
+            list.add(cnt);  // 같이 배포할 작업 개수
+        }
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 }
