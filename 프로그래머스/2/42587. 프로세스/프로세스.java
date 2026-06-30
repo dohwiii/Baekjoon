b3f60e6fc3753f39dd15107be5e369124c3d4f12
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-06-30 22:50:09 +0900

    [level 2] Title: 프로세스, Time: 2.28 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,44 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static class Num implements Comparable<Num> {
-        int pri, index;
-        public Num(int pri, int index) {
-            this.pri = pri;
-            this.index = index;
-        }
-        @Override
-        public int compareTo(Num n) {
-            return n.pri - this.pri;
+    static class Order {
+        int pri, idx;
+        
+        public Order(int pri, int idx) {
+            this.pri=pri;
+            this.idx=idx;
         }
     }
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        int order = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());    //숫자 큰 순서대로
-        Queue<Num> queue = new ArrayDeque<>();
+        int answer = 0; 
+        Order[] orders = new Order[priorities.length];
         
         for(int i=0; i<priorities.length; i++) {
-            int current = priorities[i];
-            pq.offer(current);
-            queue.offer(new Num(current, i));
+            orders[i] = new Order(priorities[i], i);
         }
+        Queue<Order> queue = new ArrayDeque<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        while(!queue.isEmpty() && !pq.isEmpty()) {
-            Num now = queue.poll();
-            if(pq.peek() == now.pri) {  //제일 높은 우선순위에 해당함 -> 뺄 수 있음
-                int p = pq.poll();
-                order++;
-                if(now.index == location) {
-                    break;
+        for(int i=0; i<priorities.length; i++) {
+            queue.offer(new Order(priorities[i], i));
+            pq.offer(priorities[i]);
+        }
+
+        int order = 0;
+        while(!queue.isEmpty()) {
+            Order now = queue.poll();
+            
+            if(now.pri == pq.peek()) {  // 현재 나가도 되는 우선순위인지 (자기보다 높은 우선순위가 없다면)
+                int x = pq.poll();
+                order++;        
+                
+                if(location == now.idx) {
+                    return order;
                 }
             }
             else {
                 queue.offer(now);
             }
+            
         }
-        
         
         return order;
     }
+    
 }
