f51d99c4ff676716f4a3fda0942443bb8e24cb37
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-01 21:26:59 +0900

    [level 2] Title: 가장 큰 수, Time: 1546.23 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers) {
+    public String solution(int[] primitive) {
         String answer = "";
-        String[] arr = new String[numbers.length];
-
-        for(int i=0; i<numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(arr, new Comparator<String>(){
-            @Override
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2);
+        boolean isZero = true;
+        Integer[] numbers = new Integer[primitive.length];  // 같은 크기의 Integer 배열 생성
+        for (int i = 0; i < primitive.length; i++) {
+            numbers[i] = primitive[i];  // int -> Integer 자동 변환(오토박싱)
+            if(primitive[i] != 0) {
+                isZero = false;
             }
-        });
-        if(arr[0].equals("0")) {
+        }
+        if(isZero) {
             return "0";
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<arr.length; i++) {
-            sb.append(arr[i]);
+        Arrays.sort(numbers, new Comparator<Integer>() {
+           @Override
+            public int compare(Integer x, Integer y) {
+                String a = x + "";
+                String b = y + "";
+                String ab = a + b;
+                String ba = b + a;
+                
+                return (b + a).compareTo(a + b);
+            }
+        });
+        for(int i=0; i<numbers.length; i++) {
+            answer += numbers[i] + "";
         }
-
-        return sb.toString();
+        return answer;
     }
 }
