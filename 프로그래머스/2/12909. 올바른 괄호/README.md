527a0eaaf791925a3fec9d05fe3be2ed71c7ef23
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-02 21:03:08 +0900

    [level 2] Title: 올바른 괄호, Time: 8.65 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 53.7 MB, 시간: 18.24 ms
+메모리: 58.1 MB, 시간: 8.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 19:23:47
+2026년 07월 02일 21:03:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,35 +2,30 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        int N = s.length();
-        Queue<Character> queue = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
         
-        if(s.charAt(0) != '(') {
+        for(char c: arr) {
+            if(c == '(') {
+                stack.push(c);
+            }
+            else {  // ')'
+                if(!stack.isEmpty()) {
+                    char now = stack.pop();
+                    if(now != '(') {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+                
+            }
+        }
+        if(!stack.isEmpty()) {
             return false;
         }
-        for(int i=0; i<N; i++) {
-            char c = s.charAt(i);
-            queue.offer(c);
-        }
-        int countA = 0;
-        int countB = 0;
-        
-        while(!queue.isEmpty()) {
-            char now = queue.poll();
-            if(now == '(') {
-                countA++;
-            }
-            else if(now == ')') {
-                countB++;
-            }
-            if(countA < countB) {   //닫는괄호(')')가 여는괄호('(')보다 많은 경우
-                return false;
-            }
-        
-        }
-        if(countA == countB) {
-            return true;
-        }
-        return false;
+       
+        return true;
     }
 }
