471ac34f02dfa2997a7891b162e627e735ca0d67
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-05 17:06:27 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -8,43 +8,52 @@ class Solution {
             set.add(w);
         }
         
-        
-        return bfs(begin, target, set);
+        return bfs(begin, target, words, set);
     }
-    private static int bfs(String begin, String target, Set<String> set) {
-        Queue<Sentence> queue = new ArrayDeque<>();
+    private static int bfs(String begin, String target, String[] words, Set<String> set) {
+        Queue<Word> queue = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
-        queue.offer(new Sentence(begin, 0));
+        queue.offer(new Word(begin, 0));
         visited.add(begin); // 방문체크
-        StringBuilder sb = new StringBuilder();
         
         while(!queue.isEmpty()) {
-            Sentence now = queue.poll();
+            Word now = queue.poll();
             if(now.str.equals(target)) {
                 return now.depth;
             }
             
-            for(int i=0; i<now.str.length(); i++) {
-                sb = new StringBuilder(now.str);
+            for(String w : words) {
+                if(visited.contains(w)) {
+                    continue;
+                }
                 
-                for(char c='a'; c<='z'; c++) {
-                    sb.setCharAt(i, c);
-                    if(set.contains(sb.toString()) && !visited.contains(sb.toString()) ) {   // words에 포함이 되어있다면
-                        visited.add(sb.toString());
-                        queue.offer(new Sentence(sb.toString(), now.depth + 1));
-                    }
-
+                if(diffByOne(now.str, w)) {
+                    queue.offer(new Word(w, now.depth + 1));
+                    visited.add(w);
                 }
             }
             
         }
         return 0;
     } 
-    static class Sentence {
+    private static boolean diffByOne(String change, String word) {
+        // 하나만 바뀐게 맞는지 검사
+        int cnt = 0;
+        for(int i=0; i<word.length(); i++) {
+            if(word.charAt(i) != change.charAt(i)) {
+                cnt++;
+            }
+            if(cnt > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    static class Word {
         String str;
         int depth;
         
-        public Sentence(String str, int depth) {
+        public Word(String str, int depth) {
             this.str=str;
             this.depth=depth;
         }
