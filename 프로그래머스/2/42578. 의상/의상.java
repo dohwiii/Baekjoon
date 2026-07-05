b07acddee4b26cfa0c808818ba4758e6117ba602
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-05 13:01:49 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for(String[] cloth : clothes) {
             String name = cloth[0]; // 의상 이름
             String type = cloth[1]; // 의상 종류
             
-            List<String> value = map.getOrDefault(type, new ArrayList<>()); 
-            value.add(name);
-            map.put(type, value);
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
         
         int answer = 1;
         for(String key: map.keySet()) {
-            List<String> value = map.get(key);
-            int size = value.size();
+            int size  = map.get(key);
             answer *= (size + 1);
         }
 
