68337dba799145ab390f4f57f26c4f28fb7bc01a
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-12 03:04:01 +0900

    [level 2] Title: 미로 탈출, Time: 5.57 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,106 +1,80 @@
 import java.util.*;
-
 class Solution {
-    static PriorityQueue<Pos> pq;
-    static Pos S, L, E;
-    static char[][] map;
-    static int[][] dist;
     static int N, M;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    
     public int solution(String[] maps) {
+        int answer = 0;
         N = maps.length;
         M = maps[0].length();
-        map = new char[N][M];
-        dist = new int[N][M];
-        
-        for(int i=0; i<maps.length; i++) {
-            String str = maps[i];
-            for(int j=0; j<str.length(); j++) {
-                map[i][j] = str.charAt(j);
-                if(map[i][j] == 'S') {
-                    S = new Pos(i, j, 0);
+        char[][] maze = new char[N][M];
+        boolean[][][] visited = new boolean[N][M][2];
+        Pos start = new Pos(0, 0, 0, false);
+        Pos end = new Pos(0, 0, 0, false);
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                maze[i][j] = maps[i].charAt(j);
+                if(maze[i][j] == 'S') {
+                    start = new Pos(i, j, 0, false);
                 }
-                else if(map[i][j] == 'L') {
-                    L = new Pos(i, j, 0);
-                }
-                else if(map[i][j] == 'E') {
-                    E = new Pos(i, j, 0);
+                else if(maze[i][j] == 'E') {
+                    end = new Pos(i, j, 0, false);
                 }
             }
         }
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visited[start.x][start.y][0] = true;
+        boolean isPossible = false;
         
-        int r1 = 0, r2 = 0;
-        int min = Integer.MAX_VALUE;
-        
-        r1 = solve(S, L);
-        if(r1 == 0) {
-            return -1;
-        }
-        
-        r2 = solve(L, E);
-        if(r2 == 0) {
-            return -1;
-        }
-        for(int i=0; i<N; i++) {
-            System.out.println(Arrays.toString(dist[i]));
-        }
-        return r1+r2;
-
-        
-    }
-    public static int solve(Pos start, Pos end) {
-        for(int i=0; i<N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        pq = new PriorityQueue<>();
-        pq.offer(start);
-        dist[start.x][start.y] = 0;
-        char endChar = map[end.x][end.y];
-        
-        while(!pq.isEmpty()) {
-            Pos now = pq.poll();
-            
-            if(dist[now.x][now.y] < now.sec) {
-                continue;
+        while(!queue.isEmpty()) {
+            Pos now = queue.poll();
+            if(now.x == end.x && now.y == end.y) {
+                if(now.isArrived) {
+                    answer = now.cnt;
+                    isPossible = true;
+                    break;
+                }
             }
             
-            for(int i=0; i<4; i++) {
+            for(int i=0; i<4; i++){
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
+                boolean newArrived = now.isArrived;
+                int arrivedCheck = now.isArrived ? 1 : 0;
                 
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] == 'X') {
+                if(nx<0||nx>=N||ny<0||ny>=M) {
                     continue;
                 }
-                if(map[nx][ny] == endChar) {
-                    if(dist[nx][ny] > dist[now.x][now.y] + 1) {
-                        dist[nx][ny] = dist[now.x][now.y] + 1;
-                    }
+                if(visited[nx][ny][arrivedCheck] || maze[nx][ny] == 'X') {
                     continue;
                 }
-                int nextSec = Math.min(dist[nx][ny], dist[now.x][now.y] + 1);
-                if(dist[nx][ny] > nextSec) {
-                    dist[nx][ny] = nextSec;
-                    pq.offer(new Pos(nx, ny, nextSec));
+                if(maze[nx][ny] == 'L') {   // 레버라면
+                    newArrived = true;
+                    arrivedCheck = 1;
                 }
+                queue.offer(new Pos(nx, ny, now.cnt + 1, newArrived));
+                visited[nx][ny][arrivedCheck] = true;
             }
+            
         }
-        if(dist[end.x][end.y] == Integer.MAX_VALUE) {
-            return 0;
+        // 못도착하면 -1
+        if(isPossible) {
+            return answer;
         }
-        return dist[end.x][end.y];
+        else {
+            return -1;
+        }
     }
-    static class Pos implements Comparable<Pos> {
-        int x, y, sec;
-        public Pos(int x, int y, int sec) {
+    static class Pos {
+        int x, y, cnt;
+        boolean isArrived;
+        
+        public Pos(int x, int y, int cnt, boolean isArrived) {
             this.x=x;
-            this.y=y;;
-            this.sec=sec;
-        }
-        @Override
-        public int compareTo(Pos p) {
-            return this.sec - p.sec;
+            this.y=y;
+            this.cnt=cnt;
+            this.isArrived = isArrived;
         }
     }
 }
