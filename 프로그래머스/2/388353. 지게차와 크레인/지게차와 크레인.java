06e10042bbfd6c687d667147b1d69e6c0d3f87e1
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-07-25 21:16:46 +0900

    [level 2] Title: 지게차와 크레인, Time: 8.38 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,140 +1,91 @@
 import java.util.*;
 
 class Solution {
-    static int N, M;
     static char[][] map;
-    // 상하좌우
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-
-    static class Pos {
-        int x, y;
-        Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static int N, M;
+    static int cnt;
     
     public int solution(String[] storage, String[] requests) {
+        int answer = 0;
         N = storage.length;
         M = storage[0].length();
-        map = new char[N][M];
-        for (int i = 0; i < N; i++) {
-            map[i] = storage[i].toCharArray();
+        map = new char[N+2][M+2];
+        
+        // 초기화
+        for(int i=0; i<N+2; i++) {
+            for(int j=0; j<M+2; j++) {
+                map[i][j] = '0';
+            }
         }
         
-        for (String req : requests) {
-            if (req.length() == 2) {
-                // 크레인: 해당 알파벳의 모든 컨테이너 제거
-                char target = req.charAt(0);
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < M; j++) {
-                        if (map[i][j] == target) {
-                            map[i][j] = ' ';
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=M; j++) {
+                map[i][j] = storage[i-1].charAt(j-1);
+            }
+        }
+        for(int i=0; i<N+2; i++) {
+            System.out.println(Arrays.toString(map[i]));
+        }
+        
+        for(int i=0; i<requests.length; i++) {
+            String request = requests[i];
+            
+            if(request.length() > 1) {  // 크레인
+                char ch = request.charAt(0);
+                
+                for(int x=1; x<=N; x++) {
+                    for(int y=1; y<=M; y++) {
+                        if(map[x][y] == ch) {
+                            cnt++;
+                            map[x][y] = '0';
                         }
                     }
                 }
-            } else {
-                // 지게차: 현재 상태에서 접근 가능한 컨테이너만 제거
-                char target = req.charAt(0);
-                forklift(target);
+            }
+            else {  // 지게차
+                boolean[][] visited = new boolean[N+2][M+2];
+                boolean[][] soonRemove = new boolean[N+2][M+2];
+                
+                find(0, 0, request.charAt(0), visited, soonRemove);
+                
+                for(int x=1; x<=N; x++) {
+                    for(int y=1; y<=M; y++) {
+                        if(soonRemove[x][y]) {
+                            map[x][y] = '0';
+                        }
+                    }
+                }
             }
         }
         
-        // 남은 컨테이너 수 세기
-        int remain = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != ' ') remain++;
-            }
-        }
-        return remain;
+        return N*M - cnt;
     }
-    
-    // 지게차 요청 처리: 접근성 판단 후 제거 (한 요청 당 동시 제거)
-    public void forklift(char target) {
-        // 1. 기존의 빈 셀을 기준으로 창고 외부와 연결된 영역(air 영역) 구하기
-        boolean[][] air = new boolean[N][M];
-        boolean[][] visited = new boolean[N][M];
-        Queue<Pos> queue = new LinkedList<>();
+    private static void find(int x, int y, char ch, boolean[][] visited, boolean[][] soonRemove) {
+        visited[x][y] = true;
         
-        // 경계에 있는 빈 셀부터 시작
-        for (int i = 0; i < N; i++) {
-            if (map[i][0] == ' ') {
-                queue.offer(new Pos(i, 0));
-                visited[i][0] = true;
-                air[i][0] = true;
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx<0||nx>=N+2||ny<0||ny>=M+2 || visited[nx][ny]) {
+                continue;
             }
-            if (map[i][M-1] == ' ') {
-                if (!visited[i][M-1]) {
-                    queue.offer(new Pos(i, M-1));
-                    visited[i][M-1] = true;
-                    air[i][M-1] = true;
-                }
+            
+            if(map[nx][ny] == ch) {
+                soonRemove[nx][ny] = true;
+                visited[nx][ny] = true;
+                cnt++;
+                continue;
             }
-        }
-        for (int j = 0; j < M; j++) {
-            if (map[0][j] == ' ') {
-                if (!visited[0][j]) {
-                    queue.offer(new Pos(0, j));
-                    visited[0][j] = true;
-                    air[0][j] = true;
-                }
+            // 빈칸 아니면 넘어가기
+            if(map[nx][ny] != '0') {
+                continue;
             }
-            if (map[N-1][j] == ' ') {
-                if (!visited[N-1][j]) {
-                    queue.offer(new Pos(N-1, j));
-                    visited[N-1][j] = true;
-                    air[N-1][j] = true;
-                }
-            }
+            find(nx, ny, ch, visited, soonRemove);
         }
         
-        while (!queue.isEmpty()) {
-            Pos cur = queue.poll();
-            for (int d = 0; d < 4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (visited[nx][ny]) continue;
-                if (map[nx][ny] == ' ') {
-                    visited[nx][ny] = true;
-                    air[nx][ny] = true;
-                    queue.offer(new Pos(nx, ny));
-                }
-            }
-        }
-        
-        // 2. 접근 가능한 컨테이너(타겟 알파벳) 찾기  
-        //    (단, 이번 요청에서 제거되는 컨테이너의 제거로 인한 연쇄 반응은 고려하지 않음)
-        List<Pos> removeList = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == target) {
-                    // 경계에 있으면 바로 접근 가능
-                    if (i == 0 || i == N - 1 || j == 0 || j == M - 1) {
-                        removeList.add(new Pos(i, j));
-                    } else {
-                        // 인접 4방향 중 하나라도 air 영역이면 접근 가능
-                        boolean accessible = false;
-                        for (int d = 0; d < 4; d++) {
-                            int nx = i + dx[d];
-                            int ny = j + dy[d];
-                            if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                            if (air[nx][ny]) {
-                                accessible = true;
-                                break;
-                            }
-                        }
-                        if (accessible) removeList.add(new Pos(i, j));
-                    }
-                }
-            }
-        }
-        
-        // 3. 한 번에 접근 가능한 컨테이너 제거
-        for (Pos pos : removeList) {
-            map[pos.x][pos.y] = ' ';
-        }
+ 
     }
 }
