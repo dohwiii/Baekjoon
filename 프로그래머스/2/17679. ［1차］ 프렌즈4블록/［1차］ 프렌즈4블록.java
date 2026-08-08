b8ccb70cf571ab49c4d431409a4543e49e4a8c85
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-08-08 11:00:30 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 20.40 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,58 +1,107 @@
 import java.util.*;
 
 class Solution {
+    static char[][] map;
+    static int m, n;
+    static int count;
     public int solution(int m, int n, String[] board) {
         int answer = 0;
-        char[][] map = new char[m][n];
-        for (int i = 0; i < m; i++) {
+        this.m = m;
+        this.n = n;
+        map = new char[m][n];
+        
+        for(int i=0; i<board.length; i++) {
             map[i] = board[i].toCharArray();
         }
         
-        while (true) {
-            boolean[][] visited = new boolean[m][n];
-            boolean hasBlockToRemove = false;
-            
-            // 1. 제거할 블록 찾기
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n - 1; j++) {
-                    if (map[i][j] != ' ' && 
-                        map[i][j] == map[i][j + 1] && 
-                        map[i][j] == map[i + 1][j] && 
-                        map[i][j] == map[i + 1][j + 1]) {
-                        visited[i][j] = visited[i][j + 1] = visited[i + 1][j] = visited[i + 1][j + 1] = true;
-                        hasBlockToRemove = true;
+        while(findBlock()) {
+            makeDown();
+        }
+
+        return count;
+    }
+    private static boolean findBlock() {
+        boolean[][] is4block = new boolean[m][n];
+        int[] dx = {0, 1, 1};
+        int[] dy = {1, 1, 0};
+        Queue<int[]> queue = new ArrayDeque<>();
+       
+        for(int x=0; x<m; x++) {
+            for(int y=0; y<n; y++) {
+                if(map[x][y] == '0') {
+                    continue;
+                } 
+                int isFound = 1;
+                queue = new ArrayDeque<>();
+                queue.offer(new int[]{x, y});
+                 
+                for(int dir = 0; dir < 3; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+                    
+                    if(nx < 0 || nx >=m || ny<0 || ny>=n) {
+                        continue;
+                    }
+                    if(map[x][y] != map[nx][ny]) {  // 같은 블록이 아님
+                        isFound = 0;
+                        while(!queue.isEmpty()) {
+                            queue.poll();
+                        }
+                        break;
+                    }
+                    isFound++;
+                    queue.offer(new int[]{nx, ny});
+                }
+                if(isFound == 4) {   // 2x2 같은 블록
+                    while(!queue.isEmpty()) {
+                        int[] arr = queue.poll();
+                        is4block[arr[0]][arr[1]] = true;    // 2x2 블록 완성
+                    } 
+                }
+                else {
+                    while(!queue.isEmpty()) {   // 2x2 아니니깐 다 버리기
+                        queue.poll();
                     }
                 }
-            }
-            
-            if (!hasBlockToRemove) break;
-            
-            // 2. 블록 제거 및 개수 세기
-            int removedBlocks = 0;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (visited[i][j]) {
-                        map[i][j] = ' ';
-                        removedBlocks++;
-                    }
-                }
-            }
-            answer += removedBlocks;
-            
-            // 3. 블록 내리기 (열별로 처리)
-            for (int j = 0; j < n; j++) {
-                int writeIdx = m - 1;
-                for (int i = m - 1; i >= 0; i--) {
-                    if (map[i][j] != ' ') {
-                        map[writeIdx--][j] = map[i][j];
-                    }
-                }
-                while (writeIdx >= 0) {
-                    map[writeIdx--][j] = ' ';
-                }
+                
             }
         }
+        int cnt = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(is4block[i][j]) {
+                    map[i][j] = '0';
+                    cnt++;
+                    count++;
+                }
+            }
+        }   
+        if(cnt == 0) {
+            return false;
+        }
+        return true;
+    }
+    private static void makeDown() {    // '0' 인 블록들 끌어내리기
         
-        return answer;
+        for(int c = 0; c < n; c++) {    // 열
+            Queue<Integer> queue = new ArrayDeque<>();
+            
+            for(int r = m-1; r>=0; r--) {
+                if(map[r][c] == '0') {
+                    queue.offer(r);
+                }
+                else {  // 숫자인 블록
+                    // 0인 블록들이 아래에 있다면
+                    while(!queue.isEmpty()) {
+                        int nr = queue.poll();
+                        map[nr][c] = map[r][c];
+                        map[r][c] = '0';
+                        queue.offer(r);
+                        break;
+                    }
+                }
+            }
+            
+        }
     }
 }
