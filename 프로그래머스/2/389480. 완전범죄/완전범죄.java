7cb70298f6267580881387c44a56c87368d80ce2
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-09-24 20:02:39 +0900

    [level 2] Title: 완전범죄, Time: 0.24 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+        int itemCnt = info.length;
+        int[] dp = new int[itemCnt * 3 + 1];
+        Arrays.fill(dp, itemCnt * 3 + 1);
+        dp[0] = 0;
+        
+        for(int i=0; i<itemCnt; i++) {
+            int[] next = new int[itemCnt * 3 + 1];
+            int a = info[i][0];
+            int b = info[i][1];
+            Arrays.fill(next, itemCnt * 3 + 1);
+            
+            
+            for(int j=0; j<m; j++) {
+                if(dp[j] != itemCnt * 3 + 1) {
+                    // A가 훔쳤을 때
+                    if(dp[j] + a < n) {
+                        next[j] = Math.min(next[j], dp[j] + a);
+                    } 
+                    
+                    // B가 훔쳤을 때
+                    if(j != itemCnt * 3 + 1) {
+                        if(j + b < m) {
+                            next[j + b] = Math.min(next[j+b], dp[j]);
+                        } 
+                    }
+                }
+            }
+            
+            dp = next.clone();
+        }
+        answer = 121;
+        for(int i=0; i<m; i++) {
+            if(dp[i] == itemCnt * 3 + 1) {
+                continue;
+            }
+            answer = Math.min(answer, dp[i]);
+        }
+        if(answer == 121) {
+            answer = -1;
+        }
+
+        // 두 도둑 모두 경찰에 붙잡히지 않게 할 수 없다면 -1
+        return answer;
+    }
+}
