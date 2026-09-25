f6368d005b5002766df90efd8791b0bbc613d037
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-09-25 21:18:27 +0900

    [level 2] Title: 피로도, Time: 5630.31 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nB9xkiVZFsqw-uD3NZz3N_7g&gclid=Cj0KCQjw94bTBhDQARIsAN3vv0wrJ4ZY62ljTH89OhWcnzKqbCRuNa7NWNHvj_IY5WnhlsujWau9yE0aAoC6EALw_wcB) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 1.81 ms
+메모리: 380 MB, 시간: 5630.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 23일 22:19:16
+2026년 09월 25일 21:18:24
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,39 @@
 import java.util.*;
 
 class Solution {
-    static int max;
+    static int maxDungeon;
+    
     public int solution(int k, int[][] dungeons) {
         int answer = -1;
         int N = dungeons.length;    // 던전 개수
         
-        permutation(0, k, dungeons, new boolean[N]);
+        // dp[피로도] = 탐험한 던전 개수 이렇게 저장해서 풀어야하나?
+        // 아니면 진짜 완전탐색? -> 근데 두번쨰 (50, 40)일 떄 선택을 안하고 세번쨰로 넘어가면 두번째는 언제함?
         
-        return max;
+        dfs(k, new boolean[N], 0, dungeons, 0);
+        
+        
+        
+        // 유저가 탐험할 수 있는 최대 던전 수
+        return maxDungeon;
     }
-    private static void permutation(int depth, int fatigue, int[][] dungeons, boolean[] visited) {
-        max = Math.max(max, depth);
+    private static void dfs(int k, boolean[] visited, int cnt, int[][] dungeons, int depth) {
+        // System.out.println(depth+" "+k+" "+Arrays.toString(visited));
+        if(depth == dungeons.length) {
+            maxDungeon = Math.max(maxDungeon, cnt);
+            return;
+        }
         
         for(int i=0; i<dungeons.length; i++) {
-            if(!visited[i]) {
-                if(fatigue >= dungeons[i][0]) {
-                    visited[i] = true;
-                    permutation(depth + 1, fatigue - dungeons[i][1], dungeons, visited);
-                    visited[i] = false;
-                }
+            if(!visited[i]) {   // 아직 방문 안했다면
+                dfs(k, visited.clone(), cnt, dungeons, depth+1);    // 선택안함
                 
+                int min = dungeons[i][0];
+                if(k >= min) {
+                    boolean[] newVisited = visited.clone();
+                    newVisited[i] = true;
+                    dfs(k - dungeons[i][1], newVisited, cnt+1, dungeons, depth+1);   // 선택함
+                }
             }
         }
     }
