acc6571c13016fb41a196430c2c04e67909dac73
dohwiii <102406112+dohwiii@users.noreply.github.com>
2026-09-26 15:06:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 16.66 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,58 +1,60 @@
 import java.util.*;
+
 class Solution {
     static List<Integer>[] list;
-    static boolean[] visited;
-    static int[] depth;
-    
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        depth = new int[n+1];
-        visited = new boolean[n+1];
+        int answer = 1;
         list = new List[n+1];
-        for(int i=0; i<=n; i++) {
+        
+        for(int i=1; i<=n; i++) {
             list[i] = new ArrayList<>();
         }
-        
         for(int[] e : edge) {
+            // 양방향
             list[e[0]].add(e[1]);
             list[e[1]].add(e[0]);
         }
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(1, 1));
-        Arrays.fill(depth, Integer.MAX_VALUE);
-        depth[1] = 0;
-        int maxDepth = 0;
+        int max = 0;
+        int[] dist = bfs(1, n);
+        for(int i=1; i<=n; i++) {
+            if(dist[i] > max) {
+                answer = 1;
+                max = dist[i];
+            }
+            else if(max == dist[i]) {
+                answer++;
+            }
+        }        
+        // 1번 노드로부터 가장 멀리 떨어진 노드가 몇 개 -> 다익스트라
+        return answer;
+    }
+    private static int[] bfs(int node, int n) {
+        Queue<Node> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        visited[1] = true;
+        int[] dist = new int[n+1];
+        queue.offer(new Node(1, 0));
         
-        while(!pq.isEmpty()) {
-            Node now = pq.poll();
+        while(!queue.isEmpty()) {
+            Node now = queue.poll();
             
-            for(int next : list[now.v]) {
-                int nextCost = Math.min(depth[next], depth[now.v] + 1);
-                if(depth[next] > nextCost) {
-                    depth[next] = nextCost;
-                    pq.offer(new Node(next, nextCost));
-                    maxDepth = Math.max(maxDepth, depth[next]);
+            for(int next : list[now.node]) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    dist[next] = now.step + 1;
+                    queue.offer(new Node(next, now.step + 1));
                 }
             }
         }
-        for(int i=1; i<=n; i++) {
-            if(depth[i] == maxDepth) {
-                answer++;
-            }
-        }
+        return dist;
         
-        return answer;
     }
-
-}
-class Node implements Comparable<Node> {
-    int v, c;
-    public Node(int v, int c) {
-        this.v=v;
-        this.c=c;
+    static class Node {
+        int node, step;
+        
+        public Node(int node, int step) {
+            this.node=node;
+            this.step=step;
+        }
     }
-    @Override
-    public int compareTo(Node n) {
-        return this.c - n.c;
-    }
-}
+} 
